1d3f103f396d06827470166b7eca517cbc07a738
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2020-06-10 23:35:43 +0200

    Update installPythonVirtualenv.sh

--- installPythonVirtualenv.sh
+++ installPythonVirtualenv.sh
@@ -18,8 +18,8 @@ python3 -m pip install adafruit-blinka RPI.GPIO
 echo "source ~/venv/venv3.7/bin/activate" >> ~/.bashrc
 source ~/.bashrc
 
-python3 -m pip install openpyxl pyzmail o365 ttn qrcode pillow pyodbc sqlalchemy pymsteams
-python3 -m pip install picamera opencv-contrib-python libwebp6
+python3 -m pip install openpyxl pyzmail o365 ttn qrcode pillow sqlalchemy pymsteams
+python3 -m pip install picamera opencv-contrib-python
 
 sudo apt install -y tesseract-ocr libtesseract-dev libatlas3-base libqtcore4 -y
 python3 -m pip install pytesseract opencv-contrib-python libwebp6
