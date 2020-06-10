6701bf0fb6ab87d661a761e9b701de075d2cb54d
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2020-06-10 08:44:56 +0200

    Update installPythonCircuitpython.sh

--- installPythonCircuitpython.sh
+++ installPythonCircuitpython.sh
@@ -1,5 +1,6 @@
 #/usr/bin/sh
-echo "Installeer nieuwe python3 virtuale omgeving" source ~/venv/venv3.7 
+echo "Installeer nieuwe python3 virtuale omgeving"
+source ~/venv/venv3.7/bin/activate 
 pip3 install adafruit-blinka adafruit-circuitpython-ble-adafruit adafruit-circuitpython-ina219 adafruit-circuitpython-tinylora adafruit-circuitpython-ssd1306 
 pip3 install adafruit-circuitpython-miniqr adafruit-circuitpython-turtle adafruit-circuitpython-74hc595 adafruit-circuitpython-display-button adafruit-circuitpython-mpu6050 adafruit-circuitpython-ina260 adafruit-circuitpython-ssd1306 adafruit-circuitpython-ble  adafruit-circuitpython-ble-heart-rate adafruit-circuitpython-ble-broadcastnet adafruit-circuitpython-binascii  adafruit-circuitpython-onewire adafruit-circuitpython-motor  adafruit-circuitpython-hts221 adafruit-circuitpython-midi adafruit-circuitpython-bmp280  adafruit-circuitpython-bme680 adafruit-circuitpython-dotstar  adafruit-circuitpython-max7219 adafruit-circuitpython-display-text adafruit-circuitpython-rfm69 adafruit-circuitpython-dht adafruit-circuitpython-ble-apple-media adafruit-circuitpython-bme280 adafruit-circuitpython-bitbangio adafruit-circuitpython-led-animation adafruit-circuitpython-ble-midi adafruit-circuitpython-sd adafruit-circuitpython-neopixel adafruit-circuitpython-hid adafruit-circuitpython-gps  adafruit-circuitpython-rfm9x  circuitpython-base64 circuitpython-base64 circuitpython-esp32connection adafruit-circuitpython-ble-radio circuitpython-nrf24l01 thonny-circuitpython 
 
