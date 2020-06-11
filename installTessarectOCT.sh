5234ee8a86ade581f5875a6111bf80ddf21bfcb4
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2020-06-11 20:53:53 +0200

    Create installTessarectOCT.sh

---
+++ installTessarectOCT.sh
@@ -0,0 +1,5 @@
+sudo apt install -y tesseract-ocr libtesseract-dev libatlas3-base libqtcore4 -y
+tesseract --version
+sudo apt install -y imagemagick
+source ~/venv/venv3.7/bin/activate
+pip3 install pytesseract opencv-contrib-python libwebp6
