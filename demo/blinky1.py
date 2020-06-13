658a643d88f791961749d0a8d830110efc0033fc
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2020-06-13 14:21:18 +0200

    Update blinky1.py

--- demo/blinky1.py
+++ demo/blinky1.py
@@ -6,11 +6,11 @@ import digitalio
  
 print("hello blinky!")
  
-led = digitalio.DigitalInOut(board.D18)
+led = digitalio.DigitalInOut(board.D16)
 led.direction = digitalio.Direction.OUTPUT
  
 while True:
     led.value = True
     time.sleep(0.5)
     led.value = False
-    time.sleep(0.5)
+    time.sleep(0.2)
