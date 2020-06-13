08858d73b7a9daa036eac9a52338e141e289ea31
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2020-06-13 14:19:51 +0200

    Create blinky1.py

---
+++ demo/blinky1.py
@@ -0,0 +1,16 @@
+# https://learn.adafruit.com/circuitpython-on-raspberrypi-linux?view=all
+
+import time
+import board
+import digitalio
+ 
+print("hello blinky!")
+ 
+led = digitalio.DigitalInOut(board.D18)
+led.direction = digitalio.Direction.OUTPUT
+ 
+while True:
+    led.value = True
+    time.sleep(0.5)
+    led.value = False
+    time.sleep(0.5)
