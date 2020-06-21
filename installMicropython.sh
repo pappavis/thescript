2783d948e7d8befdcead391f1e80032f84bbe5ce
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2020-06-21 15:57:40 +0200

    Update installMicropython.sh

--- installMicropython.sh
+++ installMicropython.sh
@@ -6,4 +6,12 @@ make
 cd ~/micropython/ports/unix/
 make clean
 make
-./micropython
+sudo ln -s ~/micropython/ports/unix/micropython /usr/local/bin/micropython
+echo "START module intstall"
+micropython  -m upip install micropython-socket
+micropython  -m upip install micropython-machine
+micropython  -m upip install micropython-os.path
+micropython  -m upip install micropython-umqtt.robust
+micropython  -m upip install micropython-pwd
+micropython  -m upip install micropython-smtplib
+echo "EINDE micropython module intstall"
