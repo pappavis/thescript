35ce1e5ec7aeede8da4a4deb7bd616a6f6dd441b
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-08-13 14:36:19 +0200

    Update installPythonVirtualenv.sh

--- installPythonVirtualenv.sh
+++ installPythonVirtualenv.sh
@@ -1,9 +1,9 @@
 #/usr/bin/sh
 echo "Installeer nieuwe python3 virtuale omgeving"
-sudo usermod -aG gpio pi
-sudo usermod -aG dialout pi
-sudo usermod -aG i2c pi
-sudo usermod -aG tty pi
+sudo usermod -aG gpio pi &
+sudo usermod -aG dialout pi  &
+sudo usermod -aG i2c pi &
+sudo usermod -aG tty pi &
 
 sudo apt install -y python3-pip python3-venv
 python3 -m pip install --user pipx pipenv
@@ -20,8 +20,6 @@ pip3.7 install adafruit-blinka RPI.GPIO
 echo "source ~/venv/venv3.7/bin/activate" >> ~/.bashrc
 source ~/.bashrc
 
-pip3.7 install openpyxl o365 ttn qrcode pillow sqlalchemy pymsteams esptool adafruit-ampy pyserial pyparsing
-easy_install pyzmail
+pip3.7 install openpyxl o365 ttn qrcode pillow sqlalchemy pymsteams esptool adafruit-ampy pyserial pyparsing pyzmail
 pip3.7 install picamera opencv-contrib-python
-pip3.7 install  scikit-build
-easy_install pygamer
+pip3.7 install  scikit-build pygamer
