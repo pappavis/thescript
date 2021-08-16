fcd9d6fad09f5bb6243fcd068f52e3bd1304369b
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-08-16 18:47:13 +0200

    Update installOctoprintWebcam.sh

--- installOctoprintWebcam.sh
+++ installOctoprintWebcam.sh
@@ -1,10 +1,10 @@
-prinf "\nStart octprint webcam install\n"
+echo "\nStart octprint webcam install\n"
 cd ~/Downloads
 sudo apt install -y subversion libjpeg62-turbo-dev imagemagick ffmpeg libv4l-dev cmake
 git clone https://github.com/jacksonliam/mjpg-streamer.git
 cd ~/Downloads/mjpg-streamer/mjpg-streamer-experimental
 export LD_LIBRARY_PATH=.
 make
-prinf "\nOctprint webcam install gereed\n"
-prinf "\nOctprint webcam streaming op http://pivhere:8080/?action=stream\n"
+echo "\nOctprint webcam install gereed\n"
+echo "\nOctprint webcam streaming op http://pivhere:8080/?action=stream\n"
 ./mjpg_streamer -i "./input_uvc.so -y" -o "./output_http.so" 
