4ae18d13f01e34d94b81ef7b2992733ad4ccdcb9
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-08-22 19:31:31 +0200

    Update installExtras.sh

--- installExtras.sh
+++ installExtras.sh
@@ -7,4 +7,4 @@ chmod +x ./vhuitarm7
 sudo cp ./vhusbdarmpi3 /usr/local/bin
 sudo cp ./vhuitarm7 /usr/local/bin
 echo "Start VirtualHerer Raspberry Pi server"
-vhusbdarmpi3 -b 
+sudo vhusbdarmpi3 -b 
