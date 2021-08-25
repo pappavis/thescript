182a90d6e2571a9b00428601bab72a6b1c12e4ae
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-08-25 17:59:56 +0200

    Create installHomebridgeApple.s

---
+++ installHomebridgeApple.s
@@ -0,0 +1,11 @@
+printf "Start installatie homebridge voor Apple homekit.\n"
+printf "Zie ook https://github.com/homebridge/homebridge/wiki/Install-Homebridge-on-Raspbian\n"
+sudo apt install -y nodejs gcc g++ make python net-tools
+printf "NodeJS versie: "
+node -v
+printf "\nApple homekit wordt geïnstalleerd.\n"
+sudo npm install -g --unsafe-perm homebridge homebridge-config-ui-x
+printf "Homekit als service gestart\n"
+sudo hb-service install --user homebridge
+printf "Homekit op http://dietpi.local:8581, gebruiker=admin, wachtwoord=admin\n"
+hostname -I
