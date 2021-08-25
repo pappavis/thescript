3d549a8ed15ac0cbd66c3b2c1de0ce7ce79cbcce
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-08-25 17:45:13 +0200

    Create installUpdates.sh

---
+++ installUpdates.sh
@@ -0,0 +1,14 @@
+printf "Laatste versies worden opgehaal en geinstalleerd.\n"
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt autoclean -y
+sudo apt autoremove -y
+
+printf "NodeJS wordt bijgewerkt naar de laatste versie\n"
+curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+sudo apt install -y nodejs
+sudo apt install -y build-essential
+printf "installUpdates.sh afgerond.\n"
+printf "Werkt die NodeJS installatie?\n"
+curl -fsSL https://deb.nodesource.com/test | bash -
+printf "NodeJS test install afgerond.\n"
