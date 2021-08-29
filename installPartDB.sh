ea02c3c57515276651454afd0076aad9ad9ffc7c
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-08-29 09:52:12 +0200

    Update installPartDB.sh

--- installPartDB.sh
+++ installPartDB.sh
@@ -12,7 +12,7 @@ wget https://github.com/jbtronics/Part-DB/blob/gh-pages/vendor.zip?raw=true
 sudo mv ./vendor  /var/www/html/support/part-db
 cd  /var/www/html/support/part-db
 sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-sudo php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+rem sudo php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php composer-setup.php
 sudo php -r "unlink('composer-setup.php');"
 sudo chown -R www-data:www-data /var/www/html/support/part-db
