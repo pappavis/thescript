b1e4a2544b20d96fd865f4d8890bd171c58e1010
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-02 07:57:18 +0200

    Update get_scargillScript.sh

--- get_scargillScript.sh
+++ get_scargillScript.sh
@@ -1,3 +1,7 @@
 cd ~/
 wget https://bitbucket.org/api/2.0/snippets/scargill/kAR5qG/master/files/script.sh
 
+cd ~/.node-red/
+https://tech.scargill.net/iot/settings.txt -O settings.js
+cp settings.js settings.js.bak-pre-crypt
+printf "\nScargill script is afgelaai.\n"
