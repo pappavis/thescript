120b25af7663c3b47456ee1d086bf0f282191d6c
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-04 13:00:34 +0200

    Update get_scargillScript.sh

--- get_scargillScript.sh
+++ get_scargillScript.sh
@@ -1,7 +1,14 @@
-cd ~/
+printf "\nScargill script wordt afgelaai.\n"
+cd /home/pi
 wget https://bitbucket.org/api/2.0/snippets/scargill/kAR5qG/master/files/script.sh
 
-cd ~/.node-red/
+printf "\nNode-red wachtwoorden worden verwijdert"
+cd /home/pi/.node-red/
 https://tech.scargill.net/iot/settings.txt -O settings.js
 cp settings.js settings.js.bak-pre-crypt
-printf "\nScargill script is afgelaai.\n"
+
+cd /home/pi/Downloads
+wget http://www.sensorsiot.org/transferFiles/Pi_Zero_MQTT_Broker/nodeRedUserReset.sh
+printf "\nNode-red wachtwoord reset script nodeRedUserReset.sh afgelaai.\n"
+
+printf "\nScargill script afgelaai.\n"
