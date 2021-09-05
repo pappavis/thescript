031927904554051ba858dc2599868c8658a50f37
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-05 13:38:57 +0200

    Update installExtras.sh

--- installExtras.sh
+++ installExtras.sh
@@ -1,5 +1,5 @@
 cd ~/Downloads
-echo "Download en installeer virtualhere.com Pi 3 sever & client"
+echo "Download en installeer virtualhere.com Pi 3 server & client"
 wget https://www.virtualhere.com/sites/default/files/usbserver/vhusbdarmpi3
 wget https://www.virtualhere.com/sites/default/files/usbclient/vhuitarm7
 chmod +x ./vhusbdarmpi3
@@ -8,3 +8,4 @@ sudo cp ./vhusbdarmpi3 /usr/local/bin
 sudo cp ./vhuitarm7 /usr/local/bin
 echo "Start VirtualHerer Raspberry Pi server"
 sudo vhusbdarmpi3 -b 
+echo "Je moet 'vhusbdarmpi3 -b' toevoegen aan /etc/rc.local. voor de exit 0 "
