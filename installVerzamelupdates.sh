7b123af92661623a180d5f6b8a93ef6abfb1d406
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-05 13:33:35 +0200

    Create installVerzamelupdates.sh

---
+++ installVerzamelupdates.sh
@@ -0,0 +1,10 @@
+printf "\n** 20210904 Bijwerken Python, Circuitpython, instalaltie Micropython\n**"
+cd ~/Downloads/thescript
+printf "\nTesseract OCR wordt bijgewerkt\n"
+bash ./installTessarectOCT.sh
+printf "\nCircuitPython wordt bijgewerkt\n"
+bash ./installPythonCircuitpython.sh
+printf "\nPython wordt bijgewerkt\n"
+bash ./installPythonVirtualenv.sh
+
+printf "\ninstallVerzamelupdates.sh afgerond.\n"
