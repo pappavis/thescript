d729a5dc709185a2cd2973460b685994af29f209
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-07 07:48:11 +0200

    Update installHomeassistant.sh

--- installHomeassistant.sh
+++ installHomeassistant.sh
@@ -1,11 +1,15 @@
+_hn="http://"$(hostname).local":8123"
+ehco "Installeren Homeassistant"
 ~/.local/bin/virtualenv ~/venv/homeassistant
 source ~/venv/homeassistant/bin/activate
 pip3 install homeassistant
-sudo echo "source /home/pi/venv/homeassistant/bin/activate" >> /usr/local/bin/hass
-sudo echo "/home/pi/venv/homeassistant/bin/hass" >> /usr/local/bin/hass
+sudo echo "source /home/pi/venv/homeassistant/bin/activate" >> /usr/local/bin/homeassistant
+sudo chmod +x /usr/local/bin/homeassistant
+sudo echo "/home/pi/venv/homeassistant/bin/hass" >> /usr/local/bin/homeassistant
 pip install --upgrade pip homebridge
-sudo echo "/home/pi/venv/homeassistant/bin/hass" >> /usr/local/bin/hass
-sudo echo "source /home/pi/venv/venv3.7/bin/activate"  >> /usr/local/bin/hass
-sudo echo "printf 'Start homeassitant op http://dietpi.local:8123\n'"  >> /usr/local/bin/hass
-sudo chmod +x /usr/local/bin/hass
-printf "homeassistant install afgerond\n"
+sudo echo "/home/pi/venv/homeassistant/bin/hass" >> /usr/local/bin/homeassistant
+sudo echo "source /home/pi/venv/venv3.7/bin/activate"  >> /usr/local/bin/homeassistant
+sudo echo "printf '\nStart homeassitant op http://". $_hn.local:8123\n'" . "  >> /usr/local/bin/homeassistant
+printf "\nStart homeassistant op "
+printf $_hn
+printf "\n\nhomeassistant install afgerond\n"
