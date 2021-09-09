5961093e89f5c1ff852d1d6cd9ec8b38f7ee030b
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-09 10:53:20 +0200

    Update installNFSserver.sh

--- installNFSserver.sh
+++ installNFSserver.sh
@@ -10,3 +10,4 @@ sudo find /mnt/nfsshare/ -type f -exec chmod 644 {} \;
 sudo printf "\n/mnt/nfsshare *(rw,all_squash,insecure,async,no_subtree_check,anonuid=1000,anongid=1000)\n" >> /etc/exports
 sudo exportfs -ra
 printf "\nNFS bestanddeling is ingesteld\n -- MacOS verbind aan nfs://$_hn1.local/nfsshare  of nfs://$_ip1/nfsshare \n -- Windows verbind aan //$ip1.local/nfsshare\n\nIP adres $_ip1\n"
+udo service nfs-server restart
