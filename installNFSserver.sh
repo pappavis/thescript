e82d2664463c48a7f408d46620ffcd04912b6c05
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-09 11:46:10 +0200

    Update installNFSserver.sh

--- installNFSserver.sh
+++ installNFSserver.sh
@@ -12,10 +12,11 @@ sudo exportfs -ra
 sudo service nfs-server restart
 
 printf "\nProbeer netwerk share te mount op /mnt\n"
-sudo mount -t nfs -o proto=tcp,port=2049 pilamp:/nfsshare /mnt &
-sudo mount -t nfs -o proto=tcp,port=2049 pi0:/nfsshare /mnt &
-sudo mount -t nfs -o proto=tcp,port=2049 pivhere:/nfsshare /mnt &
-sudo mount -t nfs -o proto=tcp,port=2049 retropie:/nfsshare /mnt &
+sudo mount -t nfs -o proto=tcp,port=2049 pilamp.local:/nfsshare /mnt &
+sudo mount -t nfs -o proto=tcp,port=2049 pi0.local:/nfsshare /mnt &
+sudo mount -t nfs -o proto=tcp,port=2049 pivhere.local:/nfsshare /mnt &
+sudo mount -t nfs -o proto=tcp,port=2049 retropie.local:/nfsshare /mnt &
+sudo mount -t nfs -o proto=tcp,port=2049 p1mon.local:/nfsshare /mnt &
 
 sudo printf "\n HANDMATIG toevoegen aan /etc/exports/  : \n/mnt/nfsshare *(rw,all_squash,insecure,async,no_subtree_check,anonuid=1000,anongid=1000)\n"
 printf "\nNFS bestanddeling is daarna bereikbaar:\n -- MacOS verbind aan nfs://$_hn1.local/nfsshare  of nfs://$_ip1/nfsshare \n -- Windows verbind aan //$ip1.local/nfsshare\n\nIP adres $_ip1\n"
