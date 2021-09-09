81b58cdfd9a8ac6a2f15aac336cef2ccbaf92d32
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-09 11:45:06 +0200

    Update installNFSserver.sh

--- installNFSserver.sh
+++ installNFSserver.sh
@@ -9,8 +9,6 @@ sudo find /mnt/nfsshare/ -type d -exec chmod 755 {} \;
 sudo find /mnt/nfsshare/ -type f -exec chmod 644 {} \;
 sudo cp /etc/exports ~/exp1.tmp
 sudo exportfs -ra
-sudo printf "\n HANDMATIG toevoegen aan /etc/exports/  : \n/mnt/nfsshare *(rw,all_squash,insecure,async,no_subtree_check,anonuid=1000,anongid=1000)\n"
-printf "\nNFS bestanddeling is ingesteld\n -- MacOS verbind aan nfs://$_hn1.local/nfsshare  of nfs://$_ip1/nfsshare \n -- Windows verbind aan //$ip1.local/nfsshare\n\nIP adres $_ip1\n"
 sudo service nfs-server restart
 
 printf "\nProbeer netwerk share te mount op /mnt\n"
@@ -19,6 +17,8 @@ sudo mount -t nfs -o proto=tcp,port=2049 pi0:/nfsshare /mnt &
 sudo mount -t nfs -o proto=tcp,port=2049 pivhere:/nfsshare /mnt &
 sudo mount -t nfs -o proto=tcp,port=2049 retropie:/nfsshare /mnt &
 
+sudo printf "\n HANDMATIG toevoegen aan /etc/exports/  : \n/mnt/nfsshare *(rw,all_squash,insecure,async,no_subtree_check,anonuid=1000,anongid=1000)\n"
+printf "\nNFS bestanddeling is daarna bereikbaar:\n -- MacOS verbind aan nfs://$_hn1.local/nfsshare  of nfs://$_ip1/nfsshare \n -- Windows verbind aan //$ip1.local/nfsshare\n\nIP adres $_ip1\n"
 printf "\nHandmatig uitvoeren:\n  sudo service nfs-server restart\n"
 
 
