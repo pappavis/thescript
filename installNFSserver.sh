f964583a2f8ca3714754b48d414ef6bab099e56c
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-09 11:41:06 +0200

    Update installNFSserver.sh

--- installNFSserver.sh
+++ installNFSserver.sh
@@ -8,9 +8,15 @@ sudo chown -R pi:pi /mnt/nfsshare
 sudo find /mnt/nfsshare/ -type d -exec chmod 755 {} \;
 sudo find /mnt/nfsshare/ -type f -exec chmod 644 {} \;
 sudo cp /etc/exports ~/exp1.tmp
-
-sudo printf "\n HANDMATIG toevoegen aan /etc/exports/  : \n/mnt/nfsshare *(rw,all_squash,insecure,async,no_subtree_check,anonuid=1000,anongid=1000)\n"
 sudo exportfs -ra
+sudo printf "\n HANDMATIG toevoegen aan /etc/exports/  : \n/mnt/nfsshare *(rw,all_squash,insecure,async,no_subtree_check,anonuid=1000,anongid=1000)\n"
 printf "\nNFS bestanddeling is ingesteld\n -- MacOS verbind aan nfs://$_hn1.local/nfsshare  of nfs://$_ip1/nfsshare \n -- Windows verbind aan //$ip1.local/nfsshare\n\nIP adres $_ip1\n"
 sudo service nfs-server restart
+mount -t nfs -o proto=tcp,port=2049 pilamp:/nfsshare /mnt
+mount -t nfs -o proto=tcp,port=2049 pi0:/nfsshare /mnt
+mount -t nfs -o proto=tcp,port=2049 pivhere:/nfsshare /mnt
+mount -t nfs -o proto=tcp,port=2049 retropie:/nfsshare /mnt
+
 printf "\nHandmatig uitvoeren:\n  sudo service nfs-server restart\n"
+
+
