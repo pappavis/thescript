1befcf812217db8718150915c517e05d1fbb021b
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-11 15:01:33 +0200

    Update installOwncloud.sh

--- installOwncloud.sh
+++ installOwncloud.sh
@@ -6,6 +6,6 @@ sudo phpenmod  intl
 wget https://download.owncloud.org/community/owncloud-complete-20210721.zip
 7z x ./owncloud-complete-20210721.zip
 sudo mv ./owncloud /var/html/support
-sudo chown -R root:root /var/www/html/support/owncloud
+sudo chown -R www-data:www-data /var/www/html/support/owncloud
 sudo service apache2 restart
 echo "Owncloud beschikbaar op http://$_hn1.local/support/owncloud"
