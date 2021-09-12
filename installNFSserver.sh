aa7c5cc6671e49f984f4f11c0c5659938aa2a1a1
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-12 13:05:04 +0200

    Update installNFSserver.sh

--- installNFSserver.sh
+++ installNFSserver.sh
@@ -1,15 +1,16 @@
 _id1=$(id pi)
 _hn1=$(hostname)
 _ip1=$(hostname -I)
+mkdir ~/tmp
 printf "\nStart instellen van NFS server op \n" $_ip1 ".local\n"
 sudo apt-get install -y nfs-kernel-server nfs-common
 sudo mkdir /mnt/nfsshare
-sudo chown -R pi:pi /mnt/nfsshare
-sudo find /mnt/nfsshare/ -type d -exec chmod 755 {} \;
-sudo find /mnt/nfsshare/ -type f -exec chmod 644 {} \;
-sudo cp /etc/exports ~/exp1.tmp
+sudo chown -R nobody:nogroup /mnt/nfsshare/
+sudo chmod 777 /mnt/nfsshare/
+sudo cp /etc/exports ~/tmp/exp1.tmp
 sudo exportfs -ra
 sudo service nfs-server restart
+sudo systemctl restart nfs-kernel-server
 
 printf "\nProbeer netwerk share te mount op /mnt\n"
 sudo mount -t nfs -o proto=tcp,port=2049 pilamp.local:/nfsshare /mnt &
