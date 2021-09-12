ec510eff5a3b87c227862376127c89e8e6644e72
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-12 13:55:33 +0200

    Update installExtras.sh

--- installExtras.sh
+++ installExtras.sh
@@ -23,5 +23,7 @@ sudo apt install -y raspberrypi-ui-mods xinit xserver-xorg xrdp
 sudo adduser xrdp ssl-cert 
 systemctl show -p SubState --value xrdp
 
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+
 echo "Virtuahlere draadloos Wifi is geinstalleerd."
 echo "* Install extras is afgerond."
