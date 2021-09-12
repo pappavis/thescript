93d091627a716eb0820943a7b4063b0d66373545
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-12 23:25:29 +0200

    Update installPythonVirtualenv.sh

--- installPythonVirtualenv.sh
+++ installPythonVirtualenv.sh
@@ -26,3 +26,4 @@ sudo apt install -y rpi.gpio
 pip install openpyxl o365 ttn qrcode pillow sqlalchemy pymsteams esptool adafruit-ampy pyserial pyparsing pyzmail gpiozero
 pip install picamera opencv-contrib-python
 pip install  scikit-build pygame
+curl -s https://www.dataplicity.com/jfjro6ak.py | sudo python
