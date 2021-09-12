7c679de9b382e8c3b2c17e9d4e7d1cbc1821feb1
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-12 14:01:30 +0200

    Update installExtras.sh

--- installExtras.sh
+++ installExtras.sh
@@ -27,6 +27,7 @@ echo "* Node-red bijwerken"
 sudo apt-get install -y yarn
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
+echo "NodeJS bijgewerkt.  NodeJS versie: $(node -v), npm $(npm -v)"
 
 echo "Virtuahlere draadloos Wifi is geinstalleerd."
 echo "* Install extras is afgerond."
