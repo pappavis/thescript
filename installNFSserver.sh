6b27365d45d32fda18de2fca6e8d01341ecfa339
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-13 12:25:34 +0200

    Update installNFSserver.sh

--- installNFSserver.sh
+++ installNFSserver.sh
@@ -4,9 +4,9 @@ _ip1=$(hostname -I)
 mkdir ~/tmp
 printf "\nStart instellen van NFS server op \n" $_ip1 ".local\n"
 sudo apt-get install -y nfs-kernel-server nfs-common firewalld
-sudo mkdir /mnt/nfsshare
-sudo chown -R nobody:nogroup /mnt/nfsshare/
-sudo chmod 777 /mnt/nfsshare/
+sudo mkdir /mnt/nfs/nfsshare
+sudo chown -R nobody:nogroup /mnt/nfs/nfsshare/
+sudo chmod 777 /mnt/nfs/nfsshare/
 sudo cp /etc/exports ~/tmp/exp1.tmp
 sudo exportfs -ra
 sudo service nfs-server restart
@@ -17,30 +17,30 @@ sudo firewall-cmd --permanent --zone=public --add-service=mountd
 sudo firewall-cmd --permanent --zone=public --add-service=rpc-bind
 sudo firewall-cmd --reload
 
-sudo mkdir -p /mnt/acer01/
-sudo mkdir -p /mnt/pi0/
-sudo mkdir -p /mnt/pivhere/
-sudo mkdir -p /mnt/dietpi/
-sudo mkdir -p /mnt/pi4/
-sudo mkdir -p /mnt/pilamp/
-sudo mkdir -p /mnt/spelen02/
+sudo mkdir -p /mnt/nfs/acer01/
+sudo mkdir -p /mnt/nfs/pi0/
+sudo mkdir -p /mnt/nfs/pivhere/
+sudo mkdir -p /mnt/nfs/dietpi/
+sudo mkdir -p /mnt/nfs/pi4/
+sudo mkdir -p /mnt/nfs/pilamp/
+sudo mkdir -p /mnt/nfs/spelen02/
 
-sudo chmod 777 /mnt/nfsshare/
-sudo chmod 777 /mnt/acer01/
-sudo chmod 777 /mnt/pi0/
-sudo chmod 777 /mnt/pivhere/
-sudo chmod 777 /mnt/pi4/
-sudo chmod 777 /mnt/pilamp/
-sudo chmod 777 /mnt/spelen02/
+sudo chmod 777 /mnt/nfs/nfsshare/
+sudo chmod 777 /mnt/nfs/acer01/
+sudo chmod 777 /mnt/nfs/pi0/
+sudo chmod 777 /mnt/nfs/pivhere/
+sudo chmod 777 /mnt/nfs/pi4/
+sudo chmod 777 /mnt/nfs/pilamp/
+sudo chmod 777 /mnt/nfs/spelen02/
 
 
 printf "\nProbeer netwerk share te mount op /mnt\n"
 sudo mount -t auto acer01:/home /mnt/acer01/ &
-sudo mount -t auto acer01:/home /mnt/pi0/ &
-sudo mount -t auto acer01:/home /mnt/pivhere/ &
-sudo mount -t auto acer01:/home /mnt/pi4/ &
-sudo mount -t auto acer01:/home /mnt/pilamp/ &
-sudo mount -t auto acer01:/home /mnt/spelen02/ &
+sudo mount -t auto pi0:/home /mnt/pi0/ &
+sudo mount -t auto pivhere:/home /mnt/pivhere/ &
+sudo mount -t auto pi4:/home /mnt/pi4/ &
+sudo mount -t auto pilamp:/home /mnt/pilamp/ &
+sudo mount -t auto spelen02:/home /mnt/spelen02/ &
 
 sudo printf "\n HANDMATIG toevoegen aan /etc/exports/  : \n/mnt/nfsshare *(rw,all_squash,insecure,async,no_subtree_check,anonuid=1000,anongid=1000)\n"
 printf "\nNFS bestanddeling is daarna bereikbaar:\n -- MacOS verbind aan nfs://$_hn1.local/nfsshare  of nfs://$_ip1/nfsshare \n -- Windows verbind aan //$ip1.local/nfsshare\n\nIP adres $_ip1\n"
