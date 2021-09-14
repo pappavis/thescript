79a8ba7a123ae756853c4906437c5cd56fc9d608
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-14 19:21:33 +0200

    Update installMariaDB.sh

--- installMariaDB.sh
+++ installMariaDB.sh
@@ -1,15 +1,16 @@
 echo 'Loop nu reinstall script voor alle versies van MariaDB SQL Server'
 echo 'Verwijderen MariaDB Server Software'
-sudo apt --remove purge mariadb-server* -y
+ehco '#sudo apt --remove purge mariadb-server* -y' 
 echo 'Verwijder vorige MariaDB Server instalalties....'
-sudo apt -y autoremove 
-sudo apt autoclean
+sudo apt autoremove -y
+sudo apt autoclean -y
 sudo apt clean all 
 sudo rm -rf /etc/mysql/ /var/lib/mysql/ /var/log/mysql
 sudo apt clean 
 sudo apt install mariadb-server mariadb-client   -y
-echo 'Installeer MariaDB Server 10.3 overnieuw...'
+echo 'Installeer MariaDB Server overnieuw...'
 sudo apt update -y
-sudo debconf-set-selections <<< 'mariadb-server-10.3 mysql-server/root_password password rider506'
-sudo debconf-set-selections <<< 'mariadb-server-10.3 mysql-server/root_password_again password rider506'
+printf "doen\n sudo mysql_secure installation\n"
+sudo debconf-set-selections <<< 'mariadb-server mysql-server/root_password password rider506'
+sudo debconf-set-selections <<< 'mariadb-server mysql-server/root_password_again password rider506'
 echo 'MariaDB SQL Server herinstallatie afgerond'
