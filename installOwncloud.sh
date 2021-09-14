42bca0cbbd7bbb08b0af799313cac47decacacc3
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-14 21:24:34 +0200

    Update installOwncloud.sh

--- installOwncloud.sh
+++ installOwncloud.sh
@@ -1,12 +1,13 @@
 _hn1=$(hostname)
 echo "* Instaleer Owncloud"
 cd ~/Downloads
+sudo apt update -y
 sudo apt install -y apache2 php php-mysql php-intl
 sudo phpenmod  intl
 wget https://download.owncloud.org/community/owncloud-complete-20210721.zip
-7z x ./owncloud-complete-20210721.zip
+7z x ~/Downloads/owncloud-complete-20210721.zip
 sudo mkdir /var/html/support
-sudo mv ./owncloud /var/html/support
+sudo mv ~/Downloads/owncloud /var/www/html/support
 sudo chown -R www-data:www-data /var/www/html/support/owncloud
 sudo service apache2 restart
 echo "Owncloud beschikbaar op http://$_hn1.local/support/owncloud"
