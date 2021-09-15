c01fb87984b8e2b53bb396ad0a7824637daee58c
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-16 00:34:52 +0200

    Update installPHPliteadmin.sh

--- installPHPliteadmin.sh
+++ installPHPliteadmin.sh
@@ -1,14 +1,23 @@
-echo "* Installeer phpliteadmin voor sqlite."
-sudo apt install -y apache2 php php-mysql php-sqlite3
-mkdir ~/dbs
-mkdir ~/Downloads/phpliteadmin
-wget https://bitbucket.org/phpliteadmin/public/downloads/phpLiteAdmin_v1-9-8-2.zip
-cd ~/Downloads/phpliteadmin 
-7z x ~/Downloads/phpLiteAdmin_v1-9-8-2.zip
-cp -r -v phpliteadmin.config.sample.php phpliteadmin.config.php 
-sudo mkdir /var/www/html/support
-sudo mv ~/Downloads/phpliteadmin /var/www/html/support
-sudo chown www-data:www-data -R /var/www/html/support/phpliteadmin
-sudo apt install -y php-sqlite3 php-pdo-sqlite php-mbstring openssl
-echo "PHPliteadmin is geïnstaleerd voor http://$_hn1.local/phpalitedmin"
-sudo service apache2 restart
+        cd /var/www/html
+        sudo mkdir phpliteadmin
+        cd phpliteadmin
+        #sudo wget --no-check-certificate http://bitbucket.org/phpliteadmin/public/downloads/phpLiteAdmin_v1-9-7-1.zip -a $LOGFILE
+        sudo wget --no-check-certificate https://www.phpliteadmin.org/phpliteadmin-dev.zip -a $LOGFILE
+        if [ $? -eq 0 ]; then
+            sudo unzip phpliteadmin-dev.zip 2>&1 | tee -a $LOGFILE
+            sudo mv phpliteadmin.php index.php
+            sudo mv phpliteadmin.config.sample.php phpliteadmin.config.php
+            sudo rm *.zip
+            sudo mkdir themes
+            cd themes
+            sudo wget --no-check-certificate http://bitbucket.org/phpliteadmin/public/downloads/phpliteadmin_themes_2016-02-29.zip -a $LOGFILE
+            sudo unzip phpliteadmin_themes_2016-02-29.zip 2>&1 | tee -a $LOGFILE
+            sudo rm *.zip
+            sudo sed -i -e 's#\$directory = \x27.\x27;#\$directory = \x27/home/pi/dbs/\x27;#g' /var/www/html/phpliteadmin/phpliteadmin.config.php
+            sudo sed -i -e "s#\$password = \x27admin\x27;#\$password = \x27$adminpass\x27;#g" /var/www/html/phpliteadmin/phpliteadmin.config.php
+            sudo sed -i -e "s#\$subdirectories = false;#\$subdirectories = true;#g" /var/www/html/phpliteadmin/phpliteadmin.config.php
+            cd
+        else
+            printl "${IRed}!!!! PHPLITEADMIN not installed! ${IWhite}\r\n"
+            cd ; rm -rf /var/www/html/phpliteadmin
+        fi
