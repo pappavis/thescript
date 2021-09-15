6e20650f5d4f664377cc8b81c4185285d0bb158b
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-09-15 23:52:55 +0200

    Update installNodered.sh

--- installNodered.sh
+++ installNodered.sh
@@ -219,7 +219,7 @@ if [[ $MYMENU == *"nodenew"* ]]; then
 	
 	##  bash <(curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered)
 
-    bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+    #bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
  
    	##  curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered > update-nodejs-and-nodered
     echo 143.204.15.127 deb.nodesource.com | sudo tee -a /etc/hosts
