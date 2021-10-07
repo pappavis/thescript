2b2ded17b19597fce674b82bb21ad85f4a7f4bbe
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-10-07 10:19:18 +0200

    Update adduserPi.sh

--- adduserPi.sh
+++ adduserPi.sh
@@ -1,11 +1,13 @@
+LOGFILE=~/logbestand1.txt
+echo ""  >> $LOGFILE
 echo "* Nieuwe gebruiker wordt aangemaakt: michiele"
-adduser --quiet --disabled-password --shell /bin/bash --home /home/michiele --gecos "User" michiele
-echo "michiele:raspberry" | chpasswd
-usermod michiele -g sudo -G ssh -a
-echo "pi ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/michiele
-chmod 0440 /etc/sudoers.d/michiele
-chmod 4755 /usr/bin/sudo
+sudo adduser --quiet --disabled-password --shell /bin/bash --home /home/michiele --gecos "User" michiele
+echo "michiele:raspberry" | sudo chpasswd
+sudo usermod michiele -g sudo -G ssh -a
+sudo echo "pi ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/michiele
+sudo chmod 0440 /etc/sudoers.d/michiele
+sudo chmod 4755 /usr/bin/sudo
 printf "${ICyan}gebruiker PI aangemaakt, wachtwoord is \"raspberry\". Afmelden en opnieuw aanmelden indien gewenst ${IWhite}\r\n\r\n"
-cp $(readlink -f $0) /home/raspberry && chown michiele.michiele /home/michiele/$0 && chmod 755 /home/michiele/$0
-chown michiele.michiele $LOGFILE && chmod 644 $LOGFILE && cp $LOGFILE /home/michiele
+sudo cp $(readlink -f $0) /home/michiele && chown michiele.michiele /home/michiele/$0 && chmod 755 /home/michiele/$0
+sudo chown michiele.michiele $LOGFILE && sudochmod 644 $LOGFILE && sudocp $LOGFILE /home/michiele
 echo ""
