88dce990c4b095960c873781f8eb588787dc6f2f
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-10-09 15:57:06 +0200

    Update runmeFirst.sh

--- runmeFirst.sh
+++ runmeFirst.sh
@@ -40,6 +40,7 @@ sudo apt-get install -y nodejs npm tightvncserver pure-ftpd neofetch
 sudo apt install -y wiringpi 
 sudo apt install -y rpi.gpio
 sudo apt-get install -y apache2 php php-mysql php-sqlite3 php-mbstring openssl libapache2-mod-php php-sqlite3 php-xml php-mbstring
+sudo apt-get install -y samba samba-common-bin
 
 sudo apt install -y python3-pip
 
