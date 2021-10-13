a2a1532f28b932169c646bdd0f84094018c947b2
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-10-13 10:39:33 +0200

    Update installPythonUpgrade.sh

--- installPythonUpgrade.sh
+++ installPythonUpgrade.sh
@@ -9,4 +9,9 @@ cd cpython-3.11.0a1/
 ./configure --prefix=$HOME/.local --enable-optimizations
 make -j -l 4
 make install
-export PATH=$HOME/.local/bin/:$PATH
+~/.local/bin/python3.11 -m pip install --upgrade virtualenv
+mkdir ~/venv
+~/.local/bin/python3.11 -m virtualenv ~/venv/venv3.11
+source ~/venv/venv3.11/bin/activate
+pythonV=$(python -V)
+printf "* $pythonV upgrade afgerond\n"
