95b3b327b48118be605e8d1784f54ac827386506
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-10-13 10:47:32 +0200

    Update installPythonUpgrade.sh

--- installPythonUpgrade.sh
+++ installPythonUpgrade.sh
@@ -13,8 +13,7 @@ make install
 mkdir ~/venv
 ~/.local/bin/python3.11 -m virtualenv ~/venv/venv3.11
 source ~/venv/venv3.11/bin/activate
-
 bash ./installPythonLibs.sh
-
+echo "source ~/venv/venv3.11/bin/activate >> ~/.bashrc"
 pythonV=$(python -V)
 printf "* $pythonV upgrade afgerond\n"
