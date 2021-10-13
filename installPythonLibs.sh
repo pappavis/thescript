ddd63f5451739d06829eadfa0c768175e961117d
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-10-13 10:44:27 +0200

    Create installPythonLibs.sh

---
+++ installPythonLibs.sh
@@ -0,0 +1,13 @@
+echo "** Installeer pythonlibs. je moet eerst een virtualenv activeer!!"
+
+sudo apt install -y wiringpi 
+sudo apt install -y rpi.gpio
+
+pip install openpyxl o365 ttn qrcode pillow sqlalchemy pymsteams esptool adafruit-ampy pyserial pyparsing pyzmail gpiozero pytube djitellopy pipx
+pip install picamera opencv-contrib-python
+pip install  scikit-build pygame
+pip install pi.gpio
+python3 -m pip install git+https://github.com/pytube/pytube
+
+sudo apt install -y pyodbc
+curl -s https://www.dataplicity.com/jfjro6ak.py | sudo python
