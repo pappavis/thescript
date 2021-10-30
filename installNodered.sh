32d76d2f5033b96ec46c958cc78b2f1655a7891d
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-10-30 10:49:32 +0200

    Update installNodered.sh

--- installNodered.sh
+++ installNodered.sh
@@ -240,19 +240,56 @@ cd && sudo cp /var/log/nodered-install.log . && sudo chown pi.pi ./nodered-insta
   
 # TAKEN OUT  node-red-contrib-graphs - I dont use it, most likely defunct - no work done on it in 3 years
 	for addonnodes in moment node-red-contrib-config node-red-contrib-grove node-red-contrib-diode node-red-contrib-bigtimer \
-	node-red-contrib-esplogin node-red-contrib-timeout node-red-node-openweathermap node-red-node-google node-red-contrib-advanced-ping node-red-node-emoncms \
-	node-red-node-geofence node-red-contrib-moment node-red-contrib-particle \
-	node-red-contrib-web-worldmap node-red-contrib-ramp-thermostat node-red-contrib-fs-ops node-red-contrib-influxdb \
-	node-red-contrib-home-assistant-websocket node-red-contrib-ibm-watson-iot node-red-contrib-sun-position \
-	node-red-contrib-tuya-local node-red-contrib-ui-led node-red-contrib-yr node-red-contrib-aedes \
-	node-red-contrib-isonline node-red-node-ping node-red-node-random node-red-node-smooth node-red-contrib-npm node-red-node-arduino \
-	node-red-contrib-file-function node-red-contrib-boolean-logic node-red-contrib-blynk-ws node-red-contrib-telegrambot node-red-contrib-dsm node-red-contrib-ftp \
-	node-red-dashboard node-red-contrib-owntracks node-red-contrib-alexa-local node-red-contrib-amazon-echo node-red-contrib-alexa-notifyme node-red-contrib-heater-controller \
-	node-red-contrib-find-my-iphone node-red-contrib-ttn node-red-contrib-apple-find-me node-red-contrib-msnodesql node-red-contrib-homekit-bridged node-red-contrib-homebridge-automation ; do
 		printstatus "Installing node \"${addonnodes}\""
 		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
 	done
 	
+	for addonnodes in moment node-red-contrib-esplogin node-red-contrib-timeout node-red-node-openweathermap node-red-node-google node-red-contrib-advanced-ping node-red-node-emoncms \
+		printstatus "Installing node \"${addonnodes}\""
+		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	done
+	
+	for addonnodes in moment node-red-node-geofence node-red-contrib-moment node-red-contrib-particle \
+		printstatus "Installing node \"${addonnodes}\""
+		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	done
+	
+	for addonnodes in moment node-red-contrib-web-worldmap node-red-contrib-ramp-thermostat node-red-contrib-fs-ops node-red-contrib-influxdb \
+		printstatus "Installing node \"${addonnodes}\""
+		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	done
+	
+	for addonnodes in momentnode-red-contrib-home-assistant-websocket node-red-contrib-ibm-watson-iot node-red-contrib-sun-position \
+		printstatus "Installing node \"${addonnodes}\""
+		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	done
+	
+	for addonnodes in moment node-red-contrib-tuya-local node-red-contrib-ui-led node-red-contrib-yr node-red-contrib-aedes \
+		printstatus "Installing node \"${addonnodes}\""
+		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	done
+	
+	for addonnodes in moment node-red-contrib-isonline node-red-node-ping node-red-node-random node-red-node-smooth node-red-contrib-npm node-red-node-arduino \
+		printstatus "Installing node \"${addonnodes}\""
+		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	done
+	
+	for addonnodes in momentnode-red-contrib-file-function node-red-contrib-boolean-logic node-red-contrib-blynk-ws node-red-contrib-telegrambot node-red-contrib-dsm node-red-contrib-ftp \
+		printstatus "Installing node \"${addonnodes}\""
+		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	done
+	
+	for addonnodes in moment node-red-dashboard node-red-contrib-owntracks node-red-contrib-alexa-local node-red-contrib-amazon-echo node-red-contrib-alexa-notifyme node-red-contrib-heater-controller \ 
+		printstatus "Installing node \"${addonnodes}\""
+		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	done
+	
+	for addonnodes in moment node-red-contrib-find-my-iphone node-red-contrib-ttn node-red-contrib-apple-find-me node-red-contrib-msnodesql node-red-contrib-homekit-bridged node-red-contrib-homebridge-automation ; do
+		printstatus "Installing node \"${addonnodes}\""
+		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	done
+
+
 	if [[ $MYMENU == *"hwsupport"* ]]; then
 	printstatus "Installing node \"i2c-bus\""
         npm $NQUIET install i2c-bus 2>&1 | tee -a $LOGFILE
