c977a9084193782a0dbd2f2dcbb51cc63433b082
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-10-30 11:04:17 +0200

    Add files via upload

---
+++ index_apps.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>Scargill's Default Web Page</title>
+        <meta name="description" content="Peter Scargill's default web page installed by THE SCRIPT">
+        <meta name="keywords" content="scargill,the script, https://tech.scargill.net">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes"/>
+        <link rel="stylesheet" type="text/css" href="reset.css" media="all">
+<style>
+.bighead {
+  background-color: #2094f3;
+  color: white;
+  font-size: 250%;
+  margin: 20px;
+  padding: 20px;
+}
+.padded{
+
+  padding-top: 16px; 
+}
+
+A {text-decoration: none;
+   color: #2094f3;
+}
+
+body {
+background-color: #ffffff;
+}
+
+p, .author {
+	color: #888888;
+}
+
+</style>
+    </head>
+
+<body>
+
+<div align="center">
+  <article>
+    <h1 class="bighead">"The Script" Home<br/></h1>
+    <p class="author">by PETER SCARGILL<br/><br/></p>
+    <p>
+</div>
+<div align="center"  class="padded">
+	<script language="JavaScript">
+     document.write('<a target="_blank" href="' + window.location.protocol + '//' + window.location.hostname + ':10000' +  '" >Webmin Adminstrator - if installed</a><br/>' );
+     document.write('<a target="_blank" href="' + window.location.protocol + '//' + window.location.hostname + ':1880' +  '" >Node Red Control Panel</a><br/>' );
+     document.write('<a target="_blank" href="' + window.location.protocol + '//' + window.location.hostname + ':1880' +  '/ui" >Node Red UI Desktop</a><br/>' );
+     document.write('<a target="_blank" href="' + window.location.protocol + '//' + window.location.hostname + ':3000' +  '" >Grafana - if installed</a><br/>' );
+	 document.write('<a target="_blank" href="' + window.location.protocol + '//' + window.location.hostname + ':8888' +  '" >Chronograf - if installed</a><br/>' );
+	 </script>
+	<a target="_blank" href="/phpliteadmin">SQLite Administrator</a><br/>
+	<a target="_blank" href="/phpsysinfo">System Information</a><br/>
+	<a target="_blank" href="https://bitbucket.org/scargill/esp-go/raw/517278aa44ae184d37a1e331e419b9f9c40f71b9/Hackitt-and-Bodgitt-ESP.doc">Download the ESP-GO Doc File</a><br/>	
+	<a target="_blank" href="https://tech.scargill.net">tech.scargill.net blog</a><br/>
+	<a target="_blank" href="./support">PartDB</a><br/>
+	<a href="http://pi0.local:8123" target="_blank">Home assistant</a><br>
+	<a href="http://pi0.local" target="_blank">Octoprint</a> lokaal<br>
+	Apple <a href="http://pi0.local:8581" target="_blank">Homebridge </a> integratie
+	</p><br/>
+  </article>
+</div>
+<div align="center" class-"author">Several utilities, macros and functions are installed by default including various editors</div>
+</body>
+</html>
