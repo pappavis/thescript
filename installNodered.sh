bfb96a6c3047d4ac2159a2ba3a1d2d2dc30cac23
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-10-30 10:52:05 +0200

    Update installNodered.sh

--- installNodered.sh
+++ installNodered.sh
@@ -239,47 +239,47 @@ cd && sudo cp /var/log/nodered-install.log . && sudo chown pi.pi ./nodered-insta
   
   
 # TAKEN OUT  node-red-contrib-graphs - I dont use it, most likely defunct - no work done on it in 3 years
-	for addonnodes in moment node-red-contrib-config node-red-contrib-grove node-red-contrib-diode node-red-contrib-bigtimer \
+	for addonnodes in moment node-red-contrib-config node-red-contrib-grove node-red-contrib-diode node-red-contrib-bigtimer ; do
 		printstatus "Installing node \"${addonnodes}\""
 		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
 	done
 	
-	for addonnodes in moment node-red-contrib-esplogin node-red-contrib-timeout node-red-node-openweathermap node-red-node-google node-red-contrib-advanced-ping node-red-node-emoncms \
+	for addonnodes in moment node-red-contrib-esplogin node-red-contrib-timeout node-red-node-openweathermap node-red-node-google node-red-contrib-advanced-ping node-red-node-emoncms ; do
 		printstatus "Installing node \"${addonnodes}\""
 		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
 	done
 	
-	for addonnodes in moment node-red-node-geofence node-red-contrib-moment node-red-contrib-particle \
+	for addonnodes in moment node-red-node-geofence node-red-contrib-moment node-red-contrib-particle ; do
 		printstatus "Installing node \"${addonnodes}\""
 		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
 	done
 	
-	for addonnodes in moment node-red-contrib-web-worldmap node-red-contrib-ramp-thermostat node-red-contrib-fs-ops node-red-contrib-influxdb \
+	for addonnodes in moment node-red-contrib-web-worldmap node-red-contrib-ramp-thermostat node-red-contrib-fs-ops node-red-contrib-influxdb ; do
 		printstatus "Installing node \"${addonnodes}\""
 		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
 	done
 	
-	for addonnodes in momentnode-red-contrib-home-assistant-websocket node-red-contrib-ibm-watson-iot node-red-contrib-sun-position \
+	for addonnodes in momentnode-red-contrib-home-assistant-websocket node-red-contrib-ibm-watson-iot node-red-contrib-sun-position ; do
 		printstatus "Installing node \"${addonnodes}\""
 		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
 	done
 	
-	for addonnodes in moment node-red-contrib-tuya-local node-red-contrib-ui-led node-red-contrib-yr node-red-contrib-aedes \
+	for addonnodes in moment node-red-contrib-tuya-local node-red-contrib-ui-led node-red-contrib-yr node-red-contrib-aedes ; do
 		printstatus "Installing node \"${addonnodes}\""
 		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
 	done
 	
-	for addonnodes in moment node-red-contrib-isonline node-red-node-ping node-red-node-random node-red-node-smooth node-red-contrib-npm node-red-node-arduino \
+	for addonnodes in moment node-red-contrib-isonline node-red-node-ping node-red-node-random node-red-node-smooth node-red-contrib-npm node-red-node-arduino ; do
 		printstatus "Installing node \"${addonnodes}\""
 		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
 	done
 	
-	for addonnodes in momentnode-red-contrib-file-function node-red-contrib-boolean-logic node-red-contrib-blynk-ws node-red-contrib-telegrambot node-red-contrib-dsm node-red-contrib-ftp \
+	for addonnodes in momentnode-red-contrib-file-function node-red-contrib-boolean-logic node-red-contrib-blynk-ws node-red-contrib-telegrambot node-red-contrib-dsm node-red-contrib-ftp ; do
 		printstatus "Installing node \"${addonnodes}\""
 		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
 	done
 	
-	for addonnodes in moment node-red-dashboard node-red-contrib-owntracks node-red-contrib-alexa-local node-red-contrib-amazon-echo node-red-contrib-alexa-notifyme node-red-contrib-heater-controller \ 
+	for addonnodes in moment node-red-dashboard node-red-contrib-owntracks node-red-contrib-alexa-local node-red-contrib-amazon-echo node-red-contrib-alexa-notifyme node-red-contrib-heater-controller ; do 
 		printstatus "Installing node \"${addonnodes}\""
 		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
 	done
