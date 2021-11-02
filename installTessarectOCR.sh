8dc628ff9f846691deb1fc468959b8e2e7a2f85e
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-11-02 10:33:50 +0100

    Update and rename installTessarectOCT.sh to installTessarectOCR.sh

---
+++ installTessarectOCR.sh
@@ -0,0 +1,12 @@
+sudo apt install -y tesseract-ocr libtesseract-dev libatlas3-base libqtcore4 -y
+tesseract --version
+sudo apt install -y imagemagick 
+sudo apt install -y libfontconfig1-dev libcairo2-dev
+sudo apt install -y libgdk-pixbuf2.0-dev libpango1.0-dev
+sudo apt install -y libgtk2.0-dev libgtk-3-dev
+sudo apt install -y libatlas-base-dev gfortran
+
+source ~/venv/venv3.7/bin/activate
+pip install --upgrade pytesseract 
+pip install --upgrade opencv-contrib-python 
+pip install --upgrade libwebp6
--- installTessarectOCT.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install -y tesseract-ocr libtesseract-dev libatlas3-base libqtcore4 -y
-tesseract --version
-sudo apt install -y imagemagick
-source ~/venv/venv3.7/bin/activate
-pip3 install pytesseract opencv-contrib-python 
-pip3 install libwebp6
