6dcfaacf04dd8a10774376779036e1ea42e92a04
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-11-03 10:13:40 +0100

    Create installOpenCV.sh

---
+++ installOpenCV.sh
@@ -0,0 +1,70 @@
+echo "Installeer OpenCV Python"
+echo "sudo apt-get -y remove x264 libx264-dev"
+
+echo "## Install dependencies"
+source ~/venv/venv3.7/bin/activate
+sudo apt-get -y install build-essential checkinstall cmake pkg-config yasm
+sudo apt-get -y install git gfortran
+sudo apt-get -y install libjpeg8-dev libjasper-dev libpng12-dev
+
+sudo apt-get -y install libtiff5-dev
+
+sudo apt-get -y install libtiff-dev
+
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
+sudo apt-get -y install libxine2-dev libv4l-dev
+cd /usr/include/linux
+sudo ln -s -f ../libv4l1-videodev.h videodev.h
+cd $cwd
+
+sudo apt-get -y install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+sudo apt-get -y install libgtk2.0-dev libtbb-dev qt5-default
+sudo apt-get -y install libatlas-base-dev
+sudo apt-get -y install libmp3lame-dev libtheora-dev
+sudo apt-get -y install libvorbis-dev libxvidcore-dev libx264-dev
+sudo apt-get -y install libopencore-amrnb-dev libopencore-amrwb-dev
+sudo apt-get -y install libavresample-dev
+sudo apt-get -y install x264 v4l-utils
+
+echo " Opencv Optional dependencies"
+sudo apt-get -y install libprotobuf-dev protobuf-compiler
+sudo apt-get -y install libgoogle-glog-dev libgflags-dev
+sudo apt-get -y install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+
+sudo apt-get -y install python3-testresources
+pip install --upgrade pip numpy dlib
+
+
+
+echo " Download opencv and opencv_contrib"
+cd ~/Downloads
+git clone https://github.com/opencv/opencv.git
+cd opencv
+git checkout 3.4
+cd ..
+
+git clone https://github.com/opencv/opencv_contrib.git
+cd opencv_contrib
+git checkout 3.4
+cd .
+
+cd opencv
+mkdir build
+cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+            -D CMAKE_INSTALL_PREFIX=$cwd/installation/OpenCV-"$cvVersion" \
+            -D INSTALL_C_EXAMPLES=ON \
+            -D INSTALL_PYTHON_EXAMPLES=ON \
+            -D WITH_TBB=ON \
+            -D WITH_V4L=ON \
+            -D OPENCV_PYTHON3_INSTALL_PATH=$cwd/OpenCV-$cvVersion-py3/lib/python3.5/site-packages \
+        -D WITH_QT=ON \
+        -D WITH_OPENGL=ON \
+        -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+        -D BUILD_EXAMPLES=ON ..
+        
+make -j$(nproc)
+make install
+
+echo " OpenCV build van bronkode afgerond."
