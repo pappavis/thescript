ad7a3e21b1c7092aa3c61fe15aa6b6bef0caf888
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-11-10 18:04:47 +0100

    Update installExtras.sh

--- installExtras.sh
+++ installExtras.sh
@@ -61,6 +61,7 @@ sudo mkdir /home/pi/.local/share/lxterminal
 
 echo "** installeer barrier KVM."
 sudo apt install -y remmina barrier thonny kodi chromium
+sudo apt install -y libsdl2-ttf-dev libsdl2-image-dev amiberry
 
 sudo adduser xrdp ssl-cert 
 systemctl show -p SubState --value xrdp
