39a5b090b77e65c675e403e61ac468c4b6fe2767
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-11-10 16:32:31 +0100

    Update installNodeJS.sh

--- installNodeJS.sh
+++ installNodeJS.sh
@@ -6,17 +6,9 @@ NQUIET=""
 
 
 echo "* Node-red bijwerken"
-#bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
+echo "Y\n" | bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 
 sudo apt-get install -y  yarn
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
 echo "NodeJS bijgewerkt.  NodeJS versie: $(node -v), npm $(npm -v)"
-sudo npm install -g node-red --unsafe-perm
-rm -rf ~/.node-red/node_modules
-mkdir ~/.node-red
-cd ~/.node-red
-npm install node-red-contrib-sqlitedb node-red-contrib-ttn geofence
-npm install --unsafe-perm node-red-node-sqlite
+npm install qrcode johnny-five
 
-sudo systemctl enable nodered.service
