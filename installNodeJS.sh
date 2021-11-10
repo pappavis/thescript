bae0165444b2c4a2b973cba1806a5f003335035b
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-11-10 16:38:50 +0100

    Update installNodeJS.sh

--- installNodeJS.sh
+++ installNodeJS.sh
@@ -1,11 +1,11 @@
 MYMENU=""
-LOGFILE="/home/pi/logNoderedinstall.log"
+LOGFILE="/home/pi/logNodeJSinstall.log"
 adminpass=""
 userpass=""
 NQUIET=""
 
 
-echo "* Node-red bijwerken"
+echo "* Node-JS installeren"
 echo "Y\n" | bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 
 sudo apt-get install -y  yarn
