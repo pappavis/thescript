3d424fa7cd53e494e7dd8bd560693f818e70013c
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-11-10 13:09:57 +0100

    Update installHomeassistant.sh

--- installHomeassistant.sh
+++ installHomeassistant.sh
@@ -6,7 +6,11 @@ pip3 install homeassistant
 sudo echo "source /home/pi/venv/homeassistant/bin/activate" >> /usr/local/bin/homeassistant
 sudo chmod +x /usr/local/bin/homeassistant
 sudo echo "/home/pi/venv/homeassistant/bin/hass" >> /usr/local/bin/homeassistant
-pip install --upgrade pip homebridge
+recho "pip install --upgrade pip homebridge"
+sudo systemctl enable homeassistant.service
+sudo service homeassistant restart
+
 sudo echo "/home/pi/venv/homeassistant/bin/hass" >> /usr/local/bin/homeassistant
 sudo echo "source /home/pi/venv/venv3.7/bin/activate"  >> /usr/local/bin/homeassistant
+
 printf "\nStart homeassitant op http://$_hn.local:8123\n"
