2ae7e49670051935bd4f01bfb951a1eec7c5c48c
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-11-24 15:55:50 +0100

    Update installTessarectOCR.sh

--- installTessarectOCR.sh
+++ installTessarectOCR.sh
@@ -1,15 +1,15 @@
-ehco "( Installeren Tessarect OCR"
+ehco "* Installeren Tessarect OCR"
+sudo apt update -y
 sudo apt install -y tesseract-ocr 
 sudo apt install -y libtesseract-dev libatlas3-base
 sudo apt update -y libqtcore
-_tv=$(tesseract --version)
-sudo apt update -y
 sudo apt install -y imagemagick 
 sudo apt install -y libfontconfig1-dev libcairo2-dev
 sudo apt install -y libgdk-pixbuf2.0-dev libpango1.0-dev
 sudo apt install -y libgtk2.0-dev libgtk-3-dev
 sudo apt install -y libatlas-base-dev gfortran python3-opencv
 sudo apt install -y python3-opencv
+_tv=$(tesseract --version)
 
 python ./demo/opencv_pip_fix.py
 
