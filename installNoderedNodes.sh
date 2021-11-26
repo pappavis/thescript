4883eec9c88c54563988c6a8ff4afb7e63d2e78f
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-11-26 10:24:31 +0100

    Update installNoderedNodes.sh

--- installNoderedNodes.sh
+++ installNoderedNodes.sh
@@ -14,64 +14,91 @@ printstatus() {
 	echo -e $1 >> $LOGFILE
 }
 
-echo "Installing Nodes (could take some time)"
-	cd ~/.node-red
-	printstatus "\nInstalling node \"node-red-node-sqlite\"\n"
-	npm $NQUIET install node-red-node-sqlite 2>&1 | tee -a $LOGFILE
-  
-  
-# TAKEN OUT  node-red-contrib-graphs - I dont use it, most likely defunct - no work done on it in 3 years
-	for addonnodes in moment node-red-contrib-config node-red-contrib-grove node-red-contrib-diode node-red-contrib-bigtimer ; do
-		printstatus "Installing node \"${addonnodes}\""
-		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
-	done
-	
-	for addonnodes in moment node-red-contrib-esplogin node-red-contrib-timeout node-red-node-openweathermap node-red-node-google node-red-contrib-advanced-ping node-red-node-emoncms ; do
-		printstatus "Installing node \"${addonnodes}\""
-		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
-	done
-	
-	for addonnodes in moment node-red-node-geofence node-red-contrib-moment node-red-contrib-particle ; do
-		printstatus "Installing node \"${addonnodes}\""
-		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
-	done
-	
-	for addonnodes in moment node-red-contrib-web-worldmap node-red-contrib-ramp-thermostat node-red-contrib-fs-ops node-red-contrib-influxdb ; do
-		printstatus "Installing node \"${addonnodes}\""
-		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
-	done
-	
-	for addonnodes in moment node-red-contrib-home-assistant-websocket node-red-contrib-ibm-watson-iot node-red-contrib-sun-position ; do
-		printstatus "Installing node \"${addonnodes}\""
-		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
-	done
-	
-	for addonnodes in moment node-red-contrib-tuya-local node-red-contrib-ui-led node-red-contrib-yr node-red-contrib-aedes ; do
-		printstatus "Installing node \"${addonnodes}\""
-		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
-	done
-	
-	for addonnodes in moment node-red-contrib-isonline node-red-node-ping node-red-node-random node-red-node-smooth node-red-contrib-npm node-red-node-arduino ; do
-		printstatus "Installing node \"${addonnodes}\""
-		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
-	done
-	
-	for addonnodes in moment node-red-contrib-file-function node-red-contrib-boolean-logic node-red-contrib-blynk-ws node-red-contrib-telegrambot node-red-contrib-dsm node-red-contrib-ftp ; do
-		printstatus "Installing node \"${addonnodes}\""
-		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
-	done
-	
-	for addonnodes in moment node-red-dashboard node-red-contrib-owntracks node-red-contrib-alexa-local node-red-contrib-amazon-echo node-red-contrib-alexa-notifyme node-red-contrib-heater-controller ; do 
-		printstatus "Installing node \"${addonnodes}\""
-		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
-	done
-	
-	for addonnodes in moment node-red-contrib-find-my-iphone node-red-contrib-ttn node-red-contrib-apple-find-me node-red-contrib-msnodesql node-red-contrib-homekit-bridged node-red-contrib-homebridge-automation ; do
-		printstatus "Installing node \"${addonnodes}\""
-		npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
-	done
+echo "**Installing Nodes (could take some time)"
+cd /home/pi/.node-red
 
-	sudo service nodered restart
+printstatus "bepalen laatste versies van globale NPM packages"
+npm outdated -g --depth=0
+sudo npm update -g
+for addonnodes in npm@latest
+	printstatus "Bijwerken globale node \"${addonnodes}\""
+	sudo npm $NQUIET update -g ${addonnodes} 2>&1 | tee -a $LOGFILE
+done
+
+npm outdated
+npm uninstall --save node-uuid 2>&1 | tee -a $LOGFILE
+npm install --save uuid@latest 2>&1 | tee -a $LOGFILE
+npm uninstall --save node-tar 2>&1 | tee -a $LOGFILE
+npm install node-tar@latest 2>&1 | tee -a $LOGFILE
+npm install --save tar@latest 2>&1 | tee -a $LOGFILE
+npm uninstall --save uuid 2>&1 | tee -a $LOGFILE
+npm upgrade uuid@latest 2>&1 | tee -a $LOGFILE
+npm install -g npm-check-updates 2>&1 | tee -a $LOGFILE
+ncu -u 2>&1 | tee -a $LOGFILE
+npm update 2>&1 | tee -a $LOGFILE
+
+printstatus "\nInstalling node \"node-red-node-sqlite\"\n"
+npm $NQUIET install node-red-node-sqlite 2>&1 | tee -a $LOGFILE
+npm audit fix --force
+
+		
+# TAKEN OUT  node-red-contrib-graphs - I dont use it, most likely defunct - no work done on it in 3 years
+for addonnodes in moment node-red-contrib-config node-red-contrib-grove node-red-contrib-diode node-red-contrib-bigtimer ; do
+	printstatus "Installing node \"${addonnodes}\""
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+done
+
+for addonnodes in moment node-red-contrib-esplogin node-red-contrib-timeout node-red-node-openweathermap node-red-node-google node-red-contrib-advanced-ping node-red-node-emoncms ; do
+	printstatus "Installing node \"${addonnodes}\""
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+done
+
+for addonnodes in moment node-red-node-geofence node-red-contrib-moment node-red-contrib-particle ; do
+	printstatus "Installing node \"${addonnodes}\""
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+done
+
+for addonnodes in moment node-red-contrib-web-worldmap node-red-contrib-ramp-thermostat node-red-contrib-fs-ops node-red-contrib-influxdb ; do
+	printstatus "Installing node \"${addonnodes}\""
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+done
+
+for addonnodes in moment node-red-contrib-home-assistant-websocket node-red-contrib-ibm-watson-iot node-red-contrib-sun-position ; do
+	printstatus "Installing node \"${addonnodes}\""
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+done
+
+for addonnodes in moment node-red-contrib-tuya-local node-red-contrib-ui-led node-red-contrib-yr node-red-contrib-aedes ; do
+	printstatus "Installing node \"${addonnodes}\""
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+done
+
+for addonnodes in moment node-red-contrib-isonline node-red-node-ping node-red-node-random node-red-node-smooth node-red-contrib-npm node-red-node-arduino ; do
+	printstatus "Installing node \"${addonnodes}\""
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+done
+
+for addonnodes in moment node-red-contrib-file-function node-red-contrib-boolean-logic node-red-contrib-blynk-ws node-red-contrib-telegrambot node-red-contrib-dsm node-red-contrib-ftp ; do
+	printstatus "Installing node \"${addonnodes}\""
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+done
+
+for addonnodes in moment node-red-dashboard node-red-contrib-owntracks node-red-contrib-alexa-local node-red-contrib-amazon-echo node-red-contrib-alexa-notifyme node-red-contrib-heater-controller ; do 
+	printstatus "Installing node \"${addonnodes}\""
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+done
+
+for addonnodes in moment node-red-contrib-find-my-iphone node-red-contrib-ttn node-red-contrib-apple-find-me node-red-contrib-msnodesql node-red-contrib-homekit-bridged node-red-contrib-homebridge-automation write-excel-file ; do 
+	printstatus "Installing node \"${addonnodes}\""
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+done
+
+npm audit fix --force
+
+printstatus "Bijwerken loakel nodes"
+npm $NQUIET update 2>&1 | tee -a $LOGFILE
+
+sudo service nodered restart
 echo "Nodes installatie afgerond"
 cd $_pwd
 
