3324c8e562b9eb9cbd2d9cdbe87fb5902f8de5d3
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-11-29 14:15:17 +0100

    Update installHomeassistant.sh

--- installHomeassistant.sh
+++ installHomeassistant.sh
@@ -1,16 +1,11 @@
 _hn="http://"$(hostname).local":8123"
 _pwd=$(pwd)
 echo "Installeren Homeassistant"
-virtualenv ~/venv/homeassistant
-source ~/venv/homeassistant/bin/activate
+virtualenv /home/pi/venv/homeassistant
+source /home/pi/venv/homeassistant/bin/activate
 pip install --upgrade pip
 pip install --upgrade homeassistant
 pip install --upgrade ffmpeg
-sudo echo "source /home/pi/venv/homeassistant/bin/activate" >> /usr/local/bin/homeassistant
-sudo chmod +x /usr/local/bin/homeassistant
-sudo echo "/home/pi/venv/homeassistant/bin/hass" >> /usr/local/bin/homeassistant
-echo "pip install --upgrade pip homebridge"
-sudo apt install -y ffmpeg
 
 mkdir ~/Downloads
 cd ~/Downloads
@@ -18,9 +13,6 @@ wget https://raw.githubusercontent.com/pappavis/thescript/master/homeassistant.s
 sudo mv ./homeassistant.service /etc/systemd/system
 sudo systemctl enable homeassistant.service
 sudo service homeassistant restart
-
-sudo echo "/home/pi/venv/homeassistant/bin/hass" >> /usr/local/bin/homeassistant
-sudo echo "source /home/pi/venv/venv3.7/bin/activate"  >> /usr/local/bin/homeassistant
 
 cd $_pwd
 
