f7ad25b2a41ced927bcb837e7a23f4607bdfd62d
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-12-01 11:19:38 +0100

    Update installNodered.sh

--- installNodered.sh
+++ installNodered.sh
@@ -120,6 +120,7 @@ cd linux-installers/pibuild
 bash ./node-red-pi-install.sh
 bash ./node-red-deb-pack.sh
 
+cd ~
 echo "y\n" | bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 ##node-red admin init
 npm audit fix
@@ -128,66 +129,66 @@ npm install johnny-five
 sudo apt-get install -y  yarn
 
 
-	## echo 143.204.15.127 deb.nodesource.com | sudo tee -a /etc/hosts
-	## curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered > update-nodejs-and-nodered
-	## chmod +x update-nodejs-and-nodered
-	## echo y | ./update-nodejs-and-nodered
+## echo 143.204.15.127 deb.nodesource.com | sudo tee -a /etc/hosts
+## curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered > update-nodejs-and-nodered
+## chmod +x update-nodejs-and-nodered
+## echo y | ./update-nodejs-and-nodered
 
-	##sudo npm install -g --unsafe-perm node-red
-	mkdir /home/pi/Downloads
-	 cd /home/pi/Downloads
-	 wget https://raw.githubusercontent.com/pappavis/thescript/master/settings.js
-	 wget https://raw.githubusercontent.com/pappavis/thescript/master/flows.json
-	 mv ./settings.js /home/pi/.node-red/
-	 mv ./flows.json /home/pi/.node-red/
-	sed -i -e "s#\/\/var i2c#var i2c#" /home/pi/.node-red/settings.js
-	sed -i -e "s#\/\/i2c#i2c#" /home/pi/.node-red/settings.js
+##sudo npm install -g --unsafe-perm node-red
+mkdir /home/pi/Downloads
+cd /home/pi/Downloads
+wget https://raw.githubusercontent.com/pappavis/thescript/master/settings.js
+wget https://raw.githubusercontent.com/pappavis/thescript/master/flows.json
+mv ./settings.js /home/pi/.node-red/
+mv ./flows.json /home/pi/.node-red/
+sed -i -e "s#\/\/var i2c#var i2c#" /home/pi/.node-red/settings.js
+sed -i -e "s#\/\/i2c#i2c#" /home/pi/.node-red/settings.js
 
-	mkdir ~/Downloads
-	cd ~/Downloads	
-	echo "Nodered service installeren"
-	wget https://raw.githubusercontent.com/pappavis/thescript/master/nodered.service
-	sudo mv ./nodered.service /etc/systemd/system
-	sudo systemctl enable nodered.service 2>&1 | tee -a $LOGFILE
+mkdir ~/Downloads
+cd ~/Downloads	
+echo "Nodered service installeren"
+wget https://raw.githubusercontent.com/pappavis/thescript/master/nodered.service
+sudo mv ./nodered.service /etc/systemd/system
+sudo systemctl enable nodered.service 2>&1 | tee -a $LOGFILE
 
-	echo "Nodered service starten"
-	sudo service nodered restart
+echo "Nodered service starten"
+sudo service nodered restart
 
-	cd && sudo cp /var/log/nodered-install.log . && sudo chown pi.pi ./nodered-install.log && cd ~/.node-red/
+cd && sudo cp /var/log/nodered-install.log . && sudo chown pi.pi ./nodered-install.log && cd ~/.node-red/
 
-	cd $_pwd
-	bash ./installNoderedNodes.sh
-	sudo service nodered restart
+cd $_pwd
+bash ./installNoderedNodes.sh
+sudo service nodered restart
 
-        npm $NQUIET install i2c-bus 2>&1 | tee -a $LOGFILE
-	printstatus "Installing node \"bcryptjs\""
-	sudo npm $NQUIET install bcryptjs 2>&1 | tee -a $LOGFILE
+npm $NQUIET install i2c-bus 2>&1 | tee -a $LOGFILE
+printstatus "Installing node \"bcryptjs\""
+sudo npm $NQUIET install bcryptjs 2>&1 | tee -a $LOGFILE
 
-	MYMENU="hwsupport"
+MYMENU="hwsupport"
 
-	if [[ $MYMENU == *"hwsupport"* ]]; then
-		## this last bit of code is to ensure that node-red-contrib-opi-gpio can gain access to port bits!!
-		getent group gpio || sudo groupadd gpio 2>&1 | tee -a $LOGFILE
-		getent group gpio | grep -w -l pi || sudo adduser pi gpio 2>&1 | tee -a $LOGFILE
-		[ ! -f /etc/udev/rules.d/99-com.rules ] && echo "KERNEL==\"gpio*\", RUN=\"/bin/sh -c 'chgrp -R gpio /sys/%p /sys/class/gpio && chmod -R g+w /sys/%p /sys/class/gpio'\"" | sudo tee -a /etc/udev/rules.d/99-com.rules > /dev/null
-		
-		if [[ $OPSYS == *"RASPBIAN"* ]]; then
-			sudo sed -i -e 's#exit 0#chmod 777 /dev/ttyAMA0\nexit 0#g' /etc/rc.local
-			sudo apt-get -y install python{,3}-rpi.gpio 2>&1 | tee -a $LOGFILE
-		else
-			if [[ $MYMENU == *"generich3"* ]]; then
-				npm $NQUIET install node-red-contrib-opi-gpio 2>&1 | tee -a $LOGFILE
-			fi
-		fi
-		sudo sed -i -e 's#exit 0#chmod 777 /dev/ttyS*\nexit 0#g' /etc/rc.local
+if [[ $MYMENU == *"hwsupport"* ]]; then
+## this last bit of code is to ensure that node-red-contrib-opi-gpio can gain access to port bits!!
+getent group gpio || sudo groupadd gpio 2>&1 | tee -a $LOGFILE
+getent group gpio | grep -w -l pi || sudo adduser pi gpio 2>&1 | tee -a $LOGFILE
+[ ! -f /etc/udev/rules.d/99-com.rules ] && echo "KERNEL==\"gpio*\", RUN=\"/bin/sh -c 'chgrp -R gpio /sys/%p /sys/class/gpio && chmod -R g+w /sys/%p /sys/class/gpio'\"" | sudo tee -a /etc/udev/rules.d/99-com.rules > /dev/null
+
+if [[ $OPSYS == *"RASPBIAN"* ]]; then
+	sudo sed -i -e 's#exit 0#chmod 777 /dev/ttyAMA0\nexit 0#g' /etc/rc.local
+	sudo apt-get -y install python{,3}-rpi.gpio 2>&1 | tee -a $LOGFILE
+else
+	if [[ $MYMENU == *"generich3"* ]]; then
+		npm $NQUIET install node-red-contrib-opi-gpio 2>&1 | tee -a $LOGFILE
 	fi
-            
-	cd ~/.node-red/
-    npm $NQUIET audit fix
-	sudo wget -a $LOGFILE $AQUIET https://tech.scargill.net/iot/settings.txt -O settings.js
+fi
+sudo sed -i -e 's#exit 0#chmod 777 /dev/ttyS*\nexit 0#g' /etc/rc.local
+fi
 
-    ## add a nice little command line utility (nrlog) for showing and tailing Node-Red scripts in colour
-	echo "alias nrlog='sudo journalctl -f -n 50 -u nodered -o cat | ccze -A'" | sudo tee -a /etc/bash.bashrc > /dev/null 2>&1
+cd ~/.node-red/
+npm $NQUIET audit fix
+sudo wget -a $LOGFILE $AQUIET https://tech.scargill.net/iot/settings.txt -O settings.js
+
+## add a nice little command line utility (nrlog) for showing and tailing Node-Red scripts in colour
+echo "alias nrlog='sudo journalctl -f -n 50 -u nodered -o cat | ccze -A'" | sudo tee -a /etc/bash.bashrc > /dev/null 2>&1
 
 cd ~/.node-red
 npm audit fix --force
