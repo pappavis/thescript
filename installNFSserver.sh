6df70acd734c8409f3a1a885b33d8462753527ba
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-12-09 14:35:24 +0100

    Update installNFSserver.sh

--- installNFSserver.sh
+++ installNFSserver.sh
@@ -1,6 +1,7 @@
 _id1=$(id pi)
 _hn1=$(hostname)
 _ip1=$(hostname -I)
+
 mkdir ~/tmp
 printf "\nStart instellen van NFS server op \n" $_ip1 ".local\n"
 sudo apt install -y nfs-kernel-server nfs-common
@@ -30,12 +31,13 @@ printf "\nProbeer netwerk share te mount op /mnt\n"
 sudo mount -t auto acer01:/home /mnt/nfs/acer01/ &
 sudo mount -t auto pi0:/home /mnt/nfs/pi0/ &
 sudo mount -t auto pivhere:/home /mnt/nfs/pivhere/ &
-sudo mount -t auto pi4:/home /mnt/nfs/pi4/ &
+sudo mount -t auto pi04:/home /mnt/nfs/pi4/ &
 sudo mount -t auto pilamp:/home /mnt/nfs/pilamp/ &
 sudo mount -t auto spelen02:/home /mnt/nfs/spelen02/ &
 sudo mount -t auto p1mon:/home /mnt/nfs/p1mon/ &
 sudo mount -t auto retropie:/home /mnt/nfs/retropie/ &
 sudo mount -t auto pi07:/home /mnt/nfs/pi07/ &
+sudo mount -t auto pi08:/home /mnt/nfs/pi08/ &
 
 sudo printf "\n HANDMATIG toevoegen aan /etc/exports/  : \n/home *(rw,all_squash,insecure,async,no_subtree_check)\n"
 printf "\nNFS bestanddeling is daarna bereikbaar:\n -- MacOS verbind aan nfs://$_hn1.local/nfsshare  of nfs://$_ip1/nfsshare \n -- Windows verbind aan //$ip1.local/nfsshare\n\nIP adres $_ip1\n"
