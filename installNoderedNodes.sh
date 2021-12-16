bf1441111454ddeb547f5f9c6a34b96eae2a3d3c
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-12-16 12:36:15 +0100

    Update installNoderedNodes.sh

--- installNoderedNodes.sh
+++ installNoderedNodes.sh
@@ -1,6 +1,6 @@
 NQUIET=''
 startTime="$(date +%s)"
-LOGFILE=$HOME/logs/$0-`date +%Y-%m-%d_%Hh%Mm`_installNoderedNodes_logs.log
+LOGFILE=$HOME/logs/$0-`date +%Y-%m-%d_%Hh%Mm`.log
 _pwd=$(pwd)
 mkdir $HOME/logs
 
