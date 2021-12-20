a58fb2eec21089e8501d72fcb3c6c39ce7224578
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-12-20 11:14:03 +0100

    Update installHomeassistant.sh

--- installHomeassistant.sh
+++ installHomeassistant.sh
@@ -1,15 +1,21 @@
 _hn=$(hostname)
 _pwd=$(pwd)
+LOGFILE=$HOME/logs/installNodered-`date +%Y-%m-%d_%Hh%Mm`.log
+mkdir ~/logs
+
 source /home/pi/.bashrc
 echo "Installeren Homeassistant"
+
 python3 -m ensurepip
-python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade virtualenv
+
 python3 -m virtualenv /home/pi/venv/homeassistant
 source /home/pi/venv/homeassistant/bin/activate
-pip install --upgrade pip
-pip install --upgrade homeassistant
-pip install --upgrade ffmpeg
+
+for addonnodes in  pip virtualenv homeassistant ffmpeg ; do
+	echi "Installeren Homeassistant vereisten:  \"${addonnodes}\""
+  pip install --upgrade ${addonnodes}  2>&1 | tee -a $LOGFILE
+done
+
 
 mkdir ~/Downloads
 cd ~/Downloads
