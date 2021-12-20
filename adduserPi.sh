e760c38bb7db18266db9f438cfff982b81d4085a
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-12-20 15:31:43 +0100

    Update adduserPi.sh

--- adduserPi.sh
+++ adduserPi.sh
@@ -1,19 +1,24 @@
-LOGFILE=~/logbestand1.txt
-echo ""  >> $LOGFILE
-echo "* Nieuwe gebruiker wordt aangemaakt: michiele"
-sudo adduser --quiet --disabled-password --shell /bin/bash --home /home/michiele --gecos "User" michiele
-echo "michiele:raspberry" | sudo chpasswd
-sudo usermod michiele -g sudo -G ssh -a
-sudo usermod michiele -g dialout
-sudo usermod michiele -g tty
-sudo usermod michiele -g gpio
-sudo usermod michiele -g i2c
+_hn=$(hostname)
+_pwd=$(pwd)
+LOGFILE=$HOME/logs/adduserPi-`date +%Y-%m-%d_%Hh%Mm`.log
+mkdir ~/logs
+gebr="michiele"
+wachtwoord="raspberry"
+
+echo "* Nieuwe gebruiker wordt aangemaakt: $gebr"  2>&1 | tee -a $LOGFILE
+sudo adduser --quiet --disabled-password --shell /bin/bash --home /home/$gebr --gecos "User" $gebr
+echo "$gebr:$wachtwoord" | sudo chpasswd
+sudo usermod $gebr -g sudo -G ssh -a
+sudo usermod $gebr -g dialout
+sudo usermod $gebr -g tty
+sudo usermod $gebr -g gpio
+sudo usermod $gebr -g i2c
 
 
-sudo echo "pi ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/michiele
-sudo chmod 0440 /etc/sudoers.d/michiele
+sudo echo "$gebr ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$gebr
+sudo chmod 0440 /etc/sudoers.d/$gebr
 sudo chmod 4755 /usr/bin/sudo
-printf "${ICyan}gebruiker PI aangemaakt, wachtwoord is \"raspberry\". Afmelden en opnieuw aanmelden indien gewenst ${IWhite}\r\n\r\n"
-sudo cp $(readlink -f $0) /home/michiele && chown michiele.michiele /home/michiele/$0 && chmod 755 /home/michiele/$0
-sudo chown michiele.michiele $LOGFILE && sudo chmod 644 $LOGFILE && sudo cp $LOGFILE /home/michiele
+printf "${ICyan}gebruiker $gebr aangemaakt, wachtwoord is \"$wachtwoord\". Afmelden en opnieuw aanmelden indien gewenst ${IWhite}\r\n\r\n"
+sudo cp $(readlink -f $0) /home/$gebr && chown $gebr.$gebr /home/$gebr/$0 && chmod 755 /home/$gebr/$0
+sudo chown $gebr.$gebr $LOGFILE && sudo chmod 644 $LOGFILE && sudo cp $LOGFILE /home/$gebr
 echo ""
