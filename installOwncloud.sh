91415663fa03a3e51b3883b8997a531fbe9fceed
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-12-20 11:31:38 +0100

    Update installOwncloud.sh

--- installOwncloud.sh
+++ installOwncloud.sh
@@ -1,9 +1,18 @@
 _hn1=$(hostname)
 _pwd=$(pwd)
-echo "* Instaleer Owncloud"
+LOGFILE=$HOME/logs/installOwncloud-`date +%Y-%m-%d_%Hh%Mm`.log
+mkdir ~/logs
+printf "\nStart instellen van NFS server op \n" $_ip1 ".local\n"
+
+echo "* Instaleer Owncloud"   2>&1 | tee -a $LOGFILE
 cd ~/Downloads
 sudo apt update -y
-sudo apt install -y apache2 php php-mysql php-intl
+
+for addonnodes in apache2 php php-mysql php-intl ; do
+	echo "Installeren Owncloud vereisten:  \"${addonnodes}\""
+  sudo apt install -y ${addonnodes}   2>&1 | tee -a $LOGFILE
+done
+
 sudo phpenmod  intl
 wget https://download.owncloud.org/community/owncloud-complete-20210721.zip
 7z x ~/Downloads/owncloud-complete-20210721.zip
@@ -12,4 +21,5 @@ sudo mv ~/Downloads/owncloud /var/www/html/support
 sudo chown -R www-data:www-data /var/www/html/support/owncloud
 sudo service apache2 restart
 echo "Owncloud beschikbaar op http://$_hn1.local/support/owncloud"
+sudo rm -rf ~/Downloads/*owncloud-complete*
 cd $_pwd
