2b911b4b4df9f6dfe8cea82e5578f09269282b36
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-12-20 15:19:09 +0100

    Update installOctoprint.sh

--- installOctoprint.sh
+++ installOctoprint.sh
@@ -9,7 +9,7 @@ source ~/venv/octoprint/bin/activate
 
 
 for addonnodes in  pip octoprint ffmpeg ; do
-	echo "Installeren Homeassistant vereisten:  \"${addonnodes}\""
+	echo "Installeren Octoprint vereisten:  \"${addonnodes}\""
   	pip install --upgrade ${addonnodes}  2>&1 | tee -a $LOGFILE
 done
 
@@ -32,36 +32,41 @@ sudo service octoprint restart
 ##echo "SETUP: Start octoprint service."
 
 echo "Installeer extenties." | tee -a $LOGFILE
-pip --disable-pip-version-check install https://github.com/RomainOdeval/OctoPrint-CrealityTemperature/releases/latest/download/master.zip --no-cache-dir | tee -a $LOGFILE
-pip --disable-pip-version-check install https://github.com/electr0sheep/OctoPrint-Cr10_leveling/archive/master.zip --no-cache-dir | tee -a $LOGFILE
-pip --disable-pip-version-check install https://github.com/tpmullan/OctoPrint-DetailedProgress/archive/master.zip --no-cache-dir | tee -a $LOGFILE
-pip --disable-pip-version-check install https://github.com/amsbr/OctoPrint-EEprom-Marlin/archive/master.zip --no-cache-dir | tee -a $LOGFILE
-pip --disable-pip-version-check install https://github.com/Salandora/OctoPrint-FileManager/archive/master.zip --no-cache-dir | tee -a $LOGFILE
-pip --disable-pip-version-check install https://github.com/mic159/octoprint-grbl-plugin/archive/master.zip --no-cache-dir | tee -a $LOGFILE
-pip --disable-pip-version-check install https://github.com/google/OctoPrint-LEDStripControl/archive/master.zip --no-cache-dir | tee -a $LOGFILE
-pip --disable-pip-version-check install https://github.com/OctoPrint/OctoPrint-MQTT/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/FormerLurker/Octolapse/archive/v0.4.0.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/OllisGit/OctoPrint-DisplayLayerProgress/releases/latest/download/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/iFrostizz/OctoPrint-EasyServo/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/Mechazawa/Emergency_stop_simplified/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/mikedmor/OctoPrint_MultiCam/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/jneilliii/OctoPrint-BLTouch/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/frenchie4111/complicated-octoprint/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/StefanCohen/OctoPrint-Dashboard/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/AlexVerrico/Octoprint-Display-ETA/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/jslay88/OctoPrint-Dropbox-Timelapse/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/agrif/OctoPrint-InfluxDB/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/jneilliii/OctoPrint-MQTTPublish/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/bchanudet/OctoPrint-Octorant/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/jneilliii/OctoPrint-Tasmota/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/adilinden-oss/octoprint-webcamstreamer/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/jneilliii/OctoPrint-YouTubeLive/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/ozgunawesome/OctoPrint-PCA9685LEDStripControl/archive/master.zip --no-cache-dir | tee -a $LOGFILE
-pip --disable-pip-version-check install https://github.com/marian42/octoprint-preheat/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/OctoPrint/OctoPrint-RequestSpinner/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/jneilliii/Octoprint-STLViewer/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/rlogiacco/UploadAnything/archive/master.zip --no-cache-dir
-pip --disable-pip-version-check install https://github.com/malnvenshorn/OctoPrint-WebcamTab/archive/master.zip --no-cache-dir
+
+
+for addonnodes in   https://github.com/RomainOdeval/OctoPrint-CrealityTemperature/releases/latest/download/master.zip \
+  https://github.com/electr0sheep/OctoPrint-Cr10_leveling/archive/master.zip \
+   https://github.com/tpmullan/OctoPrint-DetailedProgress/archive/master.zip \
+   https://github.com/amsbr/OctoPrint-EEprom-Marlin/archive/master.zip \
+   https://github.com/Salandora/OctoPrint-FileManager/archive/master.zip \
+   https://github.com/mic159/octoprint-grbl-plugin/archive/master.zip  \
+   https://github.com/google/OctoPrint-LEDStripControl/archive/master.zip \
+   https://github.com/OctoPrint/OctoPrint-MQTT/archive/master.zip \ 
+   https://github.com/FormerLurker/Octolapse/archive/v0.4.0.zip \
+   https://github.com/OllisGit/OctoPrint-DisplayLayerProgress/releases/latest/download/master.zip \
+   https://github.com/iFrostizz/OctoPrint-EasyServo/archive/master.zip \
+   https://github.com/Mechazawa/Emergency_stop_simplified/archive/master.zip \
+   https://github.com/mikedmor/OctoPrint_MultiCam/archive/master.zip \
+   https://github.com/jneilliii/OctoPrint-BLTouch/archive/master.zip \
+   https://github.com/frenchie4111/complicated-octoprint/archive/master.zip \
+   https://github.com/StefanCohen/OctoPrint-Dashboard/archive/master.zip \
+   https://github.com/AlexVerrico/Octoprint-Display-ETA/archive/master.zip \
+   https://github.com/jslay88/OctoPrint-Dropbox-Timelapse/archive/master.zip \
+   https://github.com/agrif/OctoPrint-InfluxDB/archive/master.zip \
+   https://github.com/jneilliii/OctoPrint-MQTTPublish/archive/master.zip \
+   https://github.com/bchanudet/OctoPrint-Octorant/archive/master.zip \
+   https://github.com/jneilliii/OctoPrint-Tasmota/archive/master.zip \
+   https://github.com/adilinden-oss/octoprint-webcamstreamer/archive/master.zip \
+   https://github.com/jneilliii/OctoPrint-YouTubeLive/archive/master.zip \
+   https://github.com/ozgunawesome/OctoPrint-PCA9685LEDStripControl/archive/master.zip \
+   https://github.com/marian42/octoprint-preheat/archive/master.zip \
+   https://github.com/OctoPrint/OctoPrint-RequestSpinner/archive/master.zip \
+   https://github.com/jneilliii/Octoprint-STLViewer/archive/master.zip \
+   https://github.com/rlogiacco/UploadAnything/archive/master.zip \
+   https://github.com/malnvenshorn/OctoPrint-WebcamTab/archive/master.zip  ; do
+	echo "Installeren Octoprint uitbreiding vereisten:  \"${addonnodes}\""
+  	pip install --upgrade --no-cache-dir ${addonnodes}  2>&1 | tee -a $LOGFILE
+done
 
 source ~/venv/venv3.7/bin/activate | tee -a $LOGFILE
 printf "\nStart Octoprint service op http://$_hn1:5000\n" | tee -a $LOGFILE
