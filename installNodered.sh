a950d54bb5ad70f638662d83f7716cccac8a46cf
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-12-22 08:39:06 +0100

    Update installNodered.sh

--- installNodered.sh
+++ installNodered.sh
@@ -113,7 +113,7 @@ printstatus "Opschonen en legen nodered cache afgerond."
 mkdir /home/pi/.node-red
 cd /home/pi/.node-red
 
-for addonnodes in build-essential libnode72 npm ; do
+for addonnodes in build-essential libnode72 npm node ; do
 	printstatus "Installing lib \"${addonnodes}\""
 	sudo apt install -y ${addonnodes} 2>&1 | tee -a $LOGFILE
 done
