1b874b093b5c67d7009b242173ad9eb8105cd546
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-12-22 13:25:33 +0100

    Update installHomebridgeApple.sh

--- installHomebridgeApple.sh
+++ installHomebridgeApple.sh
@@ -5,22 +5,16 @@ printf "Start installatie homebridge voor Apple homekit.\n"
 printf "Zie ook https://github.com/homebridge/homebridge/wiki/Install-Homebridge-on-Raspbian\n"
 
 echo "Installeren build-essentials"
-for addonnodes in build-essential libnode72 npm gcc g++ make python3 net-tools ; do
+for addonnodes in build-essential m gcc g++ make python3 net-tools ; do
 	echo "Installing lib \"${addonnodes}\""
 	sudo apt install -y ${addonnodes} 2>&1 | tee -a $LOGFILE
-done
-
-for addonnodes in build-essential libnode72  ; do
-	echo "Installing lib \"${addonnodes}\""
-	sudo apt --fix-broken install -y ${addonnodes} 2>&1 | tee -a $LOGFILE
 done
 
 sudo apt install --fix-broken -y
 sudo apt autoremove -y
 sudo apt autoclean -y
 
-node -v
-printf "\nApple homekit wordt geïnstalleerd.\n" 2>&1 | tee -a $LOGFILE
+printf "\nApple homekit wordt geïnstalleerd op node: $(node -v) en NPM: $(npm -v).\n" 2>&1 | tee -a $LOGFILE
 sudo npm install -g --unsafe-perm homebridge homebridge-config-ui-x
 printf "Homekit als service gestart\n" 2>&1 | tee -a $LOGFILE
 sudo hb-service install --user homebridge
