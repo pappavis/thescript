bd75414a29ed32e5211e41715a62aa95b1dc909d
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-12-23 10:16:44 +0100

    Update installNodeJS.sh

--- installNodeJS.sh
+++ installNodeJS.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 LOGFILE=$HOME/logs/installNodeJS-`date +%Y-%m-%d_%Hh%Mm`.log
 mkdir ~/logs
 
@@ -12,20 +13,39 @@ printstatus() {
 	echo -e $1 >> $LOGFILE
 }
 
-printstatus "Verwijderen oude NodeJS installaties"
+echo "Verwijderen oude NodeJS installaties"  2>&1 | tee -a $LOGFILE
+
+for addonnodes in npm node nodejs ; do
+	printstatus "Verwijderen oude npm install item: \"${addonnodes}\""
+	sudo apt remove -y ${addonnodes} 2>&1 | tee -a $LOGFILE
+	sudo apt purge -y ${addonnodes} 2>&1 | tee -a $LOGFILE
+done
+
+echo "Verwijderen oude NodeJS mappen in /usr/local"  2>&1 | tee -a $LOGFILE
 sudo rm -rf /usr/local/bin/npm /usr/local/share/man/man1/node* ~/.npm  2>&1 | tee -a $LOGFILE
 sudo rm -rf /usr/local/lib/node*  2>&1 | tee -a $LOGFILE
 sudo rm -rf /usr/local/bin/node*  2>&1 | tee -a $LOGFILE
 sudo rm -rf /usr/local/include/node*  2>&1 | tee -a $LOGFILE
 
-sudo apt purge nodejs npm node -y  2>&1 | tee -a $LOGFILE
 sudo apt autoremove -y  2>&1 | tee -a $LOGFILE
+sudo apt autoclean -y  2>&1 | tee -a $LOGFILE
 cd ~/Downloads
 
-printstatus "NodeJS opnieuw installeren."
-wget https://unofficial-builds.nodejs.org/download/release/v17.3.0/node-v17.3.0-linux-armv6l.tar.gz 2>&1 | tee -a $LOGFILE
-tar xzf node-v17.3.0-linux-armv6l.tar.gz 2>&1 | tee -a $LOGFILE
-sudo cp -R ./node-v17.3.0-linux-armv6l /usr/local 2>&1 | tee -a $LOGFILE
+echo "NodeJS opnieuw installeren." 2>&1 | tee -a $LOGFILE
 
-printstatus "NodeJS $(node -v) en npm $(npm -v) is geïnstalleerd". 2>&1 | tee -a $LOGFILE
+if [ $(nproc) == 1 ]; then
+	echo "NodeJS  installeren op een Pi Zero" 2>&1 | tee -a $LOGFILE
+	wget https://unofficial-builds.nodejs.org/download/release/v17.3.0/node-v17.3.0-linux-armv6l.tar.gz 2>&1 | tee -a $LOGFILE
+	tar xzf node-v17.3.0-linux-armv6l.tar.gz 2>&1 | tee -a $LOGFILE
+	sudo cp -R ./node-v17.3.0-linux-armv6l /usr/local 2>&1 | tee -a $LOGFILE
+else
+	echo "NodeJS  installeren op een Pi3,4" 2>&1 | tee -a $LOGFILE
+	## zie https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-16-04
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash 2>&1 | tee -a $LOGFILE
+	nvm install v17.13.1 2>&1 | tee -a $LOGFILE
+	nvm use v17.13.1
+fi
+
+
+printstatus "NodeJS $(node -v) en npm $(npm -v) is geïnstalleerd" 2>&1 | tee -a $LOGFILE
 
