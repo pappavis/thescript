d0c3086ee7da186fdd0b1f658b797bf404a94355
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-12-23 10:58:56 +0100

    Update installHomebridgeApple.sh

--- installHomebridgeApple.sh
+++ installHomebridgeApple.sh
@@ -1,12 +1,12 @@
-_hn1=$(hostname -I)
+_hn1=$(hostname)
 LOGFILE=$HOME/logs/installHomebridgeApple-`date +%Y-%m-%d_%Hh%Mm`.log
 
 echo "Start installatie homebridge voor Apple homekit." 2>&1 | tee -a $LOGFILE
 echo "Zie ook https://github.com/homebridge/homebridge/wiki/Install-Homebridge-on-Raspbian" 2>&1 | tee -a $LOGFILE
 
-printf "\nApple homekit wordt geïnstalleerd op node: $(node -v) en NPM: $(npm -v).\n" 2>&1 | tee -a $LOGFILE
+echo "\nApple homekit wordt geïnstalleerd op node: $(node -v) en NPM: $(npm -v)." 2>&1 | tee -a $LOGFILE
 sudo npm install -g --unsafe-perm homebridge homebridge-config-ui-x
-printf "Homekit als service gestart\n" 2>&1 | tee -a $LOGFILE
+echo "Homekit als service gestart." 2>&1 | tee -a $LOGFILE
 sudo hb-service install --user homebridge
-printf "Homekit op http://$_hn1.local:8581, gebruiker=admin, wachtwoord=admin\n" 2>&1 | tee -a $LOGFILE
+echo "Homekit op http://$_hn1.local:8581, gebruiker=admin, wachtwoord=admin   ook op http://$(hostname -I):8581" 2>&1 | tee -a $LOGFILE
 
