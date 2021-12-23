9df2c8e6474c57db7e32866dae1132014bad0c14
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-12-23 10:36:20 +0100

    Update installNodeJS.sh

--- installNodeJS.sh
+++ installNodeJS.sh
@@ -43,7 +43,8 @@ else
 	echo "NodeJS  installeren op een Pi3,4" 2>&1 | tee -a $LOGFILE
 	## zie https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-16-04
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash 2>&1 | tee -a $LOGFILE
-	nvm install v17.13.1 2>&1 | tee -a $LOGFILE
+	source ~/.bashrc
+	nvm install v17.13.1 2>&1 | tee -a $LOGFILE	
 	nvm use v17.13.1
 fi
 
