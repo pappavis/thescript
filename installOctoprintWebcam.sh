3ee3e331494eff7f4c6f3d7d2215036429187880
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-12-24 11:27:12 +0100

    Update installOctoprintWebcam.sh

--- installOctoprintWebcam.sh
+++ installOctoprintWebcam.sh
@@ -1,6 +1,6 @@
 _pwd=$(pwd)
 _hostname=$(hostname)
-LOGFILE=$HOME/logs/installOctoprintWebcam.log
+LOGFILE=$HOME/logs/installOctoprintWebcam-`date +%Y-%m-%d_%Hh%Mm`.log
 
 echo "Installeren octoprint webcam build-essentials"
 for addonnodes in subversion libjpeg62-turbo-dev imagemagick ffmpeg libv4l-dev cmake ; do
@@ -8,6 +8,8 @@ for addonnodes in subversion libjpeg62-turbo-dev imagemagick ffmpeg libv4l-dev c
 	sudo apt install -y ${addonnodes} 2>&1 | tee -a $LOGFILE
 done
 
+# https://blog.miguelgrinberg.com/post/how-to-build-and-run-mjpg-streamer-on-the-raspberry-pi
+sudo ln -s /usr/include/linux/videodev2.h /usr/include/linux/videodev.h
 
 echo "\nStart octprint webcam install op $_hostname\n" 2>&1 | tee -a $LOGFILE
 cd ~/Downloads
@@ -15,6 +17,15 @@ git clone https://github.com/jacksonliam/mjpg-streamer.git 2>&1 | tee -a $LOGFIL
 cd ~/Downloads/mjpg-streamer/mjpg-streamer-experimental
 export LD_LIBRARY_PATH=.
 make 2>&1 | tee -a $LOGFILE
-echo "Octprint webcam streaming op http://$_hostname.local:8080/?action=stream" 2>&1 | tee -a $LOGFILE
-./mjpg_streamer -i "./input_uvc.so -y" -o "./output_http.so" 
+sudo cp ./mjpg_streamer /usr/local/bin 2>&1 | tee -a $LOGFILE
+sudo cp ./output_http.so input_file.so /usr/local/lib/ 2>&1 | tee -a $LOGFILE
+sudo cp -R ./www /usr/local/www 2>&1 | tee -a $LOGFILE
+rm -rf ~/Downloads/mjpg-streamer 2>&1 | tee -a $LOGFILE
+mkdir /tmp/stream
+
+MJPGtestBestand=/temp/test_mjpg_streamer-`date +%Y-%m-%d_%Hh%Mm`.log
+echo "MJPG voorbeeld foto maken in  $MJPGtestBestand" 2>&1 | tee -a $LOGFILE
+raspistill --nopreview -w 640 -h 480 -q 5 -o $MJPGtestBestand -tl 100 -t 9999999 -th 0:0:0 & & 2>&1 | tee -a $LOGFILE
+
+echo "Octprint webcam streaming instellen: http://$_hostname.local:8080/?action=stream" 2>&1 | tee -a $LOGFILE
 sudo service octoprint restart
