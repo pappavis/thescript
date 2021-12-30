96aaa5aa005f013431fc928ea5fc88ac6e1164bb
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2021-12-31 00:29:28 +0100

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
-sudo apt-get update && sudo apt-get upgrade -y 
-sudo rpi-update -y
-sudo apt-get autoremove -y
-sudo apt-get autoclean -y
+#!/bin/bash
+mkdir /home/pi/logs
+LOGFILE=$HOME/logs/autoupdate-`date +%Y-%m-%d_%Hh%Mm`.log
+
+echo "" 2>&1 | tee -a $LOGFILE
+echo "Start autoupdate" 2>&1 | tee -a $LOGFILE
+sudo apt-get update && sudo apt-get upgrade -y  2>&1 | tee -a $LOGFILE
+sudo rpi-update -y 2>&1 | tee -a $LOGFILE
+sudo apt-get autoremove -y 2>&1 | tee -a $LOGFILE
+sudo apt-get autoclean -y 2>&1 | tee -a $LOGFILE
+echo "Einde autoupdate" 2>&1 | tee -a $LOGFILE
 sudo reboot
