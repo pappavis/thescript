2d1f96057c6bb4bdbec3ab006d236c3c52d2f030
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-01-04 16:34:46 +0100

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+_cd=$(pwd)
 mkdir /home/pi/logs
 LOGFILE=$HOME/logs/autoupdate-`date +%Y-%m-%d_%Hh%Mm`.log
 
 echo "" 2>&1 | tee -a $LOGFILE
 echo "Start autoupdate" 2>&1 | tee -a $LOGFILE
+if [ -f /home/pi/pi-apps ]; then
+    echo "Start pi-apps update" 2>&1 | tee -a $LOGFILE
+    cd /home/pi/pi-apps
+    git pull 2>&1 | tee -a $LOGFILE
+fi
 sudo apt-get update -y | tee -a $LOGFILE
 sudo apt-get upgrade -y  2>&1 | tee -a $LOGFILE
 sudo rpi-update -y 2>&1 | tee -a $LOGFILE
