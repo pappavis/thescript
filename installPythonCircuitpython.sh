1e9a95eadc62ab58adc0ec9447e0d71eb3d739f9
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-01-04 09:27:40 +0100

    Update installPythonCircuitpython.sh

--- installPythonCircuitpython.sh
+++ installPythonCircuitpython.sh
@@ -21,11 +21,9 @@ for addonnodes in rpi.gpio adafruit-blinka adafruit-circuitpython-ble-adafruit a
                   adafruit-circuitpython-miniqr adafruit-circuitpython-turtle adafruit-circuitpython-74hc595 adafruit-circuitpython-display-button adafruit-circuitpython-mpu6050 adafruit-circuitpython-ina260 adafruit-circuitpython-ssd1306 adafruit-circuitpython-ble  adafruit-circuitpython-ble-heart-rate adafruit-circuitpython-ble-broadcastnet adafruit-circuitpython-binascii  adafruit-circuitpython-onewire adafruit-circuitpython-motor  adafruit-circuitpython-hts221 adafruit-circuitpython-midi adafruit-circuitpython-bmp280  adafruit-circuitpython-bme680 adafruit-circuitpython-dotstar  adafruit-circuitpython-max7219 adafruit-circuitpython-display-text adafruit-circuitpython-rfm69 adafruit-circuitpython-dht adafruit-circuitpython-ble-apple-media adafruit-circuitpython-bme280 adafruit-circuitpython-bitbangio adafruit-circuitpython-led-animation adafruit-circuitpython-ble-midi adafruit-circuitpython-sd adafruit-circuitpython-neopixel adafruit-circuitpython-hid adafruit-circuitpython-gps  adafruit-circuitpython-rfm9x  circuitpython-base64 circuitpython-base64 circuitpython-esp32connection adafruit-circuitpython-ble-radio circuitpython-nrf24l01 thonny-circuitpython  \
                   Adafruit_Libraries Adafruit-PCA9685 Adafruit-GPIO  raspiSensors  oled-text  pi-ina219 adafruit-ads1x15 adafruit-pureio  OctoPyClient tentacle ledDriver  ampy micropython-uploader \
                   msteamsconnector matplotlib numpy imutils pyodbc  Adafruit-GPIO SQLAlchemy aniso8601 croniter flask-restful flask-sqlalchemy natsort pih2o python-dateutil pytz ; do
-    printstatus "Installeren CircuitPython lib: \"${addonnodes}\""
+    printstatus "Installeren CircuitPython bibliotheek: \"${addonnodes}\""
     pip install $NQUIET --upgrade ${addonnodes} 2>&1 | tee -a $LOGFILE
 done
-
-pip install --upgrade adafruit-blinka 2>&1 | tee -a $LOGFILE
 
 python demo/blinkatest.py  2>&1 | tee -a $LOGFILE &
 python demo/ina219demo.py  2>&1 | tee -a $LOGFILE &
