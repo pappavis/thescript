2fa9f4511c9e021365594748337b9d8fb64143df
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-01-04 10:56:39 +0100

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -4,7 +4,8 @@ LOGFILE=$HOME/logs/autoupdate-`date +%Y-%m-%d_%Hh%Mm`.log
 
 echo "" 2>&1 | tee -a $LOGFILE
 echo "Start autoupdate" 2>&1 | tee -a $LOGFILE
-sudo apt-get update && sudo apt-get upgrade -y  2>&1 | tee -a $LOGFILE
+sudo apt-get update -y
+sudo apt-get upgrade -y  2>&1 | tee -a $LOGFILE
 sudo rpi-update -y 2>&1 | tee -a $LOGFILE
 sudo apt-get autoremove -y 2>&1 | tee -a $LOGFILE
 sudo apt-get autoclean -y 2>&1 | tee -a $LOGFILE
