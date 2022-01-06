80fd3391675a8ba0131c97c322c656c7cda4cbba
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-01-06 15:26:39 +0100

    Update installVerzamelupdates.sh

--- installVerzamelupdates.sh
+++ installVerzamelupdates.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 LOGFILE=$HOME/logs/installVerzamelupdates-`date +%Y-%m-%d_%Hh%Mm`.log
 mkdir ~/logs
 
@@ -12,22 +13,21 @@ git clone https://github.com/pappavis/thescript  2>&1 | tee -a $LOGFILE
 cd ~/Downloads/thescript
 git pull
 printf "\nInstalleren VirtualHere.com USB via WiFi.\n"  2>&1 | tee -a $LOGFILE
-bash ./installExtras.sh  2>&1 | tee -a $LOGFILE
-bash ./installNFSserver.sh  2>&1 | tee -a $LOGFILE
-
 printf "\nCircuitPython wordt bijgewerkt\n"  2>&1 | tee -a $LOGFILE
 bash ./installPythonCircuitpython.sh  2>&1 | tee -a $LOGFILE
 printf "\nPython wordt bijgewerkt\n"  2>&1 | tee -a $LOGFILE
 bash ./installPythonVirtualenv.sh  2>&1 | tee -a $LOGFILE
-printf "\Micropython wordt geïnstalleerd\n"  2>&1 | tee -a $LOGFILE
+bash ./installPythonLibs.sh  2>&1 | tee -a $LOGFILE
 bash ./installMicropython.sh  2>&1 | tee -a $LOGFILE
+#bash ./installNFSserver.sh  2>&1 | tee -a $LOGFILE
+
+printf "\Micropython wordt geïnstalleerd\n"  2>&1 | tee -a $LOGFILE
 bash ./installPHPliteadmin.sh  2>&1 | tee -a $LOGFILE
 bash ./installOwncloud.sh  2>&1 | tee -a $LOGFILE
 bash ./installIOT.sh  2>&1 | tee -a $LOGFILE
-bash ./installPythonLibs.sh  2>&1 | tee -a $LOGFILE
 bash ./installUpdates.sh  2>&1 | tee -a $LOGFILE
-sudo apt autoclean -y
-sudo apt autoremove -y
+sudo apt autoclean -y  2>&1 | tee -a $LOGFILE
+sudo apt autoremove -y  2>&1 | tee -a $LOGFILE
 bash ./installOctoprint.sh  2>&1 | tee -a $LOGFILE
 bash ./installOctoprintWebcam.sh  2>&1 | tee -a $LOGFILE
 bash ./installHomeassistant.sh  2>&1 | tee -a $LOGFILE
@@ -35,8 +35,9 @@ bash ./installHomebridgeApple.sh  2>&1 | tee -a $LOGFILE
 bash ./installNodered.sh  2>&1 | tee -a $LOGFILE
 printf "\nTesseract OCR wordt bijgewerkt\n"  2>&1 | tee -a $LOGFILE
 bash ./installTessarectOCR.sh 2>&1 | tee -a $LOGFILE &
-sudo apt autoclean -y
-sudo apt autoremove -y
+bash ./installExtras.sh  2>&1 | tee -a $LOGFILE
+sudo apt autoclean -y  2>&1 | tee -a $LOGFILE
+sudo apt autoremove -y  2>&1 | tee -a $LOGFILE
 
 printf "Let op!!  Apple Homebridge & Homeassistantop Pi 3 model A installeren, dan swapfile uitbreiden naar 1Gb!!\n"  2>&1 | tee -a $LOGFILE
 printf "\nEINDE installVerzamelupdates.sh afgerond.\n"  2>&1 | tee -a $LOGFILE
