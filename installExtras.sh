29a114e7c725cdbdcc7923f67bcaf465cc87f1e9
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-01-26 11:46:44 +0100

    Update installExtras.sh

--- installExtras.sh
+++ installExtras.sh
@@ -114,18 +114,6 @@ sudo adduser xrdp ssl-cert  2>&1 | tee -a $LOGFILE
 systemctl show -p SubState --value xrdp 2>&1 | tee -a $LOGFILE
 
 cd $_pwd
-echo "* Doen ook --> sudo nano /etc/samba/smb.conf -y" 2>&1 | tee -a $LOGFILE
-echo "[Downloads]" | sudo tee -a  /etc/samba/smb.conf
-echo "  comment = aflaai map" | sudo tee -a  /etc/samba/smb.conf
-echo "  path = /home/pi/Downloads" | sudo tee -a  /etc/samba/smb.conf
-echo "  guest ok = yes" | sudo tee -a  /etc/samba/smb.conf
-echo "  browseable = yes" | sudo tee -a  /etc/samba/smb.conf
-echo "  create mask = 0600" | sudo tee -a  /etc/samba/smb.conf
-echo "  directory mask = 0700" | sudo tee -a  /etc/samba/smb.conf
-echo "rider506\nrider506\n" | sudo smbpasswd -a pi
-sudo service smbd restart
-sudo service smbd status 2>&1 | tee -a $LOGFILE
-
 echo "* Installeer pi-apps app store" 2>&1 | tee -a $LOGFILE
 wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash 2>&1 | tee -a $LOGFILE
 
