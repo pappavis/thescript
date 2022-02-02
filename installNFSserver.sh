966d528ba6ca8e2a2d143220e55444db6d28b1d6
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-02-02 12:22:46 +0100

    Update installNFSserver.sh

--- installNFSserver.sh
+++ installNFSserver.sh
@@ -37,19 +37,7 @@ sudo mkdir -p /mnt/nfs/pi08/
 sudo mkdir -p /mnt/nfs/pi09/
 
 #sudo chmod 777 -R /mnt/nfs/
-
-printf "\nProbeer netwerk share te mount op /mnt\n" 2>&1 | tee -a $LOGFILE
-sudo mount -t auto acer01:/home /mnt/nfs/acer01/  2>&1 | tee -a $LOGFILE &
-sudo mount -t auto pi0:/home /mnt/nfs/pi0/ &
-sudo mount -t auto pivhere:/home /mnt/nfs/pivhere/ &
-sudo mount -t auto pi04:/home /mnt/nfs/pi4/ &
-sudo mount -t auto pilamp:/home /mnt/nfs/pilamp/ &
-sudo mount -t auto spelen02:/home /mnt/nfs/spelen02/ &
-sudo mount -t auto p1mon:/home /mnt/nfs/p1mon/ &
-sudo mount -t auto retropie:/home /mnt/nfs/retropie/ &
-sudo mount -t auto pi07:/home /mnt/nfs/pi07/ &
-sudo mount -t auto pi08:/home /mnt/nfs/pi08/ &
-sudo mount -t auto pi09:/home /mnt/nfs/pi09/ &
+./mountNFSserverPappavis.sh  2>&1 | tee -a $LOGFILE
 
 echo "/home *(rw,all_squash,insecure,async,no_subtree_check)"  2>&1 | tee -a $LOGFILE
 
