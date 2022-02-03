d6ef27d1f5ecd778a67afa2867825a72d8de19a8
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-02-03 13:08:19 +0100

    Update installMicropython.sh

--- installMicropython.sh
+++ installMicropython.sh
@@ -10,9 +10,13 @@ for addonnodes in git build-essential libffi-dev ; do
   sudo apt install -y  ${addonnodes} 2>&1 | tee -a $LOGFILE
 done
 
-cd ~
+cd ~/Downloads
+mkdir modules
+mkdir sqlite
 git clone https://github.com/micropython/micropython.git 2>&1 | tee -a $LOGFILE
-cd ~ && cd micropython/mpy-cross
+cd modules
+git clone https://github.com/spatialdude/usqlite.git 2>&1 | tee -a $LOGFILE
+cd ~/Downloads && cd micropython/mpy-cross
 make 2>&1 | tee -a $LOGFILE
 cd ~/micropython/ports/unix/
 make clean 2>&1 | tee -a $LOGFILE
@@ -27,5 +31,14 @@ micropython  -m upip install micropython-os.path 2>&1 | tee -a $LOGFILE
 micropython  -m upip install micropython-umqtt.robust 2>&1 | tee -a $LOGFILE
 micropython  -m upip install micropython-pwd 2>&1 | tee -a $LOGFIL 2>&1 | tee -a $LOGFILEE
 micropython  -m upip install micropython-smtplib
+cd  ~/Downloads/micropython/ports/esp32/
+make clean 2>&1 | tee -a $LOGFILE
+make axtls 2>&1 | tee -a $LOGFILE
+make 2>&1 | tee -a $LOGFILE
+cd  ~/Downloads/micropython/ports/esp8266/
+make clean 2>&1 | tee -a $LOGFILE
+make axtls 2>&1 | tee -a $LOGFILE
+make 2>&1 | tee -a $LOGFILE
+
 echo "EINDE micropython $(micropython -V) module install" 2>&1 | tee -a $LOGFILE
 
