a14a8fb1a0937375a2ecdd3d421af77a05783104
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-02-03 13:15:29 +0100

    Update installMicropython.sh

--- installMicropython.sh
+++ installMicropython.sh
@@ -2,7 +2,7 @@ LOGFILE=$HOME/logs/installMicropython-`date +%Y-%m-%d_%Hh%Mm`.log
 
 echo "Start Micropython installatie"  2>&1 | tee -a $LOGFILE
 # origineel --> https://www.raspberrypi.org/forums/viewtopic.php?t=191744
-for addonnodes in git build-essential libffi-dev ; do
+for addonnodes in git build-essential libffi-dev gcc-mingw-w64 ; do
   echo " "
   echo " "
   echo "Installeren micropython benodigheden: ${addonnodes}"
@@ -18,7 +18,7 @@ cd modules
 git clone https://github.com/spatialdude/usqlite.git 2>&1 | tee -a $LOGFILE
 cd ~/Downloads && cd micropython/mpy-cross
 make 2>&1 | tee -a $LOGFILE
-cd ~/micropython/ports/unix/
+cd  ~/Downloads/micropython/ports/unix/
 make clean 2>&1 | tee -a $LOGFILE
 make axtls 2>&1 | tee -a $LOGFILE
 make 2>&1 | tee -a $LOGFILE
@@ -31,14 +31,23 @@ micropython  -m upip install micropython-os.path 2>&1 | tee -a $LOGFILE
 micropython  -m upip install micropython-umqtt.robust 2>&1 | tee -a $LOGFILE
 micropython  -m upip install micropython-pwd 2>&1 | tee -a $LOGFIL 2>&1 | tee -a $LOGFILEE
 micropython  -m upip install micropython-smtplib
-cd  ~/Downloads/micropython/ports/esp32/
-make clean 2>&1 | tee -a $LOGFILE
-make axtls 2>&1 | tee -a $LOGFILE
+make CROSS_COMPILE=i686-w64-mingw32- 2>&1 | tee -a $LOGFILE
+
+cd ~/Downloads/micropython/ports/esp32
+make submodules 2>&1 | tee -a $LOGFILE
 make 2>&1 | tee -a $LOGFILE
-cd  ~/Downloads/micropython/ports/esp8266/
-make clean 2>&1 | tee -a $LOGFILE
-make axtls 2>&1 | tee -a $LOGFILE
+make erase 2>&1 | tee -a $LOGFILE
+make deploy 2>&1 | tee -a $LOGFILE
+
+cd ~/Downloads/micropython/ports/esp8266
+make submodules 2>&1 | tee -a $LOGFILE
 make 2>&1 | tee -a $LOGFILE
+make erase 2>&1 | tee -a $LOGFILE
+make deploy 2>&1 | tee -a $LOGFILE
+
+cd ~/Downloads/micropython/ports/javascript
+make 2>&1 | tee -a $LOGFILE
+make test 2>&1 | tee -a $LOGFILE
 
 echo "EINDE micropython $(micropython -V) module install" 2>&1 | tee -a $LOGFILE
 
