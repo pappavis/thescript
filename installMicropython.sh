c5bb32300afc36f08a67fc8ef69a4f185053560f
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-02-03 13:42:06 +0100

    Update installMicropython.sh

--- installMicropython.sh
+++ installMicropython.sh
@@ -24,7 +24,9 @@ make submodules 2>&1 | tee -a $LOGFILE
 make clean 2>&1 | tee -a $LOGFILE
 make axtls 2>&1 | tee -a $LOGFILE
 make USER_C_MODULES=~/Downloads/modules 2>&1 | tee -a $LOGFILE
-sudo ln -s ~/Downloads/micropython/ports/unix/micropython /usr/local/bin/micropython
+#sudo ln -s ~/Downloads/micropython/ports/unix/micropython /usr/local/bin/micropython
+sudo cp -v ~/Downloads/micropython/ports/unix/micropython /usr/local/bin/micropython 2>&1 | tee -a $LOGFILE
+
 echo "START micropython module intstall" 2>&1 | tee -a $LOGFILE
 micropython  -m upip install micropython-urequests 2>&1 | tee -a $LOGFILE
 micropython  -m upip install micropython-socket 2>&1 | tee -a $LOGFILE
