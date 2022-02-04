9921d01a3e75d6978b2d71bc4d6b773234577935
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-02-04 14:55:59 +0100

    Update installMicropython.sh

--- installMicropython.sh
+++ installMicropython.sh
@@ -10,27 +10,29 @@ for addonnodes in git autoconf build-essential gperf bison flex texinfo libtool 
   sudo apt install -y  ${addonnodes} 2>&1 | tee -a $LOGFILE
 done
 
-cd ~/Downloads
+export MPDLDIR=~/Downloads/micropython
+
+cd $MPDLDIR
 mkdir modules
 mkdir ./sqlite
 git clone https://github.com/micropython/micropython.git 2>&1 | tee -a $LOGFILE
 cd ./modules
 git clone https://github.com/spatialdude/usqlite.git 2>&1 | tee -a $LOGFILE
 git pull
-cd ~/Downloads/micropython
+cd $MPDLDIR
 git pull
 cd ./mpy-cross
 make 2>&1 | tee -a $LOGFILE
 
-cd  ~/Downloads/micropython/ports/unix/
+cd $MPDLDIR/ports/unix/
 make submodules 2>&1 | tee -a $LOGFILE
 make clean 2>&1 | tee -a $LOGFILE
 make axtls 2>&1 | tee -a $LOGFILE
-make USER_C_MODULES=~/Downloads/modules 2>&1 | tee -a $LOGFILE
+make USER_C_MODULES=$MPDLDIR/modules 2>&1 | tee -a $LOGFILE
 #make   2>&1 | tee -a $LOGFILE
-#sudo ln -s ~/Downloads/micropython/ports/unix/micropython /usr/local/bin/micropython
+#sudo ln -s $MPDLDIR/ports/unix/micropython /usr/local/bin/micropython
 sudo rm /usr/local/bin/micropython
-sudo cp -v ~/Downloads/micropython/ports/unix/micropython /usr/local/bin/micropython 2>&1 | tee -a $LOGFILE
+sudo cp -v $MPDLDIR/ports/unix/micropython /usr/local/bin/micropython 2>&1 | tee -a $LOGFILE
 
 echo "START micropython module intstall" 2>&1 | tee -a $LOGFILE
 for addonnodes in micropython-urequests micropython-socket micropython-machine micropython-os.path micropython-umqtt.robust micropython-pwd micropython-smtplib ; do
@@ -41,28 +43,28 @@ for addonnodes in micropython-urequests micropython-socket micropython-machine m
   micropython -m upip install ${addonnodes} 2>&1 | tee -a $LOGFILE
 done
 
-cd ~/Downloads/micropython/ports/windows
+cd $MPDLDIR/ports/windows
 make submodules 2>&1 | tee -a $LOGFILE
 make CROSS_COMPILE=i686-w64-mingw32- 2>&1 | tee -a $LOGFILE
 cp -v ./micropython.exe ~/Downloads 2>&1 | tee -a $LOGFILE
 
-cd ~/Downloads/micropython/ports/esp32
+cd $MPDLDIR/ports/esp32
 make submodules 2>&1 | tee -a $LOGFILE
 make 2>&1 | tee -a $LOGFILE
 make erase 2>&1 | tee -a $LOGFILE
 make deploy 2>&1 | tee -a $LOGFILE
 
-cd ~/Downloads/micropython/ports/esp8266
+cd $MPDLDIR/ports/esp8266
 make submodules 2>&1 | tee -a $LOGFILE
 make 2>&1 | tee -a $LOGFILE
 make erase 2>&1 | tee -a $LOGFILE
 make deploy 2>&1 | tee -a $LOGFILE
 
-cd ~/Downloads/micropython/ports/rp2
+cd $MPDLDIR/ports/rp2
 make submodules 2>&1 | tee -a $LOGFILE
-make USER_C_MODULES=~/Downloads/micropython/ports/pico/modules/micropython.cmake  2>&1 | tee -a $LOGFILE
+make USER_C_MODULES=$MPDLDIR/ports/pico/modules/micropython.cmake  2>&1 | tee -a $LOGFILE
 
-cd ~/Downloads/micropython/ports/javascript
+cd $MPDLDIR/ports/javascript
 make submodules 2>&1 | tee -a $LOGFILE
 make 2>&1 | tee -a $LOGFILE
 make test 2>&1 | tee -a $LOGFILE
@@ -70,7 +72,7 @@ cp ./micropython.js ~/Downloads
 
 #rm -rf ~/Downloads/modules 2>&1 | tee -a $LOGFILE
 #rm -rf ~/Downloads/sqlite 2>&1 | tee -a $LOGFILE
-#rm -rf ~/Downloads/micropython 2>&1 | tee -a $LOGFILE
+#rm -rf $MPDLDIR 2>&1 | tee -a $LOGFILE
 
 echo "EINDE micropython $(micropython -V) module install" 2>&1 | tee -a $LOGFILE
 
