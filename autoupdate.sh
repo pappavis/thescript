aba59d794fc1cbfe1cab0fecd2149a3dd37717e6
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-02-08 22:40:36 +0100

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -16,7 +16,9 @@ if [ -f /home/pi/pi-apps ]; then
     echo "$datum Start pi-apps update" 2>&1 | tee -a $LOGFILE
     cd /home/pi/pi-apps
     git pull 2>&1 | tee -a $LOGFILE
+    /home/pi/pi-apps/updater | tee -a $LOGFILE
 fi
+
 sudo apt-get update -y | tee -a $LOGFILE
 sudo apt-get upgrade -y  2>&1 | tee -a $LOGFILE
 sudo rpi-update -y 2>&1 | tee -a $LOGFILE
