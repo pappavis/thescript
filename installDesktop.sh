39f5da06b132f75edbc76acd63d9660ad24fe2e9
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-03-21 22:02:46 +0100

    Create installDesktop.sh

---
+++ installDesktop.sh
@@ -0,0 +1,36 @@
+LOGFILE="/home/pi/logs/installDesktop.txt"
+
+echo "** installeer minimale Raspbian desktop." 2>&1 | tee -a $LOGFILE
+for addonnodes in raspberrypi-ui-mods xinit xserver-xorg xrdp   ; do
+  echo " "
+  echo " "
+  echo "Installeren: ${addonnodes}"
+  echo " "
+  sudo apt install -y  ${addonnodes} 2>&1 | tee -a $LOGFILE
+done
+
+sudo mkdir /home/pi/.local
+sudo mkdir /home/pi/.local/share
+sudo mkdir /home/pi/.local/share/lxsession
+sudo mkdir /home/pi/.local/share/lxterminal
+
+echo  "Set de  screensaver uit"  2>&1 | tee -a $LOGFILE
+sudo sed -i -e 'xserver-command xserver-command=X -s 0 -p 0 -dpms' /etc/lightdm/lightdm.conf
+sed -i -e '/timeout:\t0:10:00/s/:10:/:2048:/' /home/pi/.xscreensaver
+sed -i -e '/cycle:\t0:10:00/s/:10:/:1021:/' /home/pi/.xscreensaver
+#xset s 0
+xset -dpms
+
+
+
+echo "** installeer X-Apps zoals KVM." 2>&1 | tee -a $LOGFILE
+for addonnodes in raspberrypi-ui-mods xinit xserver-xorg xrdp  remmina barrier thonny kodi chromium code tightvncserver audacity rpi-imager piclone guvcview ; do
+  echo " "
+  echo " "
+  echo "Installeren ${addonnodes}"
+  echo " "
+  sudo apt install -y  ${addonnodes} 2>&1 | tee -a $LOGFILE
+done
+
+sudo adduser xrdp ssl-cert  2>&1 | tee -a $LOGFILE
+systemctl show -p SubState --value xrdp 2>&1 | tee -a $LOGFILE
