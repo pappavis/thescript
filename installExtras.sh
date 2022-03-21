74593d685f890c4a6bf1b6b10aacbb0a63a5dba7
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-03-21 22:01:31 +0100

    Update installExtras.sh

--- installExtras.sh
+++ installExtras.sh
@@ -81,40 +81,6 @@ sudo systemctl enable influxdb grafana-server telegraf
 sudo systemctl start influxdb grafana-server telegraf
 sudo systemctl status influxdb grafana-server telegraf 2>&1 | tee -a $LOGFILE
 
-echo "** installeer minimale Raspbian desktop." 2>&1 | tee -a $LOGFILE
-for addonnodes in raspberrypi-ui-mods xinit xserver-xorg xrdp   ; do
-  echo " "
-  echo " "
-  echo "Installeren: ${addonnodes}"
-  echo " "
-  sudo apt install -y  ${addonnodes} 2>&1 | tee -a $LOGFILE
-done
-
-sudo mkdir /home/pi/.local
-sudo mkdir /home/pi/.local/share
-sudo mkdir /home/pi/.local/share/lxsession
-sudo mkdir /home/pi/.local/share/lxterminal
-
-echo  "Set de  screensaver uit"  2>&1 | tee -a $LOGFILE
-sudo sed -i -e 'xserver-command xserver-command=X -s 0 -p 0 -dpms' /etc/lightdm/lightdm.conf
-sed -i -e '/timeout:\t0:10:00/s/:10:/:2048:/' /home/pi/.xscreensaver
-sed -i -e '/cycle:\t0:10:00/s/:10:/:1021:/' /home/pi/.xscreensaver
-#xset s 0
-xset -dpms
-
-
-
-echo "** installeer X-Apps zoals KVM." 2>&1 | tee -a $LOGFILE
-for addonnodes in raspberrypi-ui-mods xinit xserver-xorg xrdp  remmina barrier thonny kodi chromium code tightvncserver audacity rpi-imager piclone guvcview ; do
-  echo " "
-  echo " "
-  echo "Installeren ${addonnodes}"
-  echo " "
-  sudo apt install -y  ${addonnodes} 2>&1 | tee -a $LOGFILE
-done
-
-sudo adduser xrdp ssl-cert  2>&1 | tee -a $LOGFILE
-systemctl show -p SubState --value xrdp 2>&1 | tee -a $LOGFILE
 
 cd $_pwd
 echo "* Installeer pi-apps app store" 2>&1 | tee -a $LOGFILE
