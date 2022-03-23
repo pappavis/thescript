b8f46f735a9deb54eb81293171b79827ebc9f425
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-03-23 15:02:41 +0100

    Update installOpenCV.sh

--- installOpenCV.sh
+++ installOpenCV.sh
@@ -31,49 +31,4 @@ for addonnodes in pip numpy dlib ; do
 	pip install --upgrade ${addonnodes} 2>&1 | tee -a $LOGFILE
 done
 
-
-
-exit 0
-
-
-
-
-echo " Download opencv and opencv_contrib" 2>&1 | tee -a $LOGFILE
-cd ~/Downloads
-git clone https://github.com/opencv/opencv.git
-cd opencv
-git checkout 3.4
-cd ..
-
-git clone https://github.com/opencv/opencv_contrib.git
-cd opencv_contrib
-git checkout 3.4
-cd .
-
-cd opencv
-mkdir build 2>&1 | tee -a $LOGFILE
-cd build
-
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
-            -D CMAKE_INSTALL_PREFIX=$cwd/installation/OpenCV-"$cvVersion" \
-            -D INSTALL_C_EXAMPLES=ON \
-            -D INSTALL_PYTHON_EXAMPLES=ON \
-            -D WITH_TBB=ON \
-            -D WITH_V4L=ON \
-            -D OPENCV_PYTHON3_INSTALL_PATH=$cwd/OpenCV-$cvVersion-py3/lib/python3.5/site-packages \
-        -D WITH_QT=ON \
-        -D WITH_OPENGL=ON \
-        -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
-        -D BUILD_EXAMPLES=ON ..
-        
-make -j$(nproc)
-make install
-
-echo " OpenCV build van bronkode afgerond." 2>&1 | tee -a $LOGFILE
-for addonnodes in pip setuptools wheel opencv-contrib-python roundface ; do
-	printstatus "Installing Opencv python hulp \"${addonnodes}\""
-	pip install --upgrade ${addonnodes} 2>&1 | tee -a $LOGFILE
-done
-
-python ./demo/opencv_pip_fix.py
 echo " OpenCV install afgerond." 2>&1 | tee -a $LOGFILE
