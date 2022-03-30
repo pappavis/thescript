3d481d6511f81fb5f6cfb68860b8d9aaaf7b83b0
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-03-30 14:31:19 +0200

    Update installPythonLibs.sh

--- installPythonLibs.sh
+++ installPythonLibs.sh
@@ -32,7 +32,7 @@ for addonnodes in  libatlas-base-dev libwebp-dev  python3-opencv wkhtmltopdf  ; 
 for addonnodes in pip setuptools wheel openpyxl pylzma py7zr o365 ttn qrcode pillow sqlalchemy pymsteams esptool adafruit-ampy firebirdsql esptool mu-editor shortcut \
                   pyserial pyparsing pyzmail redmail gpiozero pytube pipx serial jinja2 esptool mpfshell virtualenv ffmpeg conda jupyter-notebook \
                   scikit-build pygame pymongo psycopg2-binary mysql-connector-python guizero imutils scikit-image numpy bokeh django flask \
-                  msteamsconnector matplotlib numpy imutils pyodbc influxdb pysmb opencv-contrib-python==3.4.11.45  git+https://github.com/pytube/pytube picamera djitellopy \
+                  msteamsconnector matplotlib numpy imutils pyodbc influxdb pysmb opencv-python-headless==4.4.0.44  git+https://github.com/pytube/pytube picamera djitellopy \
 		   osxphotos RPi.GPIO tox tflite tflite-runtime tflite_support PySimpleGUI libusb pyusb pdfkit python-dateutil libopencv-dev  ; do
     printstatus "Installeren python lib: \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
     pip install $NQUIET --upgrade ${addonnodes} 2>&1 | tee -a $LOGFILE
