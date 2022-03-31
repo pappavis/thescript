437da96c0dd41f1e6807d6e24b1f646744147f0c
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-03-31 17:22:57 +0200

    Update installOctoprint.sh

--- installOctoprint.sh
+++ installOctoprint.sh
@@ -24,11 +24,11 @@ echo "SETUP: Installeer Octoprint als service." | tee -a $LOGFILE
 mkdir ~/Downloads
 cd ~/Downloads
 wget https://raw.githubusercontent.com/pappavis/thescript/master/octoprint.service | tee -a $LOGFILE
-sudo mv ./octoprint.service /etc/systemd/system
-sudo systemctl enable octoprint.service | tee -a $LOGFILE
-sudo service octoprint restart
+sudo mv ./octoprint.service /etc/systemd/system 2>&1 | tee -a $LOGFILE
+sudo systemctl enable octoprint.service 2>&1 | tee -a $LOGFILE
+sudo service octoprint restart 2>&1 | tee -a $LOGFILE
 
-echo "Installeer Octoprint extenties." | tee -a $LOGFILE
+echo "Installeer Octoprint extenties." 2>&1 | tee -a $LOGFILE
 
 for addonnodes in   https://github.com/RomainOdeval/OctoPrint-CrealityTemperature/releases/latest/download/master.zip   https://github.com/electr0sheep/OctoPrint-Cr10_leveling/archive/master.zip    https://github.com/tpmullan/OctoPrint-DetailedProgress/archive/master.zip    https://github.com/amsbr/OctoPrint-EEprom-Marlin/archive/master.zip   https://github.com/Salandora/OctoPrint-FileManager/archive/master.zip    https://github.com/mic159/octoprint-grbl-plugin/archive/master.zip     https://github.com/google/OctoPrint-LEDStripControl/archive/master.zip    https://github.com/OctoPrint/OctoPrint-MQTT/archive/master.zip    https://github.com/FormerLurker/Octolapse/archive/v0.4.0.zip    https://github.com/OllisGit/OctoPrint-DisplayLayerProgress/releases/latest/download/master.zip \
    https://github.com/iFrostizz/OctoPrint-EasyServo/archive/master.zip    https://github.com/Mechazawa/Emergency_stop_simplified/archive/master.zip    https://github.com/mikedmor/OctoPrint_MultiCam/archive/master.zip    https://github.com/jneilliii/OctoPrint-BLTouch/archive/master.zip    https://github.com/frenchie4111/complicated-octoprint/archive/master.zip    https://github.com/StefanCohen/OctoPrint-Dashboard/archive/master.zip    https://github.com/AlexVerrico/Octoprint-Display-ETA/archive/master.zip    https://github.com/jslay88/OctoPrint-Dropbox-Timelapse/archive/master.zip    https://github.com/agrif/OctoPrint-InfluxDB/archive/master.zip    https://github.com/jneilliii/OctoPrint-MQTTPublish/archive/master.zip    https://github.com/bchanudet/OctoPrint-Octorant/archive/master.zip    https://github.com/jneilliii/OctoPrint-Tasmota/archive/master.zip    https://github.com/adilinden-oss/octoprint-webcamstreamer/archive/master.zip    https://github.com/jneilliii/OctoPrint-YouTubeLive/archive/master.zip    https://github.com/ozgunawesome/OctoPrint-PCA9685LEDStripControl/archive/master.zip    https://github.com/marian42/octoprint-preheat/archive/master.zip    https://github.com/OctoPrint/OctoPrint-RequestSpinner/archive/master.zip    https://github.com/jneilliii/Octoprint-STLViewer/archive/master.zip    https://github.com/rlogiacco/UploadAnything/archive/master.zip \
@@ -39,11 +39,13 @@ for addonnodes in   https://github.com/RomainOdeval/OctoPrint-CrealityTemperatur
 done
 
 source ~/venv/venv/bin/activate | tee -a $LOGFILE
-printf "\nStart Octoprint service op http://$_hn1:5000\n" | tee -a $LOGFILE
+printf "\nStart Octoprint service op http://$_hn1:5000\n" 2>&1 | tee -a $LOGFILE
 sudo service octoprint restart
 sudo service octoprint status
-printf "Octoprint install afgerond.\n" | tee -a $LOGFILE
+printf "Octoprint install afgerond.\n" 2>&1 | tee -a $LOGFILE
 cd $_pwd
 
-echo "einde octoprint install." | tee - a logfile
+sudo docker pull octoprint/octoprint:edge 2>&1 | tee -a $LOGFILE
+sudo docker run octoprint --device /dev/video0:/dev/video0
+echo "einde octoprint install." | tee -a logfile
 
