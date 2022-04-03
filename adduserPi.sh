0d4cfb6ec5f5080114198524b7282e01e9082e96
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-04-03 17:32:49 +0200

    Update adduserPi.sh

--- adduserPi.sh
+++ adduserPi.sh
@@ -6,11 +6,11 @@ gebr="michiele"
 wachtwoord="raspberry"
 
 sudo usermod pi -g sudo -G ssh -a
-echo "pi ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/pi
-echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/dont-prompt-$USER-for-sudo-password
-sudo chmod 0440 /etc/sudoers.d/pi
-sudo chmod 4755 /usr/bin/sudo
-printf "${ICyan}user PI created, password is \"password\". Please log-out as root and login as pi, and redo the procedure ${IWhite}\r\n\r\n"
+echo "pi ALL=(ALL) NOPASSWD: ALL"  2>&1 | sudo tee -a /etc/sudoers.d/pi
+echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL"  2>&1 | sudo tee /etc/sudoers.d/dont-prompt-$USER-for-sudo-password
+sudo chmod 0440 /etc/sudoers.d/pi  2>&1 | tee -a $LOGFILE
+sudo chmod 4755 /usr/bin/sudo  2>&1 | tee -a $LOGFILE
+printf "${ICyan}user PI created, password is \"password\". Please log-out as root and login as pi, and redo the procedure ${IWhite}\r\n\r\n"  2>&1 | tee -a $LOGFILE
 sudo cp $(readlink -f $0) /home/pi && sudo chown pi.pi /home/pi/$0 && sudo chmod 755 /home/pi/$0
 sudo chown pi.pi $LOGFILE && sudo chmod 644 $LOGFILE && sudo cp $LOGFILE /home/pi
 
@@ -26,9 +26,9 @@ for addonnodes in dialout tty gpio i2c ; do
 done
 
 sed -e "$gebr ALL=(ALL) NOPASSWD: ALL" /etc/sudoers.d/$gebr
-sudo chmod 0440 /etc/sudoers.d/$gebr
-sudo chmod 4755 /usr/bin/sudo
-printf "${ICyan}gebruiker $gebr aangemaakt, wachtwoord is \"$wachtwoord\". Afmelden en opnieuw aanmelden indien gewenst ${IWhite}\r\n\r\n"
+sudo chmod 0440 /etc/sudoers.d/$gebr  2>&1 | tee -a $LOGFILE
+sudo chmod 4755 /usr/bin/sudo  2>&1 | tee -a $LOGFILE
+printf "${ICyan}gebruiker $gebr aangemaakt, wachtwoord is \"$wachtwoord\". Afmelden en opnieuw aanmelden indien gewenst ${IWhite}\r\n\r\n"  2>&1 | tee -a $LOGFILE
 sudo cp $(readlink -f) /home/$gebr && chown $gebr.$gebr -R /home/$gebr/ && chmod 755 /home/$gebr/
-echo ""
+echo ""  2>&1 | tee -a $LOGFILE
 echo "Gebruiker $gebr toevoeging afgerond." 2>&1 | tee -a $LOGFILE
