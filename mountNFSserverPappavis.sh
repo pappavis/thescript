f12c1b7bbf72134184397b79d1bcc94fc14e946b
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-04-21 15:13:40 +0200

    Update mountNFSserverPappavis.sh

--- mountNFSserverPappavis.sh
+++ mountNFSserverPappavis.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 printf "\nProbeer netwerk share te mount op /mnt\n" 2>&1 | tee -a $LOGFILE
 sudo mount -t auto acer01:/home /mnt/nfs/acer01/  2>&1 | tee -a $LOGFILE &
+sudo mount -t auto ict-beheer:/home /mnt/nfs/acer01/  2>&1 | tee -a $LOGFILE &
 sudo mount -t auto pi0:/home /mnt/nfs/pi0/ &
 sudo mount -t auto pi07:/home /mnt/nfs/pi07/ &
 sudo mount -t auto pi08:/home /mnt/nfs/pi08/ &
@@ -12,4 +13,6 @@ sudo mount -t auto spelen02:/home /mnt/nfs/spelen02/ &
 sudo mount -t auto p1mon:/home /mnt/nfs/p1mon/ &
 sudo mount -t auto retropie:/home /mnt/nfs/retropie/ &
 sudo mount -t auto octopi:/home /mnt/nfs/octopi/ &
-sudo mount -t auto dietpi:/home /mnt/nfs/dietpi/ &
+sudo mount -t auto octopi.local:/home /mnt/nfs/octopi/ &
+sudo mount -t auto octopi:/home /mnt/nfs/octopi/ &
+sudo mount -t auto dietpi.local:/home /mnt/nfs/dietpi/ &
