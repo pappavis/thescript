4c841f9a71490759fdd11112a6818e9aab469973
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-04-21 12:08:01 +0200

    Update installPythonVirtualenv.sh

--- installPythonVirtualenv.sh
+++ installPythonVirtualenv.sh
@@ -7,6 +7,8 @@ sudo usermod -aG i2c pi &
 sudo usermod -aG tty pi &
 mkdir $HOME/logs/
 
+
+
 for addonnodes in  unixodbc-dev wiringpi python3-opencv ; do
     echo "Installeren: \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
     sudo apt install -y ${addonnodes}  2>&1 | tee -a $LOGFILE
@@ -23,15 +25,21 @@ echo "source ~/.bashrc" >> ~/.bash_profile
 
 #python3 -m pip install virtualenv
 mkdir ~/venv
-~/.local/bin/pipx install ~/venv/virtualenv  2>&1 | tee -a $LOGFILE
-python3 -m pip install --upgrade virtualenv 
-python3 -m virtualenv ~/venv/venv  2>&1 | tee -a $LOGFILE
-source ~/venv/venv/bin/activate  2>&1 | tee -a $LOGFILE
-for addonnodes in adafruit-blinka RPI.GPIO  ; do
-    echo "Installeren: \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
-    pip install --user --upgrade ${addonnodes}  2>&1 | tee -a $LOGFILE
+VENV="venv"
+rm -rf ~/venv/$VENV
+mkdir ~/venv
+for addonnodes in  virtualenv python3-virtualenv  ; do 
+	echo  "Installing python virtualenv  \"${addonnodes}\""
+	sudo apt install -y ${addonnodes} 2>&1 | tee -a $LOGFILE
 done
-echo "source ~/venv/venv/bin/activate" >> ~/.bashrc
+python3 -m pip install virtualenv 2>&1 | tee -a $LOGFILE
+virtualenv -p /usr/bin/python3  virtualenv
+#/usr/python/python3.11 -m pip install virtualenv 2>&1 | tee -a $LOGFILE
+#~/.local/bin/virtualenv3.11 ~/venv/venv3.11
+echo "source ~/venv/$VENV/bin/activate" >> ~/.bashrc
+source ~/.bashrc
+echo "Virtualenv versie: $(python -V)" 2>&1 | tee -a $LOGFILE
+echo "PATH=$PATH:~/.local/bin" >> ~/.bashrc
 source ~/.bashrc  2>&1 | tee -a $LOGFILE
 
 bash ./installPythonLibs.sh  2>&1 | tee -a $LOGFILE
