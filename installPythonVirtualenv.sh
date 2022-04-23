e86d6877cb9973144959d237933b9b39c3d3b125
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-04-23 12:22:15 +0200

    Update installPythonVirtualenv.sh

--- installPythonVirtualenv.sh
+++ installPythonVirtualenv.sh
@@ -11,20 +11,12 @@ echo ""  2>&1 | tee -a $LOGFILE
 echo "Uitvoeren installPythonVirtualenv.sh begint."  2>&1 | tee -a $LOGFILE
 echo ""  2>&1 | tee -a $LOGFILE
 
-for addonnodes in pipx pipenv wheel ; do
-    echo "Installeren: \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
-    pip install --user --upgrade ${addonnodes}  2>&1 | tee -a $LOGFILE
-done
-
-python3 -m pipx ensurepath  2>&1 | tee -a $LOGFILE
-echo "source ~/.bashrc" >> ~/.bash_profile
-
 #python3 -m pip install virtualenv
 mkdir ~/venv
 VENV="venv"
 rm -rf ~/venv/$VENV
 mkdir ~/venv
-for addonnodes in  virtualenv python3-virtualenv python3-pip  ; do 
+for addonnodes in  virtualenv python3-virtualenv python3-pip python3-wheel python3-setuptools ; do 
 	echo  "Installing python virtualenv  \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
 	sudo apt install -y ${addonnodes} 2>&1 | tee -a $LOGFILE
 done
@@ -33,10 +25,15 @@ done
 #/usr/python/python3.11 -m pip install virtualenv 2>&1 | tee -a $LOGFILE
 #~/.local/bin/virtualenv3.11 ~/venv/venv3.11
 echo "source ~/venv/$VENV/bin/activate" >> ~/.bashrc
+python3 -m pipx ensurepath  2>&1 | tee -a $LOGFILE
 source ~/.bashrc
 echo "Virtualenv versie: $(python -V)" 2>&1 | tee -a $LOGFILE
 echo "PATH=$PATH:~/.local/bin" >> ~/.bashrc
-source ~/.bashrc  2>&1 | tee -a $LOGFILE
+
+for addonnodes in pipx pipenv wheel ; do
+    echo "Installeren: \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
+    pip install --user --upgrade ${addonnodes}  2>&1 | tee -a $LOGFILE
+done
 
 echo "" 2>&1 | tee -a $LOGFILE
 echo "InstallPythonVirtualenv afgerond." 2>&1 | tee -a $LOGFILE
