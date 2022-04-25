b56c7319dbb9eff668c52e089308e43dee854277
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-04-25 16:52:43 +0200

    Update installOwncloud.sh

--- installOwncloud.sh
+++ installOwncloud.sh
@@ -13,7 +13,15 @@ for addonnodes in apache2 php php-mysql php-intl ; do
   sudo apt install -y ${addonnodes}   2>&1 | tee -a $LOGFILE
 done
 
-sudo phpenmod  intl
+# https://superuser.com/questions/835542/how-can-i-access-owncloud-files-by-the-owncloud-server
+echo " mounting dafvs naar /mnt/davfs2/$(hostname)"   2>&1 | tee -a $LOGFILE
+echo "\n" | sudo apt install -y davfs2   2>&1 | tee -a $LOGFILE
+sudo mkdir /mnt/davfs2
+sudo mkdir /mnt/davfs2/$(hostname)
+mount -t auto $(hostname)/owncloud/remote.php/webdav /mnt/davfs2/$(hostname)
+echo " dafvs mouting afgerond."   2>&1 | tee -a $LOGFILE
+
+sudo phpenmod  intl  2>&1 | tee -a $LOGFILE
 wget https://download.owncloud.org/community/owncloud-latest.zip  2>&1 | tee -a $LOGFILE
 7z x ~/Downloads/owncloud-latest.zip  2>&1 | tee -a $LOGFILE
 sudo mkdir /var/www/html/support  2>&1 | tee -a $LOGFILE
