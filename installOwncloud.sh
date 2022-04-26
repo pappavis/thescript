4bef16ca76c62aedd1afc894945dcf8c905deb4e
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-04-26 10:24:32 +0200

    Update installOwncloud.sh

--- installOwncloud.sh
+++ installOwncloud.sh
@@ -4,6 +4,8 @@ LOGFILE=$HOME/logs/installOwncloud-`date +%Y-%m-%d_%Hh%Mm`.log
 mkdir ~/logs
 printf "\nStart instellen van NFS server op \n" $_ip1 ".local\n"
 
+echo ""   2>&1 | tee -a $LOGFILE
+echo ""   2>&1 | tee -a $LOGFILE
 echo "* Instaleer Owncloud"   2>&1 | tee -a $LOGFILE
 cd ~/Downloads
 sudo apt update -y
@@ -17,7 +19,19 @@ done
 echo " mounting dafvs naar /mnt/davfs2/$(hostname)"   2>&1 | tee -a $LOGFILE
 echo "\n" | sudo apt install -y davfs2   2>&1 | tee -a $LOGFILE
 sudo mkdir /mnt/davfs2
-sudo mkdir /mnt/davfs2/$(hostname)
+sudo mkdir /mnt/davfs2/$(hostname)  2>&1 | tee -a $LOGFILE &
+# https://ajclarkson.co.uk/blog/auto-mount-webdav-raspberry-pi/
+sudo chmod u+s /usr/sbin/mount.davfs  2>&1 | tee -a $LOGFILE  2>&1 | tee -a $LOGFILE &
+sudo usermod -a -G davfs2 pi  2>&1 | tee -a $LOGFILE &
+mkdir ~/.davfs2  2>&1 | tee -a $LOGFILE &
+touch ~/.dav2fs/secrets  2>&1 | tee -a $LOGFILE &
+chmod 0600 ~/.dav2fs/secrets  2>&1 | tee -a $LOGFILE &
+openssl x509 -in server.crt -out ~/Downloads/server.pem -outform PEM
+mv ~/Downloads/server.pem ~/.dav2fs/secrets 
+echo "servercert	server.pem" | tee -a ~/.davfs2/davfs2.conf
+
+echo "https://$(hostname).local/support/owncloud/remote.php/webdav admin  admin" | tee -a /home/user/.dav2fs/secret  2>&1 | tee -a $LOGFILE &
+
 sudo mount -t davfs  $(hostname).local/support/owncloud/remote.php/webdav /mnt/davfs2/$(hostname)  2>&1 | tee -a $LOGFILE &
 echo " dafvs mouting afgerond."   2>&1 | tee -a $LOGFILE
 
@@ -31,3 +45,9 @@ sudo service apache2 restart  2>&1 | tee -a $LOGFILE
 echo "Owncloud beschikbaar op http://$(hostname).local/support/owncloud"  2>&1 | tee -a $LOGFILE
 sudo rm -rf ~/Downloads/*owncloud-complete*  2>&1 | tee -a $LOGFILE
 cd $_pwd
+
+echo " mounting dafvs naar /mnt/davfs2/$(hostname)"   2>&1 | tee -a $LOGFILE
+sudo chmod u+s /usr/sbin/mount.davfs 2>&1 | tee -a $LOGFILE
+echo "EINDE installOwncloud.sh"   2>&1 | tee -a $LOGFILE
+echo ""   2>&1 | tee -a $LOGFILE
+echo ""   2>&1 | tee -a $LOGFILE
