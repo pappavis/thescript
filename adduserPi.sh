4a035620aa169ce4753a055e7e624d5a187970f6
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-04-28 15:27:39 +0200

    Update adduserPi.sh

--- adduserPi.sh
+++ adduserPi.sh
@@ -2,7 +2,6 @@ _hn=$(hostname)
 _pwd=$(pwd)
 LOGFILE=$HOME/logs/adduserPi-`date +%Y-%m-%d_%Hh%Mm`.log
 mkdir ~/logs
-gebr="michiele"
 wachtwoord="raspberry"
 
 sudo usermod pi -g sudo -G ssh -a
@@ -14,20 +13,24 @@ sudo cp $(readlink -f $0) /home/pi && sudo chown pi.pi /home/pi/$0 && sudo chmod
 sudo chown pi.pi $LOGFILE && sudo chmod 644 $LOGFILE && sudo cp $LOGFILE /home/pi
 
 
-echo "* Nieuwe gebruiker wordt aangemaakt: $gebr"  2>&1 | tee -a $LOGFILE
-sudo adduser --quiet --disabled-password --shell /bin/bash --home /home/$gebr --gecos "User" $gebr
-echo "$gebr:$wachtwoord" | sudo chpasswd
+for gebr in michiele pi ; do
+  echo "* Nieuwe gebruiker wordt aangemaakt: $gebr"  2>&1 | tee -a $LOGFILE
+  sudo adduser --quiet --disabled-password --shell /bin/bash --home /home/$gebr --gecos "User" $gebr
+  echo "$gebr:$wachtwoord" | sudo chpasswd
+  echo "$gebr ALL=(ALL:ALL) NOPASSWD: ALL"  2>&1 | sudo tee /etc/sudoers.d/dont-prompt-$gebr-for-sudo-password
+  sed -e "$gebr ALL=(ALL) NOPASSWD: ALL" /etc/sudoers.d/$gebr
+  sudo chmod 0440 /etc/sudoers.d/$gebr  2>&1 | tee -a $LOGFILE
+  sudo chmod 4755 /usr/bin/sudo  2>&1 | tee -a $LOGFILE
+  echo "${ICyan}gebruiker $gebr aangemaakt, wachtwoord is \"$wachtwoord\". Afmelden en opnieuw aanmelden indien gewenst ${IWhite}\r\n\r\n"  2>&1 | tee -a $LOGFILE
+  sudo cp $(readlink -f) /home/$gebr && chown $gebr.$gebr -R /home/$gebr/ && chmod 755 /home/$gebr/
+done
+
 
 for addonnodes in dialout tty gpio i2c ; do
   echo "Gebruiker $gebr rechten toewijzen aan groep:  \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
   sudo usermod $gebr -g ${addonnodes}  2>&1 | tee -a $LOGFILE
 done
 
-echo "$gebr ALL=(ALL:ALL) NOPASSWD: ALL"  2>&1 | sudo tee /etc/sudoers.d/dont-prompt-$gebr-for-sudo-password
-sed -e "$gebr ALL=(ALL) NOPASSWD: ALL" /etc/sudoers.d/$gebr
-sudo chmod 0440 /etc/sudoers.d/$gebr  2>&1 | tee -a $LOGFILE
-sudo chmod 4755 /usr/bin/sudo  2>&1 | tee -a $LOGFILE
-printf "${ICyan}gebruiker $gebr aangemaakt, wachtwoord is \"$wachtwoord\". Afmelden en opnieuw aanmelden indien gewenst ${IWhite}\r\n\r\n"  2>&1 | tee -a $LOGFILE
-sudo cp $(readlink -f) /home/$gebr && chown $gebr.$gebr -R /home/$gebr/ && chmod 755 /home/$gebr/
 echo ""  2>&1 | tee -a $LOGFILE
-echo "Gebruiker $gebr toevoeging afgerond." 2>&1 | tee -a $LOGFILE
+echo "AddUserPi toevoeging afgerond." 2>&1 | tee -a $LOGFILE
+echo ""  2>&1 | tee -a $LOGFILE
