c0431934714920e96d32968be716bf9ce75635d7
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-05-12 10:19:25 +0200

    Update installPythonLibs.sh

--- installPythonLibs.sh
+++ installPythonLibs.sh
@@ -45,7 +45,7 @@ for addonnodes in setuptools wheel scikit-build cmake pip numpy  ; do
 for addonnodes in pip setuptools wheel openpyxl pylzma py7zr o365 ttn qrcode pillow sqlalchemy pymsteams qscintilla esptool adafruit-ampy firebirdsql esptool mu-editor shortcut \
                   pyserial pyparsing pyzmail redmail gpiozero pytube pipx serial jinja2 esptool mpfshell virtualenv ffmpeg conda jupyter-notebook \
                   scikit-build pygame pymongo psycopg2-binary mysql-connector-python guizero imutils scikit-image numpy bokeh django flask pygrabber \
-                  msteamsconnector matplotlib numpy imutils pyodbc influxdb pysmb opencv-python  git+https://github.com/pytube/pytube picamera djitellopy \
+                  msteamsconnector matplotlib numpy imutils pyodbc influxdb pysmb opencv-contrib-python  git+https://github.com/pytube/pytube picamera djitellopy \
 		   osxphotos RPi.GPIO tox tflite tflite-runtime tflite_support PySimpleGUI libusb pyusb pdfkit python-dateutil libopencv-dev  ; do
 
     echo "" 2>&1 | tee -a $LOGFILE
