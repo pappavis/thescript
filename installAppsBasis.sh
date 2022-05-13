52f92670990cd87dc9e9c58750d0d84bbc784bb2
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-05-13 21:17:48 +0200

    Update installAppsBasis.sh

--- installAppsBasis.sh
+++ installAppsBasis.sh
@@ -23,7 +23,7 @@ for addonnodes in libatlas3-base qtchooser imagemagick libfontconfig1-dev libcai
 done
 
 
-for addonnodes in libatlas3-base qtchooser imagemagick libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libssl libcurl4-gnutls-dev libcurl4-openssl-dev libcurl4-openssl-dev  libsdl2-ttf-dev libsdl2-image-dev ccze net-tools ntfs-3g php-intl i2c-tools python3-rpi.gpio libgstreamer-plugins-base1.0-dev ; do 
+for addonnodes in libatlas3-base qtchooser imagemagick libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libssl libcurl4-gnutls-dev libcurl4-openssl-dev libcurl4-openssl-dev  libsdl2-ttf-dev libsdl2-image-dev ccze net-tools ntfs-3g php-intl i2c-tools python3-rpi.gpio libgstreamer-plugins-base1.0-dev rapidjson-dev ; do 
 	echo ""  2>&1 | tee -a $LOGFILE
 	echo "--Installeren  \"${addonnodes}\""  2>&1 | tee -a $LOGFILE
 	sudo apt install -y ${addonnodes} 2>&1 | tee -a $LOGFILE
