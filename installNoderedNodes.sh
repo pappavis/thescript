7d12e3788747073e26eca4d4dc4c82e4e4b3c3a6
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-06-08 16:50:15 +0200

    Update installNoderedNodes.sh

--- installNoderedNodes.sh
+++ installNoderedNodes.sh
@@ -73,7 +73,6 @@ for addonnodes in moment node-red-contrib-web-worldmap node-red-contrib-ramp-the
 	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
 	echo "" 2>&1 | tee -a $LOGFILE
 done
-
 sudo service nodered restart 2>&1 | tee -a $LOGFILE
 
 for addonnodes in moment node-red-contrib-home-assistant-websocket node-red-contrib-ibm-watson-iot node-red-contrib-sun-position ; do
@@ -89,24 +88,83 @@ for addonnodes in moment node-red-contrib-tuya-local node-red-contrib-ui-led nod
 	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
 	echo "" 2>&1 | tee -a $LOGFILE
 done
-
 sudo service nodered restart 2>&1 | tee -a $LOGFILE
 
-for addonnodes in moment node-red-dashboard node-red-contrib-config node-red-contrib-grove node-red-contrib-diode node-red-contrib-bigtimer \
-	node-red-contrib-esplogin node-red-contrib-timeout node-red-node-openweathermap node-red-node-google node-red-contrib-advanced-ping node-red-node-emoncms \
-	node-red-node-geofence node-red-contrib-moment node-red-contrib-particle node-red-contrib-find-my-iphone node-red-contrib-influxdb  \
-	node-red-contrib-web-worldmap node-red-contrib-ramp-thermostat node-red-contrib-fs-ops node-red-contrib-influxdb \
-	node-red-contrib-home-assistant-websocket node-red-contrib-ibm-watson-iot node-red-contrib-sun-position \
-	node-red-contrib-tuya-local node-red-contrib-ui-led node-red-contrib-yr node-red-contrib-aedes \
-	node-red-contrib-isonline node-red-node-ping node-red-node-random node-red-node-smooth node-red-contrib-npm node-red-node-arduino \
-	node-red-contrib-file-function node-red-contrib-boolean-logic node-red-contrib-blynk-ws node-red-contrib-telegrambot node-red-contrib-dsm node-red-contrib-ftp \
-	node-red-dashboard node-red-contrib-owntracks node-red-contrib-alexa-local node-red-contrib-amazon-echo node-red-contrib-alexa-notifyme node-red-contrib-heater-controller node-red-contrib-oauth2  node-red-contrib-cron-plus smithtek-node-red-lora @lora-contrib/node-red-contrib-payload-parser node-red-contrib-lora-packet-converter  node-red-contrib-zigbee2mqtt  node-red-contrib-postgresql   node-red-contrib-node-firebird  ; do
+for addonnodes in moment node-red-dashboard node-red-contrib-config node-red-contrib-grove node-red-contrib-diode node-red-contrib-bigtimer  ; do
+	echo "" 2>&1 | tee -a $LOGFILE
+	echo "Installing node \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	echo "" 2>&1 | tee -a $LOGFILE
+done
+sudo service nodered restart 2>&1 | tee -a $LOGFILE
+
+
+for addonnodes in node-red-contrib-esplogin node-red-contrib-timeout node-red-node-openweathermap node-red-node-google node-red-contrib-advanced-ping node-red-node-emoncms  ; do
+	echo "" 2>&1 | tee -a $LOGFILE
+	echo "Installing node \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	echo "" 2>&1 | tee -a $LOGFILE
+done
+sudo service nodered restart 2>&1 | tee -a $LOGFILE
+
+for addonnodes in node-red-node-geofence node-red-contrib-moment node-red-contrib-particle node-red-contrib-find-my-iphone node-red-contrib-influxdb   ; do
+	echo "Installing node \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	echo "" 2>&1 | tee -a $LOGFILE
+done
+sudo service nodered restart 2>&1 | tee -a $LOGFILE
+
+for addonnodes in node-red-contrib-web-worldmap node-red-contrib-ramp-thermostat node-red-contrib-fs-ops node-red-contrib-influxdb   ; do
 	echo "" 2>&1 | tee -a $LOGFILE
 	echo "Installing node \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
 	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
 	echo "" 2>&1 | tee -a $LOGFILE
 done
 
+for addonnodes in node-red-contrib-home-assistant-websocket node-red-contrib-ibm-watson-iot node-red-contrib-sun-position  ; do
+	echo "" 2>&1 | tee -a $LOGFILE
+	echo "Installing node \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	echo "" 2>&1 | tee -a $LOGFILE
+done
+
+for addonnodes in node-red-contrib-tuya-local node-red-contrib-ui-led node-red-contrib-yr node-red-contrib-aedes  ; do
+	echo "" 2>&1 | tee -a $LOGFILE
+	echo "Installing node \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	echo "" 2>&1 | tee -a $LOGFILE
+done
+
+for addonnodes in node-red-contrib-isonline node-red-node-ping node-red-node-random node-red-node-smooth node-red-contrib-npm node-red-node-arduino  ; do
+	echo "" 2>&1 | tee -a $LOGFILE
+	echo "Installing node \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	echo "" 2>&1 | tee -a $LOGFILE
+done
+
+for addonnodes in node-red-contrib-file-function node-red-contrib-boolean-logic node-red-contrib-blynk-ws node-red-contrib-telegrambot node-red-contrib-dsm node-red-contrib-ftp  ; do
+	echo "" 2>&1 | tee -a $LOGFILE
+	echo "Installing node \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	echo "" 2>&1 | tee -a $LOGFILE
+done
+	
+for addonnodes in node-red-dashboard node-red-contrib-ui-upload  node-red-contrib-ui-digital-clock  node-red-contrib-ui-multistate-switch  node-red-node-ui-table  node-red-node-ui-webcam  node-red-contrib-ui-led  node-red-node-ui-microphone  node-red-contrib-dashboard-sum-bars  ; do
+	echo "" 2>&1 | tee -a $LOGFILE
+	echo "Installing node \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	echo "" 2>&1 | tee -a $LOGFILE
+done
+sudo service nodered restart
+
+
+for addonnodes in node-red-contrib-owntracks node-red-contrib-alexa-local node-red-contrib-amazon-echo node-red-contrib-alexa-notifyme node-red-contrib-heater-controller node-red-contrib-oauth2  node-red-contrib-cron-plus smithtek-node-red-lora @lora-contrib/node-red-contrib-payload-parser node-red-contrib-lora-packet-converter  node-red-contrib-zigbee2mqtt  node-red-contrib-postgresql   node-red-contrib-node-firebird \
+	node-red-node-pi-unicorn-hat ; do
+	echo "" 2>&1 | tee -a $LOGFILE
+	echo "Installing node \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
+	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
+	echo "" 2>&1 | tee -a $LOGFILE
+done
 sudo service nodered restart
 
 for addonnodes in moment node-red-contrib-find-my-iphone node-red-contrib-apple-find-me node-red-contrib-msnodesql   ; do 
@@ -116,21 +174,21 @@ for addonnodes in moment node-red-contrib-find-my-iphone node-red-contrib-apple-
 	echo "" 2>&1 | tee -a $LOGFILE
 done
  
-for addonnodes in 	node-red-contrib-homekit-bridged node-red-contrib-homebridge-automation write-excel-file node-red-contrib-web-worldmap node-red-contrib-oauth2   ; do 
+for addonnodes in node-red-contrib-homekit-bridged node-red-contrib-homebridge-automation write-excel-file node-red-contrib-web-worldmap node-red-contrib-oauth2   ; do 
 	echo "" 2>&1 | tee -a $LOGFILE
 	echo "Installing node \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
 	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
 	echo "" 2>&1 | tee -a $LOGFILE
 done
  
-for addonnodes in 	node-red-contrib-wled2 node-red-dashboard node-red-node-pi-mcp3008 node-red-contrib-webservices node-red-node-mysql node-red-contrib-car-bmw   ; do 
+for addonnodes in node-red-contrib-wled2 node-red-dashboard node-red-node-pi-mcp3008 node-red-contrib-webservices node-red-node-mysql node-red-contrib-car-bmw   ; do 
 	echo "" 2>&1 | tee -a $LOGFILE
 	echo "Installing node \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
 	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
 	echo "" 2>&1 | tee -a $LOGFILE
 done
  
-for addonnodes in 	node-red-contrib-google-sheets node-red-contrib-plate-detection node-red-contrib-norelite-homeassistant node-red-contrib-tasmota node-red-contrib-node-firebird node-red-contrib-re-postgres  node-red-contrib-chatbot  node-red-contrib-image-output ; do 
+for addonnodes in node-red-contrib-google-sheets node-red-contrib-plate-detection node-red-contrib-norelite-homeassistant node-red-contrib-tasmota node-red-contrib-node-firebird node-red-contrib-re-postgres  node-red-contrib-chatbot  node-red-contrib-image-output ; do 
 	echo "" 2>&1 | tee -a $LOGFILE
 	echo "Installing node \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
 	npm $NQUIET install --save ${addonnodes} 2>&1 | tee -a $LOGFILE
@@ -139,7 +197,7 @@ done
 
 npm audit fix --force 2>&1 | tee -a $LOGFILE
 
-echo "Installeren global nodes"
+echo "Installeren global nodes" 2>&1 | tee -a $LOGFILE
 for addonnodes in qrcode johnny-five ; do
 	echo "" 2>&1 | tee -a $LOGFILE
 	echo "Installing node \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
