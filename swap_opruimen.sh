73e2d53a4fc56f8403176e4f74bf39f87669d261
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-06-30 11:49:33 +0200

    Update swap_opruimen.sh

--- swap_opruimen.sh
+++ swap_opruimen.sh
@@ -2,6 +2,13 @@
 #ref --> https://forums.raspberrypi.com/viewtopic.php?t=221762#p1360381
 LOGFILE=/home/pi/logs/swap_opruimen.log
 
+#instructies
+# 1. start contab 
+#  pi$: crontab -e
+# 2. toevoegen odnerstaan regel en opslaan met ctrl-X 
+#   45 * * * * sudo bash /home/pi/Downloads/thescript/swap_opruimen.sh
+# 3. einde. De crontab is geinstalleerd.
+
 echo "" 2>&1 | tee -a $LOGFILE
 echo "Start swap opschonen $(date)" 2>&1 | tee -a $LOGFILE
 sudo sync 2>&1 | tee -a $LOGFILE
