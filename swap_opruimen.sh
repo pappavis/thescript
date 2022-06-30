f9ead2edbd3b03f3523e2220e20880a7bacbb0ec
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-06-30 11:23:11 +0200

    Create swap_opruimen.sh

---
+++ swap_opruimen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#ref --> https://forums.raspberrypi.com/viewtopic.php?t=221762#p1360381
+LOGFILE=/home/pi/swap_opruimen.log
+
+echo "" 2>&1 | tee -a $LOGFILE
+echo "Start swap opschonen ($date)" 2>&1 | tee -a $LOGFILE
+sudo sync 2>&1 | tee -a $LOGFILE
+sudo swapoff -a 2>&1 | tee -a $LOGFILE
+sudo swapon /var/swap 2>&1 | tee -a $LOGFILE
+free 2>&1 | tee -a $LOGFILE
+echo "EINDE swap opschonen ($date)" 2>&1 | tee -a $LOGFILE
