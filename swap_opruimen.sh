10bd352b2105fd763986fc4afcd96a3f4716c593
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-06-30 11:44:38 +0200

    Update swap_opruimen.sh

--- swap_opruimen.sh
+++ swap_opruimen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #ref --> https://forums.raspberrypi.com/viewtopic.php?t=221762#p1360381
-LOGFILE=/home/pi/swap_opruimen.log
+LOGFILE=/home/pi/logs/swap_opruimen.log
 
 echo "" 2>&1 | tee -a $LOGFILE
 echo "Start swap opschonen $(date)" 2>&1 | tee -a $LOGFILE
