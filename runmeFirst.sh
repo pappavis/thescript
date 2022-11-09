a1749db9825051a94946a95d3d3f02d0cdb42741
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-11-09 22:38:26 +0100

    Update runmeFirst.sh

--- runmeFirst.sh
+++ runmeFirst.sh
@@ -215,6 +215,7 @@ wget https://github.com/aristocratos/btop/releases/download/v1.1.2/btop-1.1.2-ar
 #wget https://github.com/aristocratos/btop/releases/download/v1.1.2/btop-1.1.2-armv7l-linux-musleabihf.tbz 2>&1 | tee -a $LOGFILE
 sudo 7z x ./btop-1.1.2-armv5l-linux-musleabi.tbz  2>&1 | tee -a $LOGFILE
 sudo 7z x ./btop-1.1.2-armv5l-linux-musleabi.tar
+sudo chown pi:pi ~/Downloads -R
 sudo make  2>&1 | tee -a $LOGFILE
 cd ~/Downloads/
 sudo rm -rf ./btop_install
