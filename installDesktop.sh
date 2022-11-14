dfed043db36cccfc48929fbd2206adcca0667ade
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-11-14 11:55:39 +0100

    Update installDesktop.sh

--- installDesktop.sh
+++ installDesktop.sh
@@ -138,6 +138,80 @@ echo 'export STEAMOS=1' | sudo tee -a /etc/profile.d/steam.sh 2>&1 | tee -a $LOG
 echo 'export STEAM_RUNTIME=1' | sudo tee -a /etc/profile.d/steam.sh
 sudo service steamlink status 2>&1 | tee -a $LOGFILE
 
+echo "** installeer OBS Studio op pi" 2>&1 | tee -a $LOGFILE
+cd~/Downloads
+# https://raspberrytips.com/install-obs-studio-raspberry-pi/
+for addonnodes in build-essential checkinstall cmake git libmbedtls-dev libasound2-dev libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavutil-dev libcurl4-openssl-dev libfontconfig1-dev libfreetype6-dev libgl1-mesa-dev libjack-jackd2-dev libjansson-dev libluajit-5.1-dev libpulse-dev libqt5x11extras5-dev libspeexdsp-dev libswresample-dev libswscale-dev libudev-dev libv4l-dev libvlc-dev libx11-dev libx11-xcb1 libx11-xcb-dev libxcb-xinput0 libxcb-xinput-dev libxcb-randr0 libxcb-randr0-dev libxcb-xfixes0 libxcb-xfixes0-dev libx264-dev libxcb-shm0-dev libxcb-xinerama0-dev libxcomposite-dev libxinerama-dev pkg-config python3-dev qtbase5-dev libqt5svg5-dev swig libwayland-dev qtbase5-private-dev ; do
+  echo " "
+  echo " "
+  echo "Installeren OBS STudio vereisten: ${addonnodes}" 2>&1 | tee -a $LOGFILE
+  echo " "
+  sudo apt install -y  ${addonnodes} 2>&1 | tee -a $LOGFILE
+done
+wget http://ftp.debian.org/debian/pool/non-free/f/fdk-aac/libfdk-aac2_2.0.1-1_armhf.deb 2>&1 | tee -a $LOGFILE
+wget http://ftp.debian.org/debian/pool/non-free/f/fdk-aac/libfdk-aac-dev_2.0.1-1_armhf.deb 2>&1 | tee -a $LOGFILE
+sudo dpkg -i libfdk-aac2_2.0.1-1_armhf.deb 2>&1 | tee -a $LOGFILE
+sudo dpkg -i libfdk-aac-dev_2.0.1-1_armhf.deb 2>&1 | tee -a $LOGFILE
+sudo git clone --recursive https://github.com/obsproject/obs-studio.git
+cd obs-studio
+sudo mkdir build
+cd build
+sudo cmake -DUNIX_STRUCTURE=1 -DCMAKE_INSTALL_PREFIX=/usr -DENABLE_PIPEWIRE=OFF -DBUILD_BROWSER=OFF .. 2>&1 | tee -a $LOGFILE
+sudo make -j1 2>&1 | tee -a $LOGFILE
+sudo make install 2>&1 | tee -a $LOGFILE
+echo MESA_GL_VERSION_OVERRIDE="3.3 obs"| sudo tee -a /etc/bash.bashrc
+rm -rf ./libfdk-aac2_2.0.1-1_armhf.deb
+rm -rf ,.libfdk-aac-dev_2.0.1-1_armhf.deb
+echo "**  OBS Studio build afgerond" 2>&1 | tee -a $LOGFILE
+echo "---------" 2>&1 | tee -a $LOGFILE
+
+cd ~/Downloads
+appTxt1="Milkytracker"
+echo "" 2>&1 | tee -a $LOGFILE
+echo "Installeren: $appTxt1" 2>&1 | tee -a $LOGFILE
+for addonnodes in libjack-dev liblhasa-dev librtmidi-dev libsdl2-dev libzzip-dev ; do
+  echo " "
+  echo " "
+  echo "Installeren $appTxt1 vereisten: ${addonnodes}" 2>&1 | tee -a $LOGFILE
+  echo " "
+  sudo apt install -y  ${addonnodes} 2>&1 | tee -a $LOGFILE
+done
+git clone https://github.com/milkytracker/MilkyTracker 2>&1 | tee -a $LOGFILE
+cd ./MilkyTracker
+mkdir ./build
+cd ./build
+cmake .. 2>&1 | tee -a $LOGFILE
+make 2>&1 | tee -a $LOGFILE
+sudo make install 2>&1 | tee -a $LOGFILE
+cd ~/Downloads
+sudo rm -rf ./MilkyTracker
+echo "Einde $appTxt1 build install" 2>&1 | tee -a $LOGFILE
+echo "" 2>&1 | tee -a $LOGFILE
+
+cd ~/Downloads
+appTxt1="FlatCam PCB router voor KiCAD"
+echo "" 2>&1 | tee -a $LOGFILE
+echo "Installeren: $appTxt1" 2>&1 | tee -a $LOGFILE
+for addonnodes in pyqt6  geos  spatialindex ; do
+  echo " "
+  echo " "
+  echo "Installeren $appTxt1 vereisten: ${addonnodes}" 2>&1 | tee -a $LOGFILE
+  pip install --upgrade  2>&1 | tee -a $LOGFILE
+  echo " "
+done
+git clone https://bitbucket.org/jpcgt/flatcam.git 2>&1 | tee -a $LOGFILE
+sudo mkdir /usr/local/share/applications/ 2>&1 | tee -a $LOGFILE
+sudo mv ./flatcam /usr/local/share/applications
+sudo ln -s /usr/local/share/applications/flatcam/flatcam /usr/local/bin/flatcam
+echo "" 2>&1 | tee -a $LOGFILE
+echo "Einde $appTxt1 build install" 2>&1 | tee -a $LOGFILE
+echo "" 2>&1 | tee -a $LOGFILE
+echo "" 2>&1 | tee -a $LOGFILE
+
+
+cd ~/Downloads
+echo "* Installeer pi-apps app store" 2>&1 | tee -a $LOGFILE
+wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash 2>&1 | tee -a $LOGFILE
 
 
 
