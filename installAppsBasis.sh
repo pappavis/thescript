ed8e8ff21e4d0946b31a98e6d263719c0bb44859
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-11-30 09:15:56 +0100

    Update installAppsBasis.sh

--- installAppsBasis.sh
+++ installAppsBasis.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 LOGFILE=$HOME/logs/installAppsBasis-`date +%Y-%m-%d_%Hh%Mm`.log
 
+#installeren basis apps, tools, utils voor een Raspbian minimale install.
+
 sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu focal main" 2>&1 | tee -a $LOGFILE
 sudo apt update -y 2>&1 | tee -a $LOGFILE
 sudo apt update --fix-missing -y  2>&1 | tee -a $LOGFILE
@@ -11,7 +13,7 @@ for addonnodes in p7zip-full mc sqlite3 i2c-tools ncftp mariadb-server mariadb-c
 	sudo apt install -y ${addonnodes} 2>&1 | tee -a $LOGFILE
 done
 
-for addonnodes in build-essential cmake rapidjson-dev libgmp-dev git gcc g++ netdiscover sysfsutils tcpdump vsftpd wget  ssh bash-completion unzip build-essential git python-serial scons libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libsqlite3-dev subversion libusb-dev python-dev python3-dev cmake curl telnet usbutils gawk jq pv samba samba-common samba-common-bin smbclient winbind dosfstools parted gcc python3-pip htop python-smbus mc cu mpg123 screen ffmpeg qemu-system default-jdk openvpn lynx clonezilla yum telnet lynx docker dnsutils clamav clamav-daemon ; do
+for addonnodes in build-essential cmake rapidjson-dev libgmp-dev git gcc g++ netdiscover sysfsutils tcpdump vsftpd wget  ssh bash-completion unzip build-essential git python-serial scons libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libsqlite3-dev subversion libusb-dev python-dev python3-dev cmake curl telnet usbutils gawk jq pv samba samba-common samba-common-bin smbclient winbind dosfstools parted gcc python3-pip htop python-smbus mc cu mpg123 screen ffmpeg qemu-system default-jdk openvpn lynx clonezilla yum telnet lynx docker dnsutils clamav clamav-daemon hdparm ; do
 	echo ""  2>&1 | tee -a $LOGFILE
 	echo "--Installeren  \"${addonnodes}\""  2>&1 | tee -a $LOGFILE
 	sudo apt install -y ${addonnodes} 2>&1 | tee -a $LOGFILE
@@ -47,6 +49,8 @@ sudo apt autoclean  -y 2>&1 | tee -a $LOGFILE
 echo "listener 1883" | sudo tee -a /etc/mosquitto/conf.d/standard.conf
 echo "protocol mqtt" | sudo tee -a /etc/mosquitto/conf.d/standard.conf
 echo "allow_anonymous true" | sudo tee -a /etc/mosquitto/conf.d/standard.conf
+
+sudo hdparm -t --direct /dev/mmcblk0  -y 2>&1 | tee -a $LOGFILE
 
 echo ""  2>&1 | tee -a $LOGFILE
 echo "Einde installAppsBasis"  2>&1 | tee -a $LOGFILE
