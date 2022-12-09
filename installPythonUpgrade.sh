ef56aa5612b4f2a5169364ceef9a7d295a6061e0
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2022-12-09 12:21:45 +0100

    Update installPythonUpgrade.sh

--- installPythonUpgrade.sh
+++ installPythonUpgrade.sh
@@ -8,8 +8,9 @@ rm -rf ~/.cache/pip 2>&1 | tee -a $LOGFILE
 
 wget -qO - https://raw.githubusercontent.com/tvdsluijs/sh-python-installer/main/python.sh | sudo bash -s 3.10.3 2>&1 | tee -a $LOGFILE
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python$PVERS 1 2>&1 | tee -a $LOGFILE
-/usr/bin/python -m pip install virtualenv 2>&1 | tee -a $LOGFILE
-virtualenv ~/venv/venv$PVERS 2>&1 | tee -a $LOGFILE
+/usr/bin/python$PVERS -m pip install virtualenv 2>&1 | tee -a $LOGFILE
+/usr/bin/python$PVERS -m virtualenv ~/venv/venv$PVERS 2>&1 | tee -a $LOGFILE
+source ~/venv/venv$PVERS/bin/activate
 
 for addonnodes in git wget build-essential checkinstall build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev  libncursesw5-dev libc6-dev openssl libffi-dev libbz2-dev liblzma-dev libsqlite3-dev libncurses5-dev libgdbm-dev zlib1g-dev libreadline-dev libssl-dev tk-dev build-essential libopencv-dev   ; do
     printstatus "CircuitPython activeren  : \"${addonnodes}\""
@@ -27,7 +28,6 @@ done
 #mkdir ~/venv
 #~/.local/bin/python$PVERS -m virtualenv ~/venv/venv3.11 2>&1 | tee -a $LOGFILE
 #echo "source ~/venv/venv$PVERS/bin/activate" >> ~/.bashrc 2>&1 | tee -a $LOGFILE
-source ~/venv/venv$PVERS/bin/activate
 bash ./installPythonLibs.sh 2>&1 | tee -a $LOGFILE
 bash ./installPythonCircuitpython.sh 2>&1 | tee -a $LOGFILE
 
