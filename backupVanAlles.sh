afa42dc57834c7cb1cb59be2378e9a8413f37f57
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2023-03-22 10:56:40 +0100

    Create backupVanAlles.sh

---
+++ backupVanAlles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+LOGFILE=$HOME/logs/backupVanAlles-`date +%Y-%m-%d_%Hh%Mm`.log
+sudo mkdir /mnt/usb0  2>&1 | tee -a $LOGFILE
+lsblk -fp  2>&1 | tee -a $LOGFILE
+sudo blkid  2>&1 | tee -a $LOGFILE
+sudo mount -t auto -o defaults  /dev/sda1 /mnt/usb0/  2>&1 | tee -a $LOGFILE
+7z a /mnt/externeUSB01/dietpi_backup.7z -r ~/dbs ~/.octoprint  2>&1 | tee -a $LOGFILE
