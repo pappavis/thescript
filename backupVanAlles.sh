4620798ddee48cb22c2d5c8dfc92cdf41e2c931c
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2023-03-24 18:35:30 +0100

    Update backupVanAlles.sh

--- backupVanAlles.sh
+++ backupVanAlles.sh
@@ -6,5 +6,5 @@ lsblk -fp  2>&1 | tee -a $LOGFILE
 sudo blkid  2>&1 | tee -a $LOGFILE
 sudo mount -t auto -o defaults  /dev/sda1 /mnt/usb0/  2>&1 | tee -a $LOGFILE
 sudo cp -v /mnt/usb0/rugsteun/dietpi_backup.7z /mnt/usb0/rugsteun/dietpi_backup_oud.7z  2>&1 | tee -a $LOGFILE
-sudo 7z a /mnt/usb0/rugsteun/dietpi_backup.7z -r ~/dbs ~/.octoprint ~/.homeassistant  ~/.node-red/ ~/Programmering/   2>&1 | tee -a $LOGFILE
+sudo 7z a -v /mnt/usb0/rugsteun/dietpi_backup.7z -r ~/dbs ~/.octoprint ~/.homeassistant  ~/.node-red/ ~/Programmering/   2>&1 | tee -a $LOGFILE
 echo "backup afgerond"  2>&1 | tee -a $LOGFILE
