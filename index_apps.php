7bd3ea2144df7cb78544244b08c642f3123e1e91
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2023-03-24 15:48:03 +0100

    Update index_apps.php

--- index_apps.php
+++ index_apps.php
@@ -143,7 +143,7 @@
 								<tr>
 									<td>
 										<script language="JavaScript">
-											document.write('SQLite <a target="_blank" href="' + window.location.protocol + '//' + window.location.hostname + '/phpliteadmin' +  '/" >phpliteadmin</a>.<br/' );				
+											document.write('SQLite <a target="_blank" href="' + window.location.protocol + '//' + window.location.hostname + '/support/phpliteadmin' +  '/" >phpliteadmin</a>.<br/' );				
 										</script>
 									</td>
 								</tr>
