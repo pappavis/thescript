0f7eebff96a79749da8595562bb68e953d25c9f2
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2023-03-26 13:41:57 +0200

    Update backupVanAlles.sh

--- backupVanAlles.sh
+++ backupVanAlles.sh
@@ -6,6 +6,8 @@ lsblk -fp  2>&1 | tee -a $LOGFILE
 sudo blkid  2>&1 | tee -a $LOGFILE
 sudo mount -t auto -o defaults  /dev/sda1 /mnt/usb0/  2>&1 | tee -a $LOGFILE
 sudo cp /mnt/usb0/rugsteun/dietpi_backup.7z /mnt/usb0/rugsteun/dietpi_backup_oud.7z  2>&1 | tee -a $LOGFILE
+echo "7z a /mnt/usb0/rugsteun/dietpi_backup.7z" 2>&1 | tee -a $LOGFILE
 sudo 7z a /mnt/usb0/rugsteun/dietpi_backup.7z -r ~/dbs ~/.octoprint ~/.homeassistant  ~/.node-red/ ~.npm/ ~/single_chan_pkt_fwd/ ~/venv /~.local ~/Programmering/ /~.micropython   2>&1 | tee -a $LOGFILE
+echo "7z u /mnt/usb0/rugsteun/dietpi_backup.7z" 2>&1 | tee -a $LOGFILE
 sudo 7z u /mnt/usb0/rugsteun/dietpi_backup.7z -r ~/dbs ~/.octoprint ~/.homeassistant  ~/.node-red/ ~.npm/ ~/single_chan_pkt_fwd/ ~/venv /~.local ~/Programmering/ /~.micropython   2>&1 | tee -a $LOGFILE
 echo "backup afgerond"  2>&1 | tee -a $LOGFILE
