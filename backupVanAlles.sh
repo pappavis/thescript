3d367fd1014b4cfc60a0ab42cff107bd4c4d593c
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2023-04-05 12:26:56 +0200

    Update backupVanAlles.sh

--- backupVanAlles.sh
+++ backupVanAlles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 LOGFILE=$HOME/logs/backupVanAlles-`date +%Y-%m-%d_%Hh%Mm`.log
-BACKUPDIR=/mnt/usb0/rugsteun/$(hostname)_backup.7z
+BACKUPDIR=/mnt/usb0/rugsteun
+BACKUPFILE=$(hostname)_backup.7z
 sudo mkdir /mnt/usb0  2>&1 | tee -a $LOGFILE
 sudo mkdir /mnt/usb0/rugsteun  2>&1 | tee -a $LOGFILE
 lsblk -fp  2>&1 | tee -a $LOGFILE
@@ -9,14 +10,14 @@ sudo mount -t auto -o defaults  /dev/sda1 /mnt/usb0/  2>&1 | tee -a $LOGFILE
 sudo cp /mnt/usb0/rugsteun/$(hostname)_backup.7z /mnt/usb0/rugsteun/$(hostname)_backup_oud.7z  2>&1 | tee -a $LOGFILE
 echo "7z a /mnt/usb0/rugsteun/$(hostname)_backup.7z" 2>&1 | tee -a $LOGFILE
 
-for addonnodes in ~/dbs ~/.octoprint ~/.homeassistant  ~/.node-red/ ~.npm/ ~/single_chan_pkt_fwd/ ~/venv /~.local ~/Programmering/ /~.micropython   ; do
+for addonnodes in ~/dbs ~/.octoprint ~/.homeassistant  ~/.node-red/ ~.npm/ ~/single_chan_pkt_fwd/ ~/venv /~.local ~/Programmering/ /~.micropython  /var/www/html/support/nextcloud/data  ; do
   echo "" 2>&1 | tee -a $LOGFILE
   echo "" 2>&1 | tee -a $LOGFILE
   echo "backupen van dir ${addonnodes}" 2>&1 | tee -a $LOGFILE
   echo "" 2>&1 | tee -a $LOGFILE
-  sudo 7z a /mnt/usb0/rugsteun/$(hostname)_backup.7z -r   ${addonnodes}  2>&1 | tee -a $LOGFILE
+  sudo 7z a $BACKUPDIR/$BACKUPFILE -r   ${addonnodes}  2>&1 | tee -a $LOGFILE
   echo "updaten van dir ${addonnodes}" 2>&1 | tee -a $LOGFILE
-  sudo 7z a /mnt/usb0/rugsteun/$(hostname)_backup.7z -r   ${addonnodes}  2>&1 | tee -a $LOGFILE
+  sudo 7z u $BACKUPDIR/$BACKUPFILE -r   ${addonnodes}  2>&1 | tee -a $LOGFILE
 done
 
 echo "Backup octopi" 2>&1 | tee -a $LOGFILE
