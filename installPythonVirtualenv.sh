74408e5c6babea54ca84d5fe7d4beba6ffe9e431
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2023-05-01 13:29:20 +0200

    Update installPythonVirtualenv.sh

--- installPythonVirtualenv.sh
+++ installPythonVirtualenv.sh
@@ -45,7 +45,7 @@ git clone https://github.com/AIGC-Audio/AudioGPT 2>&1 | tee -a $LOGFILE
 /usr/bin/python3  -m virtualenv -p /usr/bin/python3 ~/venv/audiogpt 2>&1 | tee -a $LOGFILE
 source ~/venv/audiogpt/bin/activate 2>&1 | tee -a $LOGFILE
 for addonnodes in accelerate addict==2.4.0 aiofiles albumentations==1.3.0 appdirs==1.4.4 basicsr==1.4.2 beautifulsoup4==4.10.0 Cython==0.29.24 diffusers \
-	einops==0.3.0 espnet espnet_model_zoo ffmpeg-python  g2p-en==2.1.0 google==3.0.0 gradio  h5py imageio==2.9.0  imageio-ffmpeg==0.4.2  invisible-watermark>=0.1.5 \
+	einops==0.3.0 espnet espnet_model_zoo ffmpeg-python  g2p-en==2.1.0 google==3.0.0 gradio  h5py imageio==2.9.0  imageio-ffmpeg==0.4.2  invisible-watermark=0.1.5 \
 	jieba kornia==0.6  langchain==0.0.101 librosa loguru miditoolkit==0.1.7 mmcv==1.5.0 mmdet==2.23.0  mmengine==0.7.2 moviepy==1.0.3 numpy==1.23.1 omegaconf==2.1.1 \
 	open_clip_torch==2.0.2  openai openai-whisper opencv-contrib-python==4.3.0.36 praat-parselmouth==0.3.3 prettytable==3.6.0 proglog==0.1.9 pycwt==0.3.0a22 pyloudnorm==0.1.0 \
 	pypinyin==0.43.0  pytorch-lightning==1.5.0  pytorch-ssim==0.1  pyworld==0.3.0  resampy==0.2.2 Resemblyzer==0.1.1.dev0  safetensors==0.2.7  sklearn==0.0 soundfile \
