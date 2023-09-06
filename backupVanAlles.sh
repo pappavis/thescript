4a9d3bf622c37ce67096ac6177c3ac99d9d68203
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2023-09-06 10:54:06 +0200

    Update backupVanAlles.sh

--- backupVanAlles.sh
+++ backupVanAlles.sh
@@ -10,15 +10,15 @@ sudo mount -t auto -o defaults  /dev/sda1 /mnt/usb0/  2>&1 | tee -a $LOGFILE
 sudo cp /mnt/usb0/rugsteun/$(hostname)_backup.7z /mnt/usb0/rugsteun/$(hostname)_backup_oud.7z  2>&1 | tee -a $LOGFILE
 echo "7z a /mnt/usb0/rugsteun/$(hostname)_backup.7z" 2>&1 | tee -a $LOGFILE
 
-for addonnodes in ~/dbs ~/.octoprint ~/.homeassistant  ~/.node-red/ ~.npm/ ~/single_chan_pkt_fwd/ ~/venv /~.local ~/Programmering/ /~.micropython  /var/www/html/support/nextcloud/data /var/lib/mysql/   ; do
-  echo "" 2>&1 | tee -a $LOGFILE
-  echo "" 2>&1 | tee -a $LOGFILE
-  echo "backupen van dir ${addonnodes}" 2>&1 | tee -a $LOGFILE
-  echo "" 2>&1 | tee -a $LOGFILE
-  sudo 7z a $BACKUPDIR/rugsteun_${hostname}_${addonnodes}.7z -r  ${addonnodes}  2>&1 | tee -a $LOGFILE
-  echo "updaten van dir ${addonnodes}" 2>&1 | tee -a $LOGFILE
-  sudo 7z u $BACKUPDIR/$BACKUPFILE -r   ${addonnodes}  2>&1 | tee -a $LOGFILE
-done
+# for addonnodes in ~/dbs ~/.octoprint ~/.homeassistant  ~/.node-red/ ~.npm/ ~/single_chan_pkt_fwd/ ~/venv /~.local ~/Programmering/ /~.micropython  /var/www/html/support/nextcloud/data /var/lib/mysql/   ; do
+#   echo "" 2>&1 | tee -a $LOGFILE
+#   echo "" 2>&1 | tee -a $LOGFILE
+#   echo "backupen van dir ${addonnodes}" 2>&1 | tee -a $LOGFILE
+#   echo "" 2>&1 | tee -a $LOGFILE
+#   sudo 7z a $BACKUPDIR/rugsteun_${hostname}_${addonnodes}.7z -r  ${addonnodes}  2>&1 | tee -a $LOGFILE
+#   echo "updaten van dir ${addonnodes}" 2>&1 | tee -a $LOGFILE
+#   sudo 7z u $BACKUPDIR/$BACKUPFILE -r   ${addonnodes}  2>&1 | tee -a $LOGFILE
+# done
 
 7z a $BACKUPDIR/$(hostname)_dbs_backup.7z -r  ~/dbs  2>&1 | tee -a $LOGFILE
 7z a $BACKUPDIR/$(hostname)_octoprint_backup.7z -r  ~/.octoprint  2>&1 | tee -a $LOGFILE
@@ -30,10 +30,11 @@ done
 7z a $BACKUPDIR/$(hostname)_local_backup.7z -r  ~/.local  2>&1 | tee -a $LOGFILE
 7z a $BACKUPDIR/$(hostname)_Programmering_backup.7z -r  ~/Programmering  2>&1 | tee -a $LOGFILE
 7z a $BACKUPDIR/$(hostname)_micropython_backup.7z -r  ~/.micropython  2>&1 | tee -a $LOGFILE
+sudo 7z a $BACKUPDIR/$(hostname)_userLocal_backup.7z -r  ~/usr/local  2>&1 | tee -a $LOGFILE
 sudo 7z a $BACKUPDIR/$(hostname)_nextcloud_backup.7z -r  /var/www/html/support/nextcloud/data  2>&1 | tee -a $LOGFILE
 sudo 7z a $BACKUPDIR/$(hostname)_mysql_backup.7z -r  /var/lib/mysql/   2>&1 | tee -a $LOGFILE
 
-~/.octoprint ~/.homeassistant  ~/.node-red/ ~/.npm/ ~/single_chan_pkt_fwd/ ~/venv /~.local ~/Programmering/ /~.micropython  /var/www/html/support/nextcloud/data /var/lib/mysql/   ; do
+# ~/.octoprint ~/.homeassistant  ~/.node-red/ ~/.npm/ ~/single_chan_pkt_fwd/ ~/venv /~.local ~/Programmering/ /~.micropython  /var/www/html/support/nextcloud/data /var/lib/mysql/   ; do
 
 echo "Backup octopi" 2>&1 | tee -a $LOGFILE
 octopiDir=/mnt/nfs/octopi/pi
