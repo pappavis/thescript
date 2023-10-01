81b8b6cffdc9a540de1e65ebc3d9361ab71c47a1
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2023-10-01 13:24:08 +0200

    Update installPythonLibs.sh

--- installPythonLibs.sh
+++ installPythonLibs.sh
@@ -50,8 +50,8 @@ for addonnodes in pip setuptools wheel openpyxl pylzma py7zr o365 ttn qrcode pil
                   msteamsconnector matplotlib numpy imutils pyodbc influxdb pysmb libopencv-dev opencv-python  git+https://github.com/pytube/pytube picamera djitellopy \
 		   osxphotos RPi.GPIO tox PySimpleGUI libusb pyusb pdfkit python-dateutil pymysql python-vkontakte easyocr pygrabber \
 		   imutils psycopg2 postgres firebirdsql html2pdf open3d face-recognition pyftdi psycopg2 asyncio pyshorteners picamera  homekit  pyaudio \ 
-		   tk-tools pyqt5 aspose-words Office365-REST-Python-Client  pyresidfp soundcard pandas 
-     		   streamlit ; do
+		   tk-tools pyqt5 aspose-words Office365-REST-Python-Client  pyresidfp  \
+     		   soundcard pandas streamlit ; do
 
     echo "" 2>&1 | tee -a $LOGFILE
     echo "Installeren python lib: \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
