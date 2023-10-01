f9cff57818c7c346a40a6c694b724825482fe9e9
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2023-10-01 19:45:06 +0200

    Update installsqlite3.sh

--- installsqlite3.sh
+++ installsqlite3.sh
@@ -1,38 +1,34 @@
 $LOGFILE="~/logs/installsqlite3_logs.log"
 echo "**start SQLite install" 2>&1 | tee -a $LOGFILE
 mkdir ~/tmp
+adminpass="admin"
 
 sudo apt update -y
 sudo apt install -y apache2 php php-mysql php-sqlite3 php-mbstring openssl libapache2-mod-php php-xml php-mbstring
-cd ~
+cd ~/
 
   # if apache OR nginx installed and /var/www/html exists, go on...
-    if [ $webserver -eq 1 ] && [ -d /var/www/html ]; then
+#    if [ $webserver -eq 1 ] && [ -d /var/www/html ]; then
         cd /var/www/html
-        sudo mkdir phpliteadmin
-        cd phpliteadmin
+        sudo mkdir ,.phpliteadmin
+        cd ./phpliteadmin
         #sudo wget --no-check-certificate http://bitbucket.org/phpliteadmin/public/downloads/phpLiteAdmin_v1-9-7-1.zip -a $LOGFILE
-        sudo wget --no-check-certificate https://www.phpliteadmin.org/phpliteadmin-dev.zip -a $LOGFILE
-        if [ $? -eq 0 ]; then
-            sudo unzip phpliteadmin-dev.zip 2>&1 | tee -a $LOGFILE
-            sudo mv phpliteadmin.php index.php
-            sudo mv phpliteadmin.config.sample.php phpliteadmin.config.php
-            sudo rm *.zip
-            sudo mkdir themes
-            cd themes
-            sudo wget --no-check-certificate http://bitbucket.org/phpliteadmin/public/downloads/phpliteadmin_themes_2016-02-29.zip -a $LOGFILE
-            sudo unzip phpliteadmin_themes_2016-02-29.zip 2>&1 | tee -a $LOGFILE
-            sudo rm *.zip
-            sudo sed -i -e 's#\$directory = \x27.\x27;#\$directory = \x27/home/pi/dbs/\x27;#g' /var/www/html/phpliteadmin/phpliteadmin.config.php
-            sudo sed -i -e "s#\$password = \x27admin\x27;#\$password = \x27$adminpass\x27;#g" /var/www/html/phpliteadmin/phpliteadmin.config.php
-            sudo sed -i -e "s#\$subdirectories = false;#\$subdirectories = true;#g" /var/www/html/phpliteadmin/phpliteadmin.config.php
-            cd
-        else
-            printl "${IRed}!!!! PHPLITEADMIN not installed! ${IWhite}\r\n"
-            cd ; rm -rf /var/www/html/phpliteadmin
-        fi
+	sudo wget --no-check-certificate https://www.phpliteadmin.org/phpliteadmin-dev.zip -a $LOGFILE
+	sudo unzip phpliteadmin-dev.zip 2>&1 | tee -a $LOGFILE
+	sudo mv phpliteadmin.php index.php
+	sudo mv phpliteadmin.config.sample.php phpliteadmin.config.php
+	sudo rm *.zip
+	sudo mkdir themes
+	cd themes
+	sudo wget --no-check-certificate http://bitbucket.org/phpliteadmin/public/downloads/phpliteadmin_themes_2016-02-29.zip -a $LOGFILE
+	sudo unzip phpliteadmin_themes_2016-02-29.zip 2>&1 | tee -a $LOGFILE
+	sudo rm *.zip
+	sudo sed -i -e 's#\$directory = \x27.\x27;#\$directory = \x27/home/pi/dbs/\x27;#g' /var/www/html/phpliteadmin/phpliteadmin.config.php
+	sudo sed -i -e "s#\$password = \x27admin\x27;#\$password = \x27$adminpass\x27;#g" /var/www/html/phpliteadmin/phpliteadmin.config.php
+	sudo sed -i -e "s#\$subdirectories = false;#\$subdirectories = true;#g" /var/www/html/phpliteadmin/phpliteadmin.config.php
+	cd
         
-        mkdir dbs
+        mkdir  /home/pi/dbs
 		sqlite3 /home/pi/dbs/iot.db << EOF
 		CREATE TABLE IF NOT EXISTS \`pinDescription\` (
 		 \`pinID\` INTEGER PRIMARY KEY NOT NULL,
@@ -108,9 +104,5 @@ EOF
         chmod 777 /home/pi/dbs
         chmod 666 /home/pi/dbs/iot.db
         cd
-    else
-        printl "${IRed}!!!! Webserver+PHP+SQLITE+PHPLITEADMIN NOT INSTALLED! ${IWhite}\r\n"
-    fi
-fi
 
-echo "SQLIte3 install afgerond"
+echo "SQLIte3 install afgerond" 2>&1 | tee -a $LOGFILE
