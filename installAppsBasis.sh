29a0a4f7cd931f176380ff6c14878108bd7fef07
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2023-11-07 13:54:19 +0100

    Update installAppsBasis.sh

--- installAppsBasis.sh
+++ installAppsBasis.sh
@@ -27,15 +27,21 @@ for addonnodes in libatlas3-base qtchooser imagemagick libfontconfig1-dev libcai
 	sudo apt install -y ${addonnodes} 2>&1 | tee -a $LOGFILE
 done
 
-
-for addonnodes in libatlas3-base qtchooser imagemagick libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libssl libcurl4-gnutls-dev libcurl4-openssl-dev libcurl4-openssl-dev  libsdl2-ttf-dev libsdl2-image-dev ccze net-tools ntfs-3g php-intl i2c-tools python3-rpi.gpio libgstreamer-plugins-base1.0-dev rapidjson-dev libatomic-ops-dev ; do 
+for addonnodes in libatlas3-base qtchooser imagemagick libfontconfig1-dev libcairo2-dev libgdk-pixbuf2.0-dev libpango1.0-dev libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran libssl libcurl4-gnutls-dev libcurl4-openssl-dev libcurl4-openssl-dev  libsdl2-ttf-dev libsdl2-image-dev ccze net-tools ntfs-3g php-intl i2c-tools python3-rpi.gpio libgstreamer-plugins-base1.0-dev rapidjson-dev libatomic-ops-dev   ; do 
 	echo ""  2>&1 | tee -a $LOGFILE
 	echo "--Installeren  \"${addonnodes}\""  2>&1 | tee -a $LOGFILE
 	sudo apt install -y ${addonnodes} 2>&1 | tee -a $LOGFILE
 done
 
+for addonnodes in   alsa alsa-utils  libfluidsynth-dev fluidsynth  ; do 
+	echo ""  2>&1 | tee -a $LOGFILE
+	echo "--Installeren  muziek libs & haxophone \"${addonnodes}\""  2>&1 | tee -a $LOGFILE
+	sudo apt install -y ${addonnodes} 2>&1 | tee -a $LOGFILE
+done
+
+
 echo ""  2>&1 | tee -a $LOGFILE
-echo  "vsftpd, wijzigen write_enable"  2>&1 | tee -a $LOGFILE
+echo  "* vsftpd, wijzigen write_enable"  2>&1 | tee -a $LOGFILE
 sudo sed -i -e '/#write_enable/s/#write_enable/write_enable/' /etc/vsftpd.conf
 sudo service vsftpd restart
 sudo service vsftpd status 2>&1 | tee -a $LOGFILE
