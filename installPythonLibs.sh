3c5e2039239f77a84756924504919472e4676044
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2024-10-17 13:54:10 +0200

    Update installPythonLibs.sh

--- installPythonLibs.sh
+++ installPythonLibs.sh
@@ -45,7 +45,7 @@ for addonnodes in pip setuptools wheel scikit-build numpy ; do
     echo "" 2>&1 | tee -a $LOGFILE
   done
 
-for addonnodes in pip setuptools wheel openpyxl pylzma py7zr o365 ttn qrcode pillow sqlalchemy sqlalchemy-access pymsteams qscintilla esptool adafruit-ampy firebirdsql   \
+for addonnodes in pip setuptools wheel openpyxl pylzma py7zr o365 ttn qrcode pillow sqlalchemy sqlalchemy-access pymsteams qscintilla esptool adafruit-ampy firebirdsql pyresidfp  \
                   pyserial pyparsing pyzmail redmail gpiozero pytube pipx serial jinja2 esptool mpfshell virtualenv ffmpeg  jupyter-notebook \
                   scikit-build pygame pymongo psycopg2-binary mysql-connector-python guizero imutils scikit-image bokeh django flask pygrabber paho-mqtt \
                   msteamsconnector matplotlib numpy imutils pyodbc influxdb pysmb libopencv-dev opencv-python  git+https://github.com/pytube/pytube picamera djitellopy \
