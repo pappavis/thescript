98946183316720afe2118639ce86fc7ab1b18f2a
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2025-03-13 09:37:06 +0100

    Update installAI.sh

--- installAI.sh
+++ installAI.sh
@@ -51,5 +51,7 @@ curl http://localhost:11434/api/generate -d '{
   "stream": false
 }' 
 
+nohup open-webui serve  2>&1 | tee -a $LOGFILE &
+
 echo "EINDE installAI.sh" 2>&1 | tee -a $LOGFILE
 echo "" 2>&1 | tee -a $LOGFILE
