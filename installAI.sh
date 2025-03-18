e32422d080273f69eb75ea769754cb40a274a90f
Michiel Erasmus <15038654+pappavis@users.noreply.github.com>
2025-03-18 08:00:58 +0100

    Update installAI.sh

--- installAI.sh
+++ installAI.sh
@@ -12,11 +12,10 @@ echo "START installAI.sh" 2>&1 | tee -a $LOGFILE
 echo '** Installeer Kunstmatige intilligentie. Je moet eerst een virtualenv activeer!!'  2>&1 | tee -a $LOGFILE
 source ~/venv/venv/bin/activate
 
-for addonnodes in curl ollama exim4 ; do
+for addonnodes in curl ollama exim4 postgresql ; do
     echo "" 2>&1 | tee -a $LOGFILE
     echo "Installeren Kunstmatige Intilligentie hulp: \"${addonnodes}\"" 2>&1 | tee -a $LOGFILE
     sudo apt install $NQUIET -y ${addonnodes} 2>&1 | tee -a $LOGFILE
-    #conda install --upgrade --no-cache-dir  ${addonnodes} 2>&1 | tee -a $LOGFILE
     echo "" 2>&1 | tee -a $LOGFILE
   done
 
